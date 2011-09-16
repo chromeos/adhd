5a0133b3ec9915ecbd57ab50469b710bcb8eabd7
Taylor Hutt <thutt@chromium.org>
2011-09-16 08:31:11 -0700

    ADHD: Add missing 'board-tegra2.h' file.
    Details
    
      Buildbots are failing because they are building in ways I didn't know.
    
      This adds a file that is required for the buildbots to succeed in
      building the ADHD package.
    
    BUG=none
    TEST=Let buildbot build.
    Signed-off-by: Taylor Hutt <thutt@chromium.org>
    
    Change-Id: If234bba9f68cf76d112cde312ff0682657c130e2

---
+++ gavd/board-tegra2.h
@@ -0,0 +1,10 @@
+/* Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#if !defined(_BOARD_TEGRA2_KAEN_H_)
+#define _BOARD_TEGRA2_KAEN_H_
+
+#undef ADHD_GPIO_HEADPHONE
+
+#endif
