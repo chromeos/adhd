00654eecb47a9b619df0881e97a9490e44176d67
Ryan Harrison <rharrison@chromium.org>
2011-10-26 12:03:30 -0400

    Added in a new header and config for Bumblebee, so that we can build a basic overlay.
    These are stub values that I cloned from another tegra device so I don't expect
    the sound to work at the moment.
    
    BUG=chromium-os:22237
    BUG=chromium-os:22234
    TEST=manual, built image using external kernel and u-boot and got a working image
    
    Change-Id: Ia906d0b2e07e0495a4be04fc853489fb43bcf616

---
+++ gavd/board-tegra2_bumblebee.h
@@ -0,0 +1,8 @@
+/* Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#if !defined(_BOARD_TEGRA2_BUMBLEBEE_H_)
+#define _BOARD_TEGRA2_BUMBLEBEE_H_
+
+#endif
