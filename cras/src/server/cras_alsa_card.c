9df420f1e41c8113555497baf7d9939996733e3d
Dylan Reid <dgreid@chromium.org>
2012-03-22 10:31:27 -0700

    CRAS: Read config file for each card found.
    If there is a config file found for a new card read in the file.  This
    file will be used to configure a custom volume curve for certain
    devices and outputs.  This uses libiniparser to read the file as it
    had an ebuild handy.
    
    BUG=chrome-os-partner:8155
    TEST=updated unitests: make check.
    
    Change-Id: I508e63b9e5a188322d542abd88af4ba929156454
    Signed-off-by: Dylan Reid <dgreid@chromium.org>

--- cras/src/server/cras_alsa_card.c
+++ cras/src/server/cras_alsa_card.c
@@ -4,6 +4,7 @@
  */
 
 #include <alsa/asoundlib.h>
+#include <iniparser.h>
 #include <syslog.h>
 
 #include "cras_alsa_io.h"
@@ -13,17 +14,27 @@
 
 #define MAX_ALSA_CARDS 32 /* Alsa limit on number of cards. */
 #define MAX_ALSA_PCM_NAME_LENGTH 6 /* Alsa names "hw:XX" + 1 for null. */
+#define CARD_CONFIG_FILE_DIR "/etc/cras"
+#define MAX_INI_NAME_LENGTH 63 /* 63 chars + 1 for null where declared. */
 
 struct iodev_list_node {
 	struct cras_iodev *iodev;
 	struct iodev_list_node *prev, *next;
 };
 
+/* Holds information about each sound card on the system.
+ * name - of the form hw:XX,YY.
+ * card_index - 0 based index, value of "XX" in the name.
+ * iodevs - Input and output devices for this card.
+ * mixer - Controls the mixer controls for this card.
+ * ini - Dictionary from libiniparser containing the config file info.
+ */
 struct cras_alsa_card {
 	char name[MAX_ALSA_PCM_NAME_LENGTH];
 	size_t card_index;
 	struct iodev_list_node *iodevs;
 	struct cras_alsa_mixer *mixer;
+	dictionary *ini;
 };
 
 /* Creates an iodev for the given device. */
@@ -48,6 +59,21 @@ static struct iodev_list_node *create_iodev_for_device(
 	return new_dev;
 }
 
+/* Read the config file for this card.  The config file will specify any special
+ * volume curves needed for the device. */
+static int read_card_config(dictionary **ini, const char *ini_dir,
+			   const char *card_name)
+{
+	char ini_name[MAX_INI_NAME_LENGTH + 1];
+
+	snprintf(ini_name, MAX_INI_NAME_LENGTH, "%s/%s", ini_dir, card_name);
+	ini_name[MAX_INI_NAME_LENGTH] = '\0';
+	*ini = iniparser_load(ini_name);
+	if (*ini == NULL)
+		return -EINVAL;
+	return 0;
+}
+
 /*
  * Exported Interface.
  */
@@ -56,6 +82,7 @@ struct cras_alsa_card *cras_alsa_card_create(size_t card_idx)
 {
 	snd_ctl_t *handle = NULL;
 	int rc, dev_idx;
+	snd_ctl_card_info_t *card_info;
 	snd_pcm_info_t *dev_info;
 	struct cras_alsa_card *alsa_card;
 
@@ -64,6 +91,7 @@ struct cras_alsa_card *cras_alsa_card_create(size_t card_idx)
 		return NULL;
 	}
 
+	snd_ctl_card_info_alloca(&card_info);
 	snd_pcm_info_alloca(&dev_info);
 
 	alsa_card = calloc(1, sizeof(*alsa_card));
@@ -75,16 +103,27 @@ struct cras_alsa_card *cras_alsa_card_create(size_t card_idx)
 		 "hw:%zu",
 		 card_idx);
 
+	rc = snd_ctl_open(&handle, alsa_card->name, 0);
+	if (rc < 0) {
+		syslog(LOG_ERR, "Fail opening control %s.", alsa_card->name);
+		goto error_bail;
+	}
+
+	rc = snd_ctl_card_info(handle, card_info);
+	if (rc < 0) {
+		syslog(LOG_ERR, "Error getting card info.");
+		goto error_bail;
+	}
+
+	/* Read config file for this card if it exists. */
+	if (read_card_config(&alsa_card->ini, CARD_CONFIG_FILE_DIR,
+			     snd_ctl_card_info_get_name(card_info)) < 0)
+		syslog(LOG_DEBUG, "No config file for %s", alsa_card->name);
+
 	/* Create one mixer per card. */
 	alsa_card->mixer = cras_alsa_mixer_create(alsa_card->name);
 	if (alsa_card->mixer == NULL) {
 		syslog(LOG_ERR, "Fail opening mixer for %s.", alsa_card->name);
-		goto error_bail;
-	}
-
-	rc = snd_ctl_open(&handle, alsa_card->name, 0);
-	if (rc < 0) {
-		syslog(LOG_ERR, "Fail opening control %s.", alsa_card->name);
 		goto error_bail;
 	}
 
@@ -140,6 +179,8 @@ error_bail:
 		snd_ctl_close(handle);
 	if (alsa_card->mixer)
 		cras_alsa_mixer_destroy(alsa_card->mixer);
+	if (alsa_card->ini)
+		iniparser_freedict(alsa_card->ini);
 	free(alsa_card);
 	return NULL;
 }
@@ -158,5 +199,7 @@ void cras_alsa_card_destroy(struct cras_alsa_card *alsa_card)
 		DL_DELETE(alsa_card->iodevs, curr);
 		free(curr);
 	}
+	if (alsa_card->ini)
+		iniparser_freedict(alsa_card->ini);
 	free(alsa_card);
 }
