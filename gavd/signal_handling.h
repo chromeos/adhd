675fc4a53c1aff9a6f4815bc9bd8699182e1240d
Taylor Hutt <thutt@chromium.org>
2012-06-25 09:37:04 -0700

    ADHD: Remove gavd sources
    Now that the functionality of gavd has been incorporated into cras, it
    is time to delete the source.
    
    BUG=none
    TEST=Built adhd for tegra2_kaen & x86-mario w/ no issues.
    Signed-off-by: Taylor Hutt <thutt@chromium.org>
    
    Change-Id: I651f3a2ec81373d02407c1a4176ccbe1d93e9081

--- gavd/signal_handling.h
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
- */
-#if !defined(_SIGNAL_HANDLING_H_)
-#define _SIGNAL_HANDLING_H_
-
-void signal_start(void);
-#endif
