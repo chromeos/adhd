18ee149f8b66343d0d1b1e0b893386956a39d802
Dylan Reid <dgreid@chromium.org>
2012-08-09 17:21:39 -0700

    CRAS: system state - Use shm for server state.
    Move the server state to shared memory to ease sharing it with clients.
    Currently copies of this data are sent to client through ipc.  Sharing a
    read-only copy with them will save storage on their end, make messages
    smaller, and reduce code complexity on the client side.
    
    BUG=chromium-os32347
    TEST=shm_unittest
    
    Change-Id: Ib4bff51025d8144566c908c0dc8fff2aaefcc48c
    Signed-off-by: Dylan Reid <dgreid@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/29859
    Reviewed-by: Olof Johansson <olofj@chromium.org>

--- cras/src/server/cras_system_state.c
+++ cras/src/server/cras_system_state.c
@@ -4,6 +4,8 @@
  */
 
 #include <string.h>
+#include <stdlib.h>
+#include <sys/shm.h>
 #include <syslog.h>
 
 #include "cras_alsa_card.h"
@@ -30,6 +32,8 @@ struct volume_callback_list {
 /* The system state.
  * Members:
  *    exp_state - The exported system state shared with clients.
+ *    shm_key - Key for shm area of system_state struct.
+ *    shm_id - Id for shm area of system_state struct.
  *    device_blacklist - Blacklist of device the server will ignore.
  *    volume_callbacks - Called when the system volume changes.
  *    mute_callbacks - Called when the system mute state changes.
@@ -39,7 +43,9 @@ struct volume_callback_list {
  *    cards - A list of active sound cards in the system.
  */
 static struct {
-	struct cras_server_state exp_state;
+	struct cras_server_state *exp_state;
+	int shm_key;
+	int shm_id;
 	struct cras_device_blacklist *device_blacklist;
 	struct volume_callback_list *volume_callbacks;
 	struct volume_callback_list *mute_callbacks;
@@ -100,8 +106,27 @@ static int remove_callback(struct volume_callback_list **list,
 
 void cras_system_state_init()
 {
-	struct cras_server_state *exp_state = &state.exp_state;
+	struct cras_server_state *exp_state;
+	unsigned loops = 0;
 
+	/* Find an available shm key. */
+	do {
+		state.shm_key = getpid() + rand();
+		state.shm_id = shmget(state.shm_key, sizeof(*exp_state),
+				      IPC_CREAT | IPC_EXCL | 0640);
+	} while (state.shm_id < 0 && loops++ < 100);
+	if (state.shm_id < 0) {
+		syslog(LOG_ERR, "Fatal: system state can't shmget");
+		exit(state.shm_id);
+	}
+
+	exp_state = shmat(state.shm_id, NULL, 0);
+	if (exp_state == (void *)-1) {
+		syslog(LOG_ERR, "Fatal: system state can't shmat");
+		exit(-ENOMEM);
+	}
+
+	/* Initial system state. */
 	exp_state->state_version = CRAS_SERVER_STATE_VERSION;
 	exp_state->volume = CRAS_MAX_SYSTEM_VOLUME;
 	exp_state->mute = 0;
@@ -112,6 +137,8 @@ void cras_system_state_init()
 	exp_state->min_capture_gain = DEFAULT_MIN_CAPTURE_GAIN;
 	exp_state->max_capture_gain = DEFAULT_MAX_CAPTURE_GAIN;
 	exp_state->num_streams_attached = 0;
+
+	state.exp_state = exp_state;
 
 	/* Read config file for blacklisted devices. */
 	state.device_blacklist =
@@ -125,6 +152,11 @@ void cras_system_state_deinit()
 	/* Free any resources used.  This prevents unit tests from leaking. */
 
 	cras_device_blacklist_destroy(state.device_blacklist);
+
+	if (state.exp_state) {
+		shmdt(state.exp_state);
+		shmctl(state.shm_id, IPC_RMID, (void *)state.exp_state);
+	}
 
 	DL_FOREACH_SAFE(state.volume_callbacks, cb, tmp) {
 		DL_DELETE(state.volume_callbacks, cb);
@@ -164,14 +196,14 @@ void cras_system_set_volume(size_t volume)
 	if (volume > CRAS_MAX_SYSTEM_VOLUME)
 		syslog(LOG_DEBUG, "system volume set out of range %zu", volume);
 
-	state.exp_state.volume = min(volume, CRAS_MAX_SYSTEM_VOLUME);
+	state.exp_state->volume = min(volume, CRAS_MAX_SYSTEM_VOLUME);
 	DL_FOREACH(state.volume_callbacks, vol_cb)
 		vol_cb->callback(vol_cb->data);
 }
 
 size_t cras_system_get_volume()
 {
-	return state.exp_state.volume;
+	return state.exp_state->volume;
 }
 
 int cras_system_register_volume_changed_cb(cras_system_volume_changed_cb cb,
@@ -190,14 +222,14 @@ void cras_system_set_capture_gain(long gain)
 {
 	struct volume_callback_list *capture_cb;
 
-	state.exp_state.capture_gain = gain;
+	state.exp_state->capture_gain = gain;
 	DL_FOREACH(state.capture_gain_callbacks, capture_cb)
 		capture_cb->callback(capture_cb->data);
 }
 
 long cras_system_get_capture_gain()
 {
-	return state.exp_state.capture_gain;
+	return state.exp_state->capture_gain;
 }
 
 int cras_system_register_capture_gain_changed_cb(
@@ -217,14 +249,14 @@ void cras_system_set_mute(int mute)
 {
 	struct volume_callback_list *mute_cb;
 
-	state.exp_state.mute = !!mute;
+	state.exp_state->mute = !!mute;
 	DL_FOREACH(state.mute_callbacks, mute_cb)
 		mute_cb->callback(mute_cb->data);
 }
 
 int cras_system_get_mute()
 {
-	return state.exp_state.mute;
+	return state.exp_state->mute;
 }
 
 int cras_system_register_mute_changed_cb(cras_system_volume_changed_cb cb,
@@ -243,14 +275,14 @@ void cras_system_set_capture_mute(int mute)
 {
 	struct volume_callback_list *mute_cb;
 
-	state.exp_state.capture_mute = !!mute;
+	state.exp_state->capture_mute = !!mute;
 	DL_FOREACH(state.capture_mute_callbacks, mute_cb)
 		mute_cb->callback(mute_cb->data);
 }
 
 int cras_system_get_capture_mute()
 {
-	return state.exp_state.capture_mute;
+	return state.exp_state->capture_mute;
 }
 
 int cras_system_register_capture_mute_changed_cb(
@@ -269,20 +301,20 @@ void cras_system_set_volume_limits(long min, long max)
 {
 	struct volume_callback_list *limit_cb;
 
-	state.exp_state.min_volume_dBFS = min;
-	state.exp_state.max_volume_dBFS = max;
+	state.exp_state->min_volume_dBFS = min;
+	state.exp_state->max_volume_dBFS = max;
 	DL_FOREACH(state.volume_limits_callbacks, limit_cb)
 		limit_cb->callback(limit_cb->data);
 }
 
 long cras_system_get_min_volume()
 {
-	return state.exp_state.min_volume_dBFS;
+	return state.exp_state->min_volume_dBFS;
 }
 
 long cras_system_get_max_volume()
 {
-	return state.exp_state.max_volume_dBFS;
+	return state.exp_state->max_volume_dBFS;
 }
 
 int cras_system_register_volume_limits_changed_cb(
@@ -301,30 +333,30 @@ void cras_system_set_capture_gain_limits(long min, long max)
 {
 	struct volume_callback_list *limit_cb;
 
-	state.exp_state.min_capture_gain = min;
-	state.exp_state.max_capture_gain = max;
+	state.exp_state->min_capture_gain = min;
+	state.exp_state->max_capture_gain = max;
 	DL_FOREACH(state.volume_limits_callbacks, limit_cb)
 		limit_cb->callback(limit_cb->data);
 }
 
 long cras_system_get_min_capture_gain()
 {
-	return state.exp_state.min_capture_gain;
+	return state.exp_state->min_capture_gain;
 }
 
 long cras_system_get_max_capture_gain()
 {
-	return state.exp_state.max_capture_gain;
+	return state.exp_state->max_capture_gain;
 }
 
 int cras_system_has_played_streams()
 {
-	return state.exp_state.num_streams_attached != 0;
+	return state.exp_state->num_streams_attached != 0;
 }
 
 unsigned int cras_system_increment_streams_played()
 {
-	return ++state.exp_state.num_streams_attached;
+	return ++state.exp_state->num_streams_attached;
 }
 
 int cras_system_add_alsa_card(struct cras_alsa_card_info *alsa_card_info)
