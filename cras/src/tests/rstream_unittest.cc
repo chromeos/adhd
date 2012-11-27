cdf0a914c9542183736eba27e756f8468354f0ff
Dylan Reid <dgreid@chromium.org>
2012-11-26 19:43:35 -0800

    CRAS: rstream UT - Add input creation test.
    BUG=none
    TEST=this is a test.
    
    Change-Id: I307df7c1ee0b0d9b7884ca16d8c3b0391ffeeb5e
    Signed-off-by: Dylan Reid <dgreid@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/40956
    Reviewed-by: Chih-Chung Chang <chihchung@chromium.org>

--- cras/src/tests/rstream_unittest.cc
+++ cras/src/tests/rstream_unittest.cc
@@ -92,7 +92,7 @@ TEST_F(RstreamTestSuite, InvalidStreamPointer) {
   EXPECT_NE(0, rc);
 }
 
-TEST_F(RstreamTestSuite, Create) {
+TEST_F(RstreamTestSuite, CreateOutput) {
   struct cras_rstream *s;
   struct cras_audio_format fmt_ret;
   struct cras_audio_shm *shm_ret;
@@ -117,6 +117,53 @@ TEST_F(RstreamTestSuite, Create) {
   EXPECT_EQ(2048, cras_rstream_get_min_cb_level(s));
   EXPECT_EQ(CRAS_STREAM_TYPE_DEFAULT, cras_rstream_get_type(s));
   EXPECT_EQ(CRAS_STREAM_OUTPUT, cras_rstream_get_direction(s));
+  EXPECT_NE((void *)NULL, cras_rstream_get_shm(s));
+  rc = cras_rstream_get_format(s, &fmt_ret);
+  EXPECT_EQ(0, rc);
+  EXPECT_EQ(0, memcmp(&fmt_ret, &fmt_, sizeof(fmt_)));
+
+  // Check if shm is really set up.
+  shm_ret = cras_rstream_get_shm(s);
+  ASSERT_NE((void *)NULL, shm_ret);
+  key_ret = cras_rstream_get_shm_key(s);
+  shm_size = cras_rstream_get_total_shm_size(s);
+  EXPECT_GT(shm_size, 4096);
+  shmid = shmget(key_ret, shm_size, 0600);
+  EXPECT_GE(shmid, 0);
+  shm_mapped.area = (struct cras_audio_shm_area *)shmat(shmid, NULL, 0);
+  EXPECT_NE((void *)NULL, shm_mapped.area);
+  cras_shm_copy_shared_config(&shm_mapped);
+  EXPECT_EQ(cras_shm_used_size(&shm_mapped), cras_shm_used_size(shm_ret));
+  shmdt(shm_mapped.area);
+
+  cras_rstream_destroy(s);
+}
+
+TEST_F(RstreamTestSuite, CreateInput) {
+  struct cras_rstream *s;
+  struct cras_audio_format fmt_ret;
+  struct cras_audio_shm *shm_ret;
+  struct cras_audio_shm shm_mapped;
+  int rc, key_ret, shmid;
+  size_t shm_size;
+
+  rc = cras_rstream_create(555,
+      CRAS_STREAM_TYPE_DEFAULT,
+      CRAS_STREAM_INPUT,
+      &fmt_,
+      4096,
+      1024,
+      2048,
+      0,
+      NULL,
+      &s);
+  EXPECT_EQ(0, rc);
+  EXPECT_NE((void *)NULL, s);
+  EXPECT_EQ(4096, cras_rstream_get_buffer_size(s));
+  EXPECT_EQ(1024, cras_rstream_get_cb_threshold(s));
+  EXPECT_EQ(2048, cras_rstream_get_min_cb_level(s));
+  EXPECT_EQ(CRAS_STREAM_TYPE_DEFAULT, cras_rstream_get_type(s));
+  EXPECT_EQ(CRAS_STREAM_INPUT, cras_rstream_get_direction(s));
   EXPECT_NE((void *)NULL, cras_rstream_get_shm(s));
   rc = cras_rstream_get_format(s, &fmt_ret);
   EXPECT_EQ(0, rc);
