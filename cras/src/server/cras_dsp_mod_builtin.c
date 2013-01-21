894a93374c5404028aa873b904b22d5fa7daf2c3
Hsin-Yu Chao <hychao@chromium.org>
2013-01-21 21:39:44 +0800

    CRAS software dsp: Add a mix channel builtin module
    Stumpy has a mono speaker but report itself as stereo. Add
    a module with the functionality to mix two channels into one.
    
    BUG=chromium-os:37880
    TEST=Try speaker test left/right youtube, can hear both channel now.
    
    Change-Id: I5fced006a7348e0c6f6ceecf496a05806ec4c0f7
    Reviewed-on: https://gerrit.chromium.org/gerrit/41735
    Reviewed-by: Chih-Chung Chang <chihchung@chromium.org>
    Commit-Queue: Hsinyu Chao <hychao@chromium.org>
    Reviewed-by: Hsinyu Chao <hychao@chromium.org>
    Tested-by: Hsinyu Chao <hychao@chromium.org>

--- cras/src/server/cras_dsp_mod_builtin.c
+++ cras/src/server/cras_dsp_mod_builtin.c
@@ -3,26 +3,69 @@
  * found in the LICENSE file.
  */
 
+#include <stdlib.h>
+
 #include "cras_dsp_module.h"
+
 
 static int instantiate(struct dsp_module *module, unsigned long sample_rate)
 {
 	return 0;
 }
 
+static int instantiate_mix_stereo(struct dsp_module *module,
+				  unsigned long sample_rate)
+{
+	module->data = calloc(4, sizeof(float*));
+
+	return 0;
+}
+
 static void connect_port(struct dsp_module *module, unsigned long port,
 			 float *data_location) {}
+
+static void connect_port_mix_stereo(struct dsp_module *module,
+		unsigned long port, float *data_location)
+{
+	float **ports;
+	ports = (float **)module->data;
+	ports[port] = data_location;
+}
+
 static void run(struct dsp_module *module, unsigned long sample_count) {}
+
+static void run_mix_stereo(struct dsp_module *module,
+		unsigned long sample_count)
+{
+	size_t i;
+	float tmp;
+	float **ports = (float **)module->data;
+
+	for (i = 0; i < sample_count; i++) {
+		tmp = ports[0][i] + ports[1][i];
+		ports[2][i] = tmp;
+		ports[3][i] = tmp;
+	}
+}
+
 static void deinstantiate(struct dsp_module *module) {}
+
+static void deinstantiate_mix_stereo(struct dsp_module *module)
+{
+	free(module->data);
+}
+
 static int get_properties(struct dsp_module *module) { return 0; }
 static void dump(struct dsp_module *module, struct dumper *d)
 {
 	dumpf(d, "built-in module\n");
 }
+
 static void free_module(struct dsp_module *module)
 {
 	free(module);
 }
+
 struct dsp_module *cras_dsp_module_load_builtin(struct plugin *plugin)
 {
 	struct dsp_module *module;
@@ -37,5 +80,13 @@ struct dsp_module *cras_dsp_module_load_builtin(struct plugin *plugin)
 	module->free_module = &free_module;
 	module->get_properties = &get_properties;
 	module->dump = &dump;
+
+	if (strcmp(plugin->label, "mix_stereo") == 0) {
+		module->instantiate = &instantiate_mix_stereo;
+		module->run = &run_mix_stereo;
+		module->connect_port = &connect_port_mix_stereo;
+		module->deinstantiate = &deinstantiate_mix_stereo;
+	}
+
 	return module;
 }
