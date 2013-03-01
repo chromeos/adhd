6b4684c2b1961f1662e08951912cde5e7ec5aaad
Dylan Reid <dgreid@chromium.org>
2013-03-01 15:53:18 -0800

    CRAS: dbus - Use introspection for methods.
    Don't add matches for each method.  Instead use introspection to list the available methods.
    
    BUG=none
    TEST= use dbus-send to test each message:
    dbus-send --system  --type=method_call --print-reply --dest=org.chromium.cras /org/chromium/cras/Control org.freedesktop.DBus.Introspectable.Introspect
    dbus-send --system  --type=method_call --dest=org.chromium.cras /org/chromium/cras/Control org.chromium.cras.Control.SetSystemVolume byte:55
    dbus-send --system  --type=method_call --dest=org.chromium.cras /org/chromium/cras/Control org.chromium.cras.Control.SetSystemCaptureGain int32:300
    dbus-send --system  --type=method_call --dest=org.chromium.cras /org/chromium/cras/Control org.chromium.cras.Control.SetSystemCaptureMute boolean:true
    dbus-send --system  --type=method_call --dest=org.chromium.cras /org/chromium/cras/Control org.chromium.cras.Control.SetSystemMute boolean:true
    dbus-send --system --print-reply  --type=method_call --dest=org.chromium.cras /org/chromium/cras/Control org.chromium.cras.Control.GetSystemVolumeState
    
    watch signals with "dbus-monitor --system"
    
    Change-Id: Ia047818f4a59eca35ab8b9e5f0f5fe0d9102351f
    Signed-off-by: Dylan Reid <dgreid@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/44434

--- cras/src/server/cras_dbus_control.c
+++ cras/src/server/cras_dbus_control.c
@@ -16,6 +16,38 @@
 #include "cras_util.h"
 #include "utlist.h"
 
+#define CRAS_CONTROL_NAME "org.chromium.cras.Control"
+#define CRAS_CONTROL_PATH "/org/chromium/cras/Control"
+#define CONTROL_INTROSPECT_XML                                             \
+    DBUS_INTROSPECT_1_0_XML_DOCTYPE_DECL_NODE                               \
+    "<node>\n"                                                              \
+    "  <interface name=\""CRAS_CONTROL_NAME"\">\n"                      \
+    "    <method name=\"SetSystemVolume\">\n"                              \
+    "      <arg name=\"volume\" type=\"y\" direction=\"in\"/>\n"         \
+    "    </method>\n"                                                       \
+    "    <method name=\"SetSystemMute\">\n"                              \
+    "      <arg name=\"muted\" type=\"b\" direction=\"in\"/>\n"         \
+    "    </method>\n"                                                       \
+    "    <method name=\"SetSystemCaptureGain\">\n"                              \
+    "      <arg name=\"gain\" type=\"i\" direction=\"in\"/>\n"         \
+    "    </method>\n"                                                       \
+    "    <method name=\"SetSystemCaptureMute\">\n"                              \
+    "      <arg name=\"muted\" type=\"b\" direction=\"in\"/>\n"         \
+    "    </method>\n"                                                       \
+    "    <method name=\"GetSystemVolumeState\">\n"                           \
+    "      <arg name=\"volume\" type=\"y\" direction=\"out\"/>\n"   \
+    "      <arg name=\"muted\" type=\"b\" direction=\"out\"/>\n"   \
+    "      <arg name=\"capture_gain\" type=\"i\" direction=\"out\"/>\n"   \
+    "      <arg name=\"capture_mute\" type=\"b\" direction=\"out\"/>\n"   \
+    "    </method>\n"                                                       \
+    "  </interface>\n"                                                      \
+    "  <interface name=\"" DBUS_INTERFACE_INTROSPECTABLE "\">\n"          \
+    "    <method name=\"Introspect\">\n"                                    \
+    "      <arg name=\"data\" type=\"s\" direction=\"out\"/>\n"             \
+    "    </method>\n"                                                       \
+    "  </interface>\n"                                                      \
+    "</node>\n"
+
 struct cras_dbus_control {
 	DBusConnection *conn;
 };
@@ -65,10 +97,6 @@ static DBusHandlerResult handle_set_system_volume(
 	int rc;
 	uint8_t new_vol;
 
-	if (!dbus_message_is_method_call(message, "org.chromium.cras",
-					 "SetSystemVolume"))
-		return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
-
 	rc = get_single_arg(message, DBUS_TYPE_BYTE, &new_vol);
 	if (rc)
 		return rc;
@@ -87,10 +115,6 @@ static DBusHandlerResult handle_set_system_mute(
 {
 	int rc;
 	dbus_bool_t new_mute;
-
-	if (!dbus_message_is_method_call(message, "org.chromium.cras",
-					 "SetSystemMute"))
-		return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
 
 	rc = get_single_arg(message, DBUS_TYPE_BOOLEAN, &new_mute);
 	if (rc)
@@ -111,10 +135,6 @@ static DBusHandlerResult handle_set_system_capture_gain(
 	int rc;
 	int32_t new_gain;
 
-	if (!dbus_message_is_method_call(message, "org.chromium.cras",
-					 "SetSystemCaptureGain"))
-		return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
-
 	rc = get_single_arg(message, DBUS_TYPE_INT32, &new_gain);
 	if (rc)
 		return rc;
@@ -133,10 +153,6 @@ static DBusHandlerResult handle_set_system_capture_mute(
 {
 	int rc;
 	dbus_bool_t new_mute;
-
-	if (!dbus_message_is_method_call(message, "org.chromium.cras",
-					 "SetSystemCaptureMute"))
-		return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
 
 	rc = get_single_arg(message, DBUS_TYPE_BOOLEAN, &new_mute);
 	if (rc)
@@ -161,10 +177,6 @@ static DBusHandlerResult handle_get_system_volume_state(
 	dbus_int32_t capture_gain;
 	dbus_bool_t capture_muted;
 
-	if (!dbus_message_is_method_call(message, "org.chromium.cras",
-					 "GetSystemVolumeState"))
-		return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
-
 	reply = dbus_message_new_method_return(message);
 
 	volume = cras_system_get_volume();
@@ -186,60 +198,67 @@ static DBusHandlerResult handle_get_system_volume_state(
 	return DBUS_HANDLER_RESULT_HANDLED;
 }
 
-/* Adds a dbus match. */
-static void add_dbus_match(const char *match,
-			   DBusHandlerResult (*handler)(DBusConnection *,
-							DBusMessage *,
-							void *))
+/* Handle incoming messages. */
+static DBusHandlerResult handle_control_message(DBusConnection *conn,
+						DBusMessage *message,
+						void *arg)
 {
-	DBusError dbus_error;
+	syslog(LOG_DEBUG, "Control message: %s %s %s",
+	       dbus_message_get_path(message),
+	       dbus_message_get_interface(message),
+	       dbus_message_get_member(message));
 
-	dbus_error_init(&dbus_error);
+	if (dbus_message_is_method_call(message,
+					DBUS_INTERFACE_INTROSPECTABLE,
+					"Introspect")) {
+		DBusMessage *reply;
+		const char *xml = CONTROL_INTROSPECT_XML;
 
-	dbus_bus_add_match(dbus_control.conn, match, &dbus_error);
-	if (dbus_error_is_set(&dbus_error))
-		goto add_match_error;
-	if (!dbus_connection_add_filter(dbus_control.conn,
-					handler,
-					NULL, NULL))
-		goto add_filter_error;
+		reply = dbus_message_new_method_return(message);
+		if (!reply)
+			return DBUS_HANDLER_RESULT_NEED_MEMORY;
+		if (!dbus_message_append_args(reply,
+					      DBUS_TYPE_STRING, &xml,
+					      DBUS_TYPE_INVALID))
+			return DBUS_HANDLER_RESULT_NEED_MEMORY;
+		if (!dbus_connection_send(conn, reply, NULL))
+			return DBUS_HANDLER_RESULT_NEED_MEMORY;
 
-	return;
+		dbus_message_unref(reply);
+		return DBUS_HANDLER_RESULT_HANDLED;
 
-add_match_error:
-	syslog(LOG_WARNING,
-	       "Couldn't setup monitoring for control messages: %s",
-	       dbus_error.message);
-	dbus_error_free(&dbus_error);
-	dbus_connection_unref(dbus_control.conn);
-	dbus_control.conn = NULL;
-	return;
+	} else if (dbus_message_is_method_call(message,
+					       CRAS_CONTROL_NAME,
+					       "SetSystemVolume")) {
+		return handle_set_system_volume(conn, message, arg);
+	} else if (dbus_message_is_method_call(message,
+					       CRAS_CONTROL_NAME,
+					       "SetSystemMute")) {
+		return handle_set_system_mute(conn, message, arg);
+	} else if (dbus_message_is_method_call(message,
+					       CRAS_CONTROL_NAME,
+					       "SetSystemCaptureGain")) {
+		return handle_set_system_capture_gain(conn, message, arg);
+	} else if (dbus_message_is_method_call(message,
+					       CRAS_CONTROL_NAME,
+					       "SetSystemCaptureMute")) {
+		return handle_set_system_capture_mute(conn, message, arg);
+	} else if (dbus_message_is_method_call(message,
+					       CRAS_CONTROL_NAME,
+					       "GetSystemVolumeState")) {
+		return handle_get_system_volume_state(conn, message, arg);
+	}
 
-add_filter_error:
-	syslog(LOG_WARNING,
-	       "Couldn't setup monitoring for control messages: %s",
-	       strerror(ENOMEM));
-	dbus_connection_unref(dbus_control.conn);
-	dbus_control.conn = NULL;
-}
-
-/* Removes a match added with add_dbus_match. */
-static void remove_dbus_match(const char *match,
-			      DBusHandlerResult (*handler)(DBusConnection *,
-							   DBusMessage *,
-							   void *))
-{
-	dbus_bus_remove_match(dbus_control.conn, match, NULL);
-	dbus_connection_remove_filter(dbus_control.conn, handler, NULL);
+	return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
 }
 
 /* Creates a new DBus message, must be freed with dbus_message_unref. */
 static DBusMessage *create_dbus_message(const char *name)
 {
 	DBusMessage *msg;
-	msg = dbus_message_new_signal("/org/chromium/cras",
-				       "org.chromium.cras",
-				       name);
+	msg = dbus_message_new_signal(CRAS_CONTROL_PATH,
+				      CRAS_CONTROL_NAME,
+				      name);
 	if (!msg)
 		syslog(LOG_ERR, "Failed to create signal");
 
@@ -322,43 +341,27 @@ static void signal_capture_mute(void *arg)
 
 /* Exported Interface */
 
-#define MAX_MEMBER_LEN 64
-static const char base_match[] =
-	"type='method_call',interface='org.chromium.cras'";
-static const unsigned MAX_MATCH_LEN = ARRAY_SIZE(base_match) + MAX_MEMBER_LEN;
-
-struct match_method {
-	char member[MAX_MEMBER_LEN];
-	DBusHandlerResult (*handler)(DBusConnection *,
-				     DBusMessage *,
-				     void *);
-};
-static struct match_method match_methods[] = {
-	{"SetSystemVolume", handle_set_system_volume},
-	{"SetSystemMute", handle_set_system_mute},
-	{"SetSystemCaptureGain", handle_set_system_capture_gain},
-	{"SetSystemCaptureMute", handle_set_system_capture_mute},
-	{"GetSystemVolumeState", handle_get_system_volume_state},
-};
-
 void cras_dbus_control_start(DBusConnection *conn)
 {
-	char *match_str;
-	int i;
+	static const DBusObjectPathVTable control_vtable = {
+		.message_function = handle_control_message,
+	};
+
+	DBusError dbus_error;
 
 	dbus_control.conn = conn;
 	dbus_connection_ref(dbus_control.conn);
 
-	match_str = malloc(MAX_MATCH_LEN);
-
-	for (i = 0; i < ARRAY_SIZE(match_methods); i++) {
-		snprintf(match_str, MAX_MATCH_LEN,
-			 "%s,member='%s'", base_match,
-			 match_methods[i].member);
-		add_dbus_match(match_str, match_methods[i].handler);
+	if (!dbus_connection_register_object_path(conn,
+						  CRAS_CONTROL_PATH,
+						  &control_vtable,
+						  &dbus_error)) {
+		syslog(LOG_WARNING,
+		       "Couldn't register CRAS control: %s: %s",
+		       CRAS_CONTROL_PATH, dbus_error.message);
+		dbus_error_free(&dbus_error);
+		return;
 	}
-
-	free(match_str);
 
 	cras_system_register_volume_changed_cb(signal_volume, 0);
 	cras_system_register_mute_changed_cb(signal_mute, 0);
@@ -368,9 +371,6 @@ void cras_dbus_control_start(DBusConnection *conn)
 
 void cras_dbus_control_stop()
 {
-	char *match_str;
-	int i;
-
 	if (!dbus_control.conn)
 		return;
 
@@ -379,16 +379,8 @@ void cras_dbus_control_stop()
 	cras_system_remove_capture_gain_changed_cb(signal_capture_gain, 0);
 	cras_system_remove_capture_mute_changed_cb(signal_capture_mute, 0);
 
-	match_str = malloc(MAX_MATCH_LEN);
-
-	for (i = 0; i < ARRAY_SIZE(match_methods); i++) {
-		snprintf(match_str, MAX_MATCH_LEN,
-			 "%s,member='%s'", base_match,
-			 match_methods[i].member);
-		remove_dbus_match(match_str, match_methods[i].handler);
-	}
-
-	free(match_str);
+	dbus_connection_unregister_object_path(dbus_control.conn,
+					       CRAS_CONTROL_PATH);
 
 	dbus_connection_unref(dbus_control.conn);
 	dbus_control.conn = NULL;
