chromeos/adhd

commit 71aa1de70c395c3648ede3eec5838ac7c64646d5

CRAS: Only free dsp context when then iodev is destroyed.
We had a problem that the audio thread closes the iodev and frees the
dsp context while the main thread is still using it. Now we avoid the
problem by only freeing the dsp context when the iodev is destroyed.

BUG=chromium:189214
TEST=make check

Change-Id: Ia16ece72523866d77f78e47cf26ff1308bd75b22
Reviewed-on: https://gerrit.chromium.org/gerrit/47108
Reviewed-by: Dylan Reid <dgreid@chromium.org>
Commit-Queue: Chih-Chung Chang <chihchung@chromium.org>
Tested-by: Chih-Chung Chang <chihchung@chromium.org>

cras/src/server/cras_iodev.c
@@ -17,6 +17,8 @@
 #include "audio_thread.h"
 #include "utlist.h"
 
+static void cras_iodev_alloc_dsp(struct cras_iodev *iodev);
+
 /*
  * Exported Interface.
  */
@@ -65,7 +67,6 @@ int cras_iodev_set_format(struct cras_iodev *iodev,
 			  struct cras_audio_format *fmt)
 {
 	size_t actual_rate, actual_num_channels;
-	const char *purpose;
 
 	/* If this device isn't already using a format, try to match the one
 	 * requested in "fmt". */
@@ -97,15 +98,7 @@ int cras_iodev_set_format(struct cras_iodev *iodev,
 		iodev->format->num_channels = actual_num_channels;
 		/* TODO(dgreid) - allow other formats. */
 		iodev->format->format = SND_PCM_FORMAT_S16_LE;
-
-		if (iodev->direction == CRAS_STREAM_OUTPUT)
-			purpose = "playback";
-		else
-			purpose = "capture";
-		iodev->dsp_context = cras_dsp_context_new(actual_num_channels,
-							  actual_rate, purpose);
-		if (iodev->dsp_context)
-			cras_iodev_update_dsp(iodev);
+		cras_iodev_alloc_dsp(iodev);
 	}
 
 	*fmt = *(iodev->format);
@@ -128,6 +121,26 @@ void cras_iodev_free_format(struct cras_iodev *iodev)
 		free(iodev->format);
 		iodev->format = NULL;
 	}
+}
+
+static void cras_iodev_alloc_dsp(struct cras_iodev *iodev)
+{
+	const char *purpose;
+
+	if (iodev->direction == CRAS_STREAM_OUTPUT)
+		purpose = "playback";
+	else
+		purpose = "capture";
+
+	cras_iodev_free_dsp(iodev);
+	iodev->dsp_context = cras_dsp_context_new(iodev->format->frame_rate,
+						  iodev->format->num_channels,
+						  purpose);
+	cras_iodev_update_dsp(iodev);
+}
+
+void cras_iodev_free_dsp(struct cras_iodev *iodev)
+{
 	if (iodev->dsp_context) {
 		cras_dsp_context_free(iodev->dsp_context);
 		iodev->dsp_context = NULL;

cras/src/server/cras_a2dp_iodev.c
@@ -346,6 +346,7 @@ void a2dp_iodev_destroy(struct cras_iodev *iodev)
 	rc = cras_iodev_list_rm_output(iodev);
 	if (rc != -EBUSY) {
 		free_resources(a2dpio);
+		cras_iodev_free_dsp(iodev);
 		free(a2dpio);
 	}
 }

cras/src/server/cras_alsa_io.c
@@ -439,6 +439,7 @@ static void free_alsa_iodev_resources(struct alsa_io *aio)
 	}
 
 	free((void *)aio->dsp_name_default);
+	cras_iodev_free_dsp(&aio->base);
 	free(aio->dev);
 }
 

cras/src/server/cras_empty_iodev.c
@@ -167,5 +167,6 @@ void empty_iodev_destroy(struct cras_iodev *iodev)
 		cras_iodev_list_rm_output(iodev);
 	}
 	free(iodev->active_node);
+	cras_iodev_free_dsp(iodev);
 	free(empty_iodev);
 }

cras/src/server/cras_iodev.h
@@ -128,13 +128,19 @@ struct cras_iodev {
 int cras_iodev_set_format(struct cras_iodev *iodev,
 			  struct cras_audio_format *fmt);
 
-/* Clear the format previously set for this iodev. It also release the
- * dsp context for this iodev.
+/* Clear the format previously set for this iodev.
  *
  * Args:
  *    iodev - the iodev you want to free the format.
  */
 void cras_iodev_free_format(struct cras_iodev *iodev);
+
+/* Free the dsp context for this iodev
+ *
+ * Args:
+ *    iodev - the iodev you want to free the dsp context.
+ */
+void cras_iodev_free_dsp(struct cras_iodev *iodev);
 
 /* Adds a stream to the iodev.
  * Args:

cras/src/tests/a2dp_iodev_unittest.cc
@@ -31,6 +31,7 @@ static size_t a2dp_block_size_called;
 static size_t a2dp_queued_frames_val;
 static size_t a2dp_written_bytes_val;
 static size_t cras_iodev_free_format_called;
+static size_t cras_iodev_free_dsp_called;
 static int pcm_buf_size_val;
 static unsigned int a2dp_write_processed_bytes_val;
 
@@ -48,6 +49,7 @@ void ResetStubData() {
   a2dp_queued_frames_val = 0;
   a2dp_written_bytes_val = 0;
   cras_iodev_free_format_called = 0;
+  cras_iodev_free_dsp_called = 0;
   a2dp_write_processed_bytes_val = 0;
 
   fake_transport = reinterpret_cast<struct cras_bt_transport *>(0x123);
@@ -74,6 +76,7 @@ TEST(A2dpIoInit, InitializeA2dpIodev) {
 
   ASSERT_EQ(1, cras_iodev_list_rm_output_called);
   ASSERT_EQ(1, destroy_a2dp_called);
+  ASSERT_EQ(1, cras_iodev_free_dsp_called);
 }
 
 TEST(A2dpIoInit, InitializeFail) {
@@ -254,6 +257,11 @@ uint16_t cras_bt_transport_write_mtu(const struct cras_bt_transport *transport)
 void cras_iodev_free_format(struct cras_iodev *iodev)
 {
   cras_iodev_free_format_called++;
+}
+
+void cras_iodev_free_dsp(struct cras_iodev *iodev)
+{
+  cras_iodev_free_dsp_called++;
 }
 
 // Cras iodev

cras/src/tests/alsa_io_unittest.cc
@@ -106,6 +106,7 @@ static size_t ucm_get_dsp_name_default_called;
 static const char *ucm_get_dsp_name_default_value;
 static size_t cras_alsa_jack_get_dsp_name_called;
 static const char *cras_alsa_jack_get_dsp_name_value;
+static size_t cras_iodev_free_dsp_called;
 
 void ResetStubData() {
   cras_alsa_open_called = 0;
@@ -149,6 +150,7 @@ void ResetStubData() {
   ucm_get_dsp_name_default_value = NULL;
   cras_alsa_jack_get_dsp_name_called = 0;
   cras_alsa_jack_get_dsp_name_value = NULL;
+  cras_iodev_free_dsp_called = 0;
 }
 
 static long fake_get_dBFS(const cras_volume_curve *curve, size_t volume)
@@ -189,6 +191,7 @@ TEST(AlsaIoInit, InitializePlayback) {
   EXPECT_EQ(NULL, cras_iodev_update_dsp_name);
 
   alsa_iodev_destroy((struct cras_iodev *)aio);
+  EXPECT_EQ(1, cras_iodev_free_dsp_called);
 }
 
 TEST(AlsaIoInit, DefaultNodeInternalCard) {
@@ -1218,4 +1221,9 @@ void cras_iodev_set_active_node(struct cras_iodev *iodev,
                                 struct cras_ionode *node)
 {
   iodev->active_node = node;
+}
+
+void cras_iodev_free_dsp(struct cras_iodev *iodev)
+{
+  cras_iodev_free_dsp_called++;
 }

commit ccff36a4576d4810784f0b0ae5fe32a5fbcbe3c4

CRAS: Fix wrong arguments order in constructing dsp context.
This bug is introduced in 71aa1de70c395c3648ede3eec5838ac7c64646d5.

BUG=none
TEST=make check

Change-Id: I774b42e2126083baa5cb884c407de08cc1347327
Reviewed-on: https://gerrit.chromium.org/gerrit/57583
Reviewed-by: Dylan Reid <dgreid@chromium.org>
Commit-Queue: Chih-Chung Chang <chihchung@chromium.org>
Reviewed-by: Chih-Chung Chang <chihchung@chromium.org>
Tested-by: Chih-Chung Chang <chihchung@chromium.org>

cras/src/server/cras_dsp.c
@@ -102,12 +102,16 @@ static struct pipeline *prepare_pipeline(struct cras_dsp_context *ctx)
 	}
 
 	if (cras_dsp_pipeline_get_num_channels(pipeline) != ctx->channels) {
-		syslog(LOG_ERR, "pipeline channel count mismatch");
+		syslog(LOG_ERR, "pipeline channel count mismatch (%d vs %d)",
+		       cras_dsp_pipeline_get_num_channels(pipeline),
+		       ctx->channels);
 		goto bail;
 	}
 
 	if (cras_dsp_pipeline_get_sample_rate(pipeline) != ctx->sample_rate) {
-		syslog(LOG_ERR, "pipeline sample rate mismatch");
+		syslog(LOG_ERR, "pipeline sample rate mismatch (%d vs %d)",
+		       cras_dsp_pipeline_get_sample_rate(pipeline),
+		       ctx->sample_rate);
 		goto bail;
 	}
 

cras/src/server/cras_iodev.c
@@ -133,8 +133,8 @@ static void cras_iodev_alloc_dsp(struct cras_iodev *iodev)
 		purpose = "capture";
 
 	cras_iodev_free_dsp(iodev);
-	iodev->dsp_context = cras_dsp_context_new(iodev->format->frame_rate,
-						  iodev->format->num_channels,
+	iodev->dsp_context = cras_dsp_context_new(iodev->format->num_channels,
+						  iodev->format->frame_rate,
 						  purpose);
 	cras_iodev_update_dsp(iodev);
 }

cras/src/tests/iodev_unittest.cc
@@ -19,6 +19,9 @@ static size_t notify_nodes_changed_called;
 static size_t notify_active_node_changed_called;
 static size_t notify_node_volume_called;
 static size_t notify_node_capture_gain_called;
+static int dsp_context_new_channels;
+static int dsp_context_new_sample_rate;
+static const char *dsp_context_new_purpose;
 
 void ResetStubData() {
   select_node_called = 0;
@@ -26,6 +29,9 @@ void ResetStubData() {
   notify_active_node_changed_called = 0;
   notify_node_volume_called = 0;
   notify_node_capture_gain_called = 0;
+  dsp_context_new_channels = 0;
+  dsp_context_new_sample_rate = 0;
+  dsp_context_new_purpose = NULL;
 }
 
 namespace {
@@ -199,11 +205,16 @@ TEST_F(IoDevSetFormatTestSuite, SupportedFormatSecondary) {
   fmt.format = SND_PCM_FORMAT_S16_LE;
   fmt.frame_rate = 48000;
   fmt.num_channels = 2;
+  iodev_.direction = CRAS_STREAM_OUTPUT;
+  ResetStubData();
   rc = cras_iodev_set_format(&iodev_, &fmt);
   EXPECT_EQ(0, rc);
   EXPECT_EQ(SND_PCM_FORMAT_S16_LE, fmt.format);
   EXPECT_EQ(48000, fmt.frame_rate);
   EXPECT_EQ(2, fmt.num_channels);
+  EXPECT_EQ(dsp_context_new_channels, 2);
+  EXPECT_EQ(dsp_context_new_sample_rate, 48000);
+  EXPECT_EQ(dsp_context_new_purpose, "playback");
 }
 
 TEST_F(IoDevSetFormatTestSuite, SupportedFormatPrimary) {
@@ -213,11 +224,16 @@ TEST_F(IoDevSetFormatTestSuite, SupportedFormatPrimary) {
   fmt.format = SND_PCM_FORMAT_S16_LE;
   fmt.frame_rate = 44100;
   fmt.num_channels = 2;
+  iodev_.direction = CRAS_STREAM_INPUT;
+  ResetStubData();
   rc = cras_iodev_set_format(&iodev_, &fmt);
   EXPECT_EQ(0, rc);
   EXPECT_EQ(SND_PCM_FORMAT_S16_LE, fmt.format);
   EXPECT_EQ(44100, fmt.frame_rate);
   EXPECT_EQ(2, fmt.num_channels);
+  EXPECT_EQ(dsp_context_new_channels, 2);
+  EXPECT_EQ(dsp_context_new_sample_rate, 44100);
+  EXPECT_EQ(dsp_context_new_purpose, "capture");
 }
 
 TEST_F(IoDevSetFormatTestSuite, SupportedFormatDivisor) {
@@ -445,6 +461,9 @@ void cras_system_state_stream_removed() {
 struct cras_dsp_context *cras_dsp_context_new(int channels, int sample_rate,
                                               const char *purpose)
 {
+  dsp_context_new_channels = channels;
+  dsp_context_new_sample_rate = sample_rate;
+  dsp_context_new_purpose = purpose;
   return NULL;
 }
 