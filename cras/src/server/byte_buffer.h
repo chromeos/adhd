b1999952152674d417acde015e27c02f394d775c
Dylan Reid <dgreid@chromium.org>
2014-07-06 21:29:26 -0700

    CRAS: Add a byte_buffer helper.
    Circular buffers are used all over the place.  Add a common
    implementation of one.  This will be used by a2dp first, it can be
    expanded to be used in other places later.
    
    BUG=none
    TEST=need unit tests
    
    Change-Id: I3ed63a0b95e208e31b0c0bebd38e1de4e5f2355d
    Signed-off-by: Dylan Reid <dgreid@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/206615
    Reviewed-by: Hsinyu Chao <hychao@chromium.org>

---
+++ cras/src/server/byte_buffer.h
@@ -0,0 +1,86 @@
+/* Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <stdint.h>
+#include <stdlib.h>
+
+struct byte_buffer {
+	unsigned int write_idx;
+	unsigned int read_idx;
+	unsigned int level;
+	unsigned int size;
+	uint8_t bytes[];
+};
+
+/* Create a byte buffer to hold buffer_size_bytes worth of data. */
+static inline struct byte_buffer *byte_buffer_create(size_t buffer_size_bytes)
+{
+	struct byte_buffer *buf;
+	buf = calloc(1, sizeof(struct byte_buffer) + buffer_size_bytes);
+	if (!buf)
+		return buf;
+	buf->size = buffer_size_bytes;
+	return buf;
+}
+
+/* Destory a byte_buffer created with byte_buffer_create. */
+void byte_buffer_destroy(struct byte_buffer *buf)
+{
+	free(buf);
+}
+
+static inline unsigned int buf_writable_bytes(struct byte_buffer *buf)
+{
+	if (buf->level >= buf->size)
+		return 0;
+	if (buf->write_idx < buf->read_idx)
+		return buf->read_idx - buf->write_idx;
+
+	return buf->size - buf->write_idx;
+}
+
+static inline unsigned int buf_readable_bytes(struct byte_buffer *buf)
+{
+	if (buf->level == 0)
+		return 0;
+
+	if (buf->read_idx < buf->write_idx)
+		return buf->write_idx - buf->read_idx;
+
+	return buf->size - buf->read_idx;
+}
+
+static inline unsigned int buf_queued_bytes(struct byte_buffer *buf)
+{
+	return buf->level;
+}
+
+static inline uint8_t *buf_read_pointer(struct byte_buffer *buf)
+{
+	return &buf->bytes[buf->read_idx];
+}
+
+static inline void buf_increment_read(struct byte_buffer *buf, size_t inc)
+{
+	inc = MIN(inc, buf->level);
+	buf->read_idx += inc;
+	buf->read_idx %= buf->size;
+	buf->level -= inc;
+}
+
+static inline uint8_t *buf_write_pointer(struct byte_buffer *buf)
+{
+	return &buf->bytes[buf->write_idx];
+}
+
+static inline void buf_increment_write(struct byte_buffer *buf, size_t inc)
+{
+	buf->write_idx += inc;
+	buf->write_idx %= buf->size;
+	if (buf->level + inc < buf->size)
+		buf->level += inc;
+	else
+		buf->level = buf->size;
+}
