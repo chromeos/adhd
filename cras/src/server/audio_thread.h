13101b07796f8ae1388e4b7ba99ee46b2da9d3e1
Dylan Reid <dgreid@chromium.org>
2014-09-23 14:23:06 -0700

    CRAS: audio_thread - remove devs_open()
    It is no longer used.
    
    Change-Id: I32b8626eb8ca134be2ab44606e8a62d199572d6e
    Signed-off-by: Dylan Reid <dgreid@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/219971

--- cras/src/server/audio_thread.h
+++ cras/src/server/audio_thread.h
@@ -41,10 +41,8 @@ struct active_dev {
  *    main_msg_fds - Send a message from running thread to main.
  *    tid - Thread ID of the running playback/capture thread.
  *    started - Non-zero if the thread has started successfully.
- *    active_dev - Lists of active devices attached running for each
+ *    active_devs - Lists of active devices attached running for each
  *        CRAS_STREAM_DIRECTION.
- *    devs_open - Non-zero if the thread has stream attached and running
- *        for each CRAS_STREAM_DIRECTION.
  *    cb_threshold - List of callback threshold used for each
  *        CRAS_STREAM_DIRECTION, 0 means there's no stream attached for
  *        that direction.
@@ -58,7 +56,6 @@ struct audio_thread {
 	pthread_t tid;
 	int started;
 	struct active_dev *active_devs[CRAS_NUM_DIRECTIONS];
-	int devs_open[CRAS_NUM_DIRECTIONS];
 	unsigned int cb_threshold[CRAS_NUM_DIRECTIONS];
 	unsigned int buffer_frames[CRAS_NUM_DIRECTIONS];
 };
