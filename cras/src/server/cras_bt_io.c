f43ea02b12cb9a694b6e06a41b488b1af641aafb
Hsin-Yu Chao <hychao@chromium.org>
2015-01-29 17:21:58 +0800

    CRAS: bt_io - Free old format when open fail
    When a bt_io first added to active device list but failed to
    be open to stream audio, its ext_format and format members
    remains non-null. Later when this bt_io switches profile and
    about to open again, the non-null format prevents the
    update_supported_format callback be called to configure the
    profile specific iodev, and cause crash.
    
    BUG=chromium:451379
    TEST=verify in auron_yuna, see no CRAS crash by the steps below:
    1. Open Youtube to stream audio
    2. connect DUT to a HFP/HSP only headset
    3. connect DUT to the other bluetooth headset(A2DP + HFP/HSP)
    
    Change-Id: Ic47b2ff34dfa452b93aaf8566e65f820715a1e6b
    Reviewed-on: https://chromium-review.googlesource.com/244254
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Commit-Queue: Hsinyu Chao <hychao@chromium.org>
    Tested-by: Hsinyu Chao <hychao@chromium.org>

--- cras/src/server/cras_bt_io.c
+++ cras/src/server/cras_bt_io.c
@@ -177,8 +177,12 @@ static int open_dev(struct cras_iodev *iodev)
 	*dev->format = *iodev->format;
 
 	rc = dev->open_dev(dev);
-	if (rc)
+	if (rc) {
+		/* Free format here to assure the update_supported_format
+		 * callback will be called before any future open_dev call. */
+		cras_iodev_free_format(iodev);
 		return rc;
+	}
 
 	iodev->buffer_size = dev->buffer_size;
 	iodev->min_buffer_level = dev->min_buffer_level;
