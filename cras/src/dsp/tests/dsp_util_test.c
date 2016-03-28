9b34d25b3f267b11285891091ffca8caba6a8203
Frank Barchard <fbarchard@google.com>
2016-03-28 11:41:28 -0700

    Add dsp_util_test.c stand alone test for dsp_util
    This standalone test app calls dsp_util_interleave and
    dsp_util_deinterleave and compares the results to a reference
    C version.
    
    dsp_util_interleave converts float to int with rounding and
    tests special edge case values outside the normal range, and
    particular rounding cases.
    
    BUG=None
    TEST=test runs locally with expected results.
    
    Change-Id: I527c0bb5169b84e35913af496fa77616694eaf6c
    Reviewed-on: https://chromium-review.googlesource.com/335333
    Commit-Ready: Frank Barchard <fbarchard@chromium.org>
    Tested-by: Frank Barchard <fbarchard@chromium.org>
    Reviewed-by: Frank Barchard <fbarchard@chromium.org>

---
+++ cras/src/dsp/tests/dsp_util_test.c
@@ -0,0 +1,222 @@
+/* Copyright 2016 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <math.h>  /* for abs() */
+#include <stdio.h>  /* for printf() */
+#include <string.h> /* for memset() */
+#include <stdint.h> /* for uint64 definition */
+#include <stdlib.h> /* for exit() definition */
+#include <time.h> /* for clock_gettime */
+
+#include "../dsp_util.h"
+
+// Constant for converting time to milliseconds.
+#define BILLION 1000000000LL
+// Number of iterations for performance testing.
+#define ITERATIONS 40000
+
+void dsp_util_deinterleave_reference(int16_t *input, float *const *output,
+				     int channels, int frames)
+{
+	float *output_ptr[channels];
+	int i, j;
+
+	for (i = 0; i < channels; i++)
+		output_ptr[i] = output[i];
+
+	for (i = 0; i < frames; i++)
+		for (j = 0; j < channels; j++)
+			*(output_ptr[j]++) = *input++ / 32768.0f;
+}
+
+void dsp_util_interleave_reference(float *const *input, int16_t *output,
+				   int channels, int frames)
+{
+	float *input_ptr[channels];
+	int i, j;
+
+	for (i = 0; i < channels; i++)
+		input_ptr[i] = input[i];
+
+	for (i = 0; i < frames; i++)
+		for (j = 0; j < channels; j++) {
+			int16_t i16;
+			float f = *(input_ptr[j]++) * 32768.0f;
+			if (f > 32767)
+				i16 = 32767;
+			else if (f < -32768)
+				i16 = -32768;
+			else
+				i16 = (int16_t) (f > 0 ? f + 0.5f : f - 0.5f);
+			*output++ = i16;
+		}
+}
+
+// Use fixed size allocation to avoid performance fluctuation of allocation.
+#define MAXSAMPLES (65536)
+// PAD buffer to check for overflows.
+#define PAD 4096
+
+void TestRounding(float in, int16_t expected, int samples)
+{
+	int i;
+	int max_diff;
+	int d;
+
+	short* in_shorts = (short*) malloc(MAXSAMPLES * 2 * 2 + PAD);
+	float* out_floats_left_c = (float*) malloc(MAXSAMPLES * 4 + PAD);
+	float* out_floats_right_c = (float*) malloc(MAXSAMPLES * 4 + PAD);
+	float* out_floats_left_opt = (float*) malloc(MAXSAMPLES * 4 + PAD);
+	float* out_floats_right_opt = (float*) malloc(MAXSAMPLES * 4 + PAD);
+	short* out_shorts_c = (short*) malloc(MAXSAMPLES * 2 * 2 + PAD);
+	short* out_shorts_opt = (short*) malloc(MAXSAMPLES * 2 * 2 + PAD);
+
+	memset(in_shorts, 0xfb, MAXSAMPLES * 2 * 2 + PAD);
+	memset(out_floats_left_c, 0xfb, MAXSAMPLES * 4 + PAD);
+	memset(out_floats_right_c, 0xfb, MAXSAMPLES * 4 + PAD);
+	memset(out_floats_left_opt, 0xfb, MAXSAMPLES * 4 + PAD);
+	memset(out_floats_right_opt, 0xfb, MAXSAMPLES * 4 + PAD);
+	memset(out_shorts_c, 0xfb, MAXSAMPLES * 2 * 2 + PAD);
+	memset(out_shorts_opt, 0xfb, MAXSAMPLES * 2 * 2 + PAD);
+
+	float *out_floats_ptr_c[2];
+	float *out_floats_ptr_opt[2];
+
+	out_floats_ptr_c[0] = out_floats_left_c;
+	out_floats_ptr_c[1] = out_floats_right_c;
+	out_floats_ptr_opt[0] = out_floats_left_opt;
+	out_floats_ptr_opt[1] = out_floats_right_opt;
+
+	for (i = 0; i < MAXSAMPLES; ++i) {
+		out_floats_left_c[i] = in;
+		out_floats_right_c[i] = in;
+	}
+
+	/*  referenceinal C interleave */
+	dsp_util_interleave_reference(out_floats_ptr_c, out_shorts_c, 2,
+				      samples);
+
+	/* measure optimized interleave */
+	for (i = 0; i < ITERATIONS; ++i) {
+		dsp_util_interleave(out_floats_ptr_c, out_shorts_opt, 2,
+				    samples);
+	}
+
+	max_diff = 0;
+	for (i = 0; i < (MAXSAMPLES * 2 + PAD / 2); ++i) {
+		d = abs(out_shorts_c[i] - out_shorts_opt[i]);
+		if (d > max_diff) {
+			max_diff = d;
+		}
+	}
+	printf("test interleave compare %6d, %10f %13f %6d %6d %6d %s\n",
+		max_diff, in, in * 32768.0f, out_shorts_c[0], out_shorts_opt[0],
+		expected,
+		max_diff == 0 ? "PASS" : (out_shorts_opt[0] == expected ?
+		"EXPECTED DIFFERENCE" : "UNEXPECTED DIFFERENCE"));
+
+	/* measure referenceinal C deinterleave */
+	dsp_util_deinterleave_reference(in_shorts, out_floats_ptr_c, 2,
+					samples);
+
+	/* measure optimized deinterleave */
+	dsp_util_deinterleave(in_shorts, out_floats_ptr_opt, 2, samples);
+
+	d = memcmp(out_floats_ptr_c[0], out_floats_ptr_opt[0], samples * 4);
+	if (d) printf("left compare %d, %f %f\n", d, out_floats_ptr_c[0][0],
+		      out_floats_ptr_opt[0][0]);
+	d = memcmp(out_floats_ptr_c[1], out_floats_ptr_opt[1], samples * 4);
+	if (d) printf("right compare %d, %f %f\n", d, out_floats_ptr_c[1][0],
+			out_floats_ptr_opt[1][0]);
+
+	free(in_shorts);
+	free(out_floats_left_c);
+	free(out_floats_right_c);
+	free(out_floats_left_opt);
+	free(out_floats_right_opt);
+	free(out_shorts_c);
+	free(out_shorts_opt);
+}
+
+int main(int argc, char **argv)
+{
+	float e = 0.000000001f;
+	int samples = 16;
+
+	dsp_enable_flush_denormal_to_zero();
+
+	// test clamping
+	TestRounding(1.0f, 32767, samples);
+	TestRounding(-1.0f, -32768, samples);
+	TestRounding(1.1f, 32767, samples);
+	TestRounding(-1.1f, -32768, samples);
+	TestRounding(2000000000.f / 32768.f, 32767, samples);
+	TestRounding(-2000000000.f / 32768.f, -32768, samples);
+	TestRounding(5000000000.f / 32768.f, 32767, samples);
+	TestRounding(-5000000000.f / 32768.f, -32768, samples);
+
+	// test infinity
+	int inf_int = 0x7f7fffff;;
+	float inf = *(float *)&inf_int;
+	TestRounding(inf, 32767, samples);  // expect fail
+	inf_int = 0xff7fffff;;
+	inf = *(float *)&inf_int;
+	TestRounding(inf, -32768, samples);  // expect fail
+
+	// test rounding
+	TestRounding(0.25f, 8192, samples);
+	TestRounding(-0.25f, -8192, samples);
+	TestRounding(0.50f, 16384, samples);
+	TestRounding(-0.50f, -16384, samples);
+	TestRounding(1.0f / 32768.0f, 1, samples);
+	TestRounding(-1.0f / 32768.0f, -1, samples);
+	TestRounding(1.0f / 32768.0f + e, 1, samples);
+	TestRounding(-1.0f / 32768.0f - e, -1, samples);
+	TestRounding(1.0f / 32768.0f - e, 1, samples);
+	TestRounding(-1.0f / 32768.0f + e, -1, samples);
+
+	// Rounding on 'tie' is not consistent at this time.
+#if defined(__i386__) || defined(__x86_64__)
+	TestRounding(0.5f / 32768.0f, 0, samples);  // expect round to even
+	TestRounding(-0.5f / 32768.0f, 0, samples);
+#else
+	TestRounding(0.5f / 32768.0f, 1, samples);  // expect round away
+	TestRounding(-0.5f / 32768.0f, -1, samples);
+#endif
+
+	TestRounding(0.5f / 32768.0f + e, 1, samples);
+	TestRounding(-0.5f / 32768.0f - e, 1, samples);
+	TestRounding(0.5f / 32768.0f - e, 0, samples);
+	TestRounding(-0.5f / 32768.0f + e, 0, samples);
+
+	TestRounding(1.5f / 32768.0f, 2, samples);  // expect fail?
+	TestRounding(-1.5f / 32768.0f, -2, samples);  // expect fail?
+	TestRounding(1.5f / 32768.0f + e, 2, samples);
+	TestRounding(-1.5f / 32768.0f - e, -2, samples);
+	TestRounding(1.5f / 32768.0f - e, 1, samples);
+	TestRounding(-1.5f / 32768.0f + e, -1, samples);
+
+	// test denormals
+	int denorm_int = 1;
+	float denorm = *(float *)&denorm_int;
+	TestRounding(denorm, 0, samples);
+	denorm_int = 0x80000001;
+	denorm = *(float *)&denorm_int;
+	TestRounding(denorm, 0, samples);
+
+	// test nans. caveat Results vary by implementation.
+#if defined(__i386__) || defined(__x86_64__)
+#define EXPECTED_NAN_RESULT 32767
+#else
+#define EXPECTED_NAN_RESULT 0
+#endif
+	unsigned int nan_int = 0x7f800001;
+	float nan = *(float *)&nan_int;
+	TestRounding(nan, EXPECTED_NAN_RESULT, samples);
+	nan_int = 0xff800001;
+	nan = *(float *)&nan_int;
+	TestRounding(nan, EXPECTED_NAN_RESULT, samples);
+	return 0;
+}
