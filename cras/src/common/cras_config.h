ec89399871571297c854a0a1fbdd6e0d4385b450
John Muir <muirj@google.com>
2016-04-04 14:06:52 -0700

    CRAS: Make the CRAS_CONFIG_FILE_DIR configurable.
    Now it is defined by the configure script as ${sysconfdir}/cras
    which is normally /etc/cras. Required in order to enable CRAS
    on Brillo.
    
    BUG=None
    TEST=Build passes with default configuration.
         Build passes on Brillo with custom CRAS_CONFIG_FILE_DIR.
         CRAS executes on Brillo.
    
    Change-Id: Ie467fb5a7526761bb3f127800e50f7ff1fe6efa5
    Reviewed-on: https://chromium-review.googlesource.com/339404
    Commit-Ready: John Muir <muirj@google.com>
    Tested-by: John Muir <muirj@google.com>
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>

--- cras/src/common/cras_config.h
+++ cras/src/common/cras_config.h
@@ -12,7 +12,9 @@
 #define CRAS_CLIENT_RT_THREAD_PRIORITY 10
 #define CRAS_CLIENT_NICENESS_LEVEL -10
 #define CRAS_SOCKET_FILE ".cras_socket"
-#define CRAS_CONFIG_FILE_DIR "/etc/cras"
+
+/* CRAS_CONFIG_FILE_DIR is defined as $sysconfdir/cras by the configure
+   script. */
 
 /* Gets the path to save UDS socket files. */
 const char *cras_config_get_system_socket_file_dir();
