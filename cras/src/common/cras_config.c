8d225aa3ef953f235d788509a0ba23b1f6504a5d
John Muir <muirj@google.com>
2016-04-04 17:01:17 -0700

    CRAS: Make socket directory configurable.
    /var doesn't exist on Brillo, nor does /system/var, so we need
    to be able to specify a directory.
    
    BUG=None
    TEST=Build passes with default configuration.
         Build passes on Brillo with custom config.
         CRAS executes on Brillo.
    
    Change-Id: I51827695c78d093372ad53802fee8421ef3ae1e0
    Reviewed-on: https://chromium-review.googlesource.com/339405
    Commit-Ready: John Muir <muirj@google.com>
    Tested-by: John Muir <muirj@google.com>
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>

--- cras/src/common/cras_config.c
+++ cras/src/common/cras_config.c
@@ -13,5 +13,5 @@ const char *cras_config_get_system_socket_file_dir()
 	/* This directory is created by the upstart script, eventually it would
 	 * be nice to make this more dynamic, but it isn't needed right now for
 	 * Chrome OS. */
-	return "/var/run/cras";
+	return CRAS_SOCKET_FILE_DIR;
 }
