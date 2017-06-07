a0aab94fc9aaacabb198c01d560d29ac19a89900
Jorge Lucangeli Obes <jorgelo@chromium.org>
2017-06-07 10:47:50 -0400

    Set NoNewPrivs for cras.
    Also fix some typos and grammar in the shell script.
    
    BUG=chromium:730623
    TEST=Audio still works.
    TEST=security_SandboxedServices.
    
    Change-Id: Ifaed8c90df299711d5982b244893da754cf8d757
    Reviewed-on: https://chromium-review.googlesource.com/527193
    Commit-Ready: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Tested-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- init/cras.sh
+++ init/cras.sh
@@ -3,14 +3,14 @@
 # found in the LICENSE file.
 
 # Disable HSP/HFP on Google WiFi (Gale) with UART-HCI Bluetooth
-# which is incapable to handle SCO audio.
+# which is incapable of handling SCO audio.
 platform_name="$(mosys platform name)"
 if [ "$platform_name" = "Gale" ]; then
      DISABLE_PROFILE="--disable_profile=hfp,hsp"
 fi
-# For board needs different device configs, check which config
+# For boards that need a different device config, check which config
 # directory to use. Use that directory for both volume curves
-# and dsp config.
+# and DSP config.
 if [ -f /etc/cras/get_device_config_dir ]; then
   device_config_dir="$(sh /etc/cras/get_device_config_dir)"
   DEVICE_CONFIG_DIR="--device_config_dir=${device_config_dir}"
@@ -20,6 +20,6 @@ if [ -f /etc/cras/get_internal_ucm_suffix ]; then
   internal_ucm_suffix="$(sh /etc/cras/get_internal_ucm_suffix)"
   INTERNAL_UCM_SUFFIX="--internal_ucm_suffix=${internal_ucm_suffix}"
 fi
-exec minijail0 -u cras -g cras -G -- /usr/bin/cras \
+exec minijail0 -u cras -g cras -G -n -- /usr/bin/cras \
     ${DSP_CONFIG} ${DEVICE_CONFIG_DIR} ${DISABLE_PROFILE} \
     ${INTERNAL_UCM_SUFFIX}
