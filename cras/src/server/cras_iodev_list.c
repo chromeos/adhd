bac3ba7dfe95a78dba34e97b5fdcfcd2210b7e68
Chinyue Chen <chinyue@google.com>
2017-08-09 12:29:18 +0800

    CRAS: Do not close dev if it still has pinned streams.
    pinned_stream_removed closes dev after a pinned stream is removed from
    that dev, but there might be multiple pinned streams connected.
    
    BUG=b:62333370
    TEST=Tested with multiple pinned streams.
    
    Change-Id: I8c028e04544091b18625a45709e3419b17845585
    Reviewed-on: https://chromium-review.googlesource.com/607753
    Commit-Ready: Chinyue Chen <chinyue@chromium.org>
    Tested-by: Chinyue Chen <chinyue@chromium.org>
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>

--- cras/src/server/cras_iodev_list.c
+++ cras/src/server/cras_iodev_list.c
@@ -696,7 +696,8 @@ static void pinned_stream_removed(struct cras_rstream *rstream)
 	struct cras_iodev *dev;
 
 	dev = find_dev(rstream->pinned_dev_idx);
-	if (!cras_iodev_list_dev_is_enabled(dev)) {
+	if (!cras_iodev_list_dev_is_enabled(dev) &&
+	    !cras_iodev_has_pinned_stream(dev)) {
 		close_dev(dev);
 		dev->update_active_node(dev, dev->active_node->idx, 0);
 	}
