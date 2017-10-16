22b66868630394e23ccdb0025c8c646d957f3a1b
Simon Glass <sjg@chromium.org>
2017-10-16 07:51:55 -0600

    Add support for unified builds
    With unified builds we should obtain any needed configuration from the
    master configuration. Update the init script to do this.
    
    BUG=chromium:773898
    TEST=manual on pyro, electro (not done yet)
    Need to test on eve / other boards to check non-unibuild
    
    Change-Id: I9fd31ab3eb0ea69abb732c7930e8e29f125e9675
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/721062
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- init/cras.sh
+++ init/cras.sh
@@ -2,24 +2,37 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Disable HSP/HFP on Google WiFi (Gale) with UART-HCI Bluetooth
-# which is incapable of handling SCO audio.
-platform_name="$(mosys platform name)"
-if [ "$platform_name" = "Gale" ]; then
-     DISABLE_PROFILE="--disable_profile=hfp,hsp"
+# Unified build config.
+device_config_dir="$(cros_config /audio/main cras-config-dir)"
+internal_ucm_suffix="$(cros_config /audio/main ucm-suffix)"
+
+# Handle legacy config.
+if [ -z "${device_config_dir}" ]; then
+  # Disable HSP/HFP on Google WiFi (Gale) with UART-HCI Bluetooth
+  # which is incapable of handling SCO audio.
+  platform_name="$(mosys platform name)"
+  if [ "$platform_name" = "Gale" ]; then
+      DISABLE_PROFILE="--disable_profile=hfp,hsp"
+  fi
+  # For boards that need a different device config, check which config
+  # directory to use. Use that directory for both volume curves
+  # and DSP config.
+  if [ -f /etc/cras/get_device_config_dir ]; then
+    device_config_dir="$(sh /etc/cras/get_device_config_dir)"
+  fi
+  if [ -f /etc/cras/get_internal_ucm_suffix ]; then
+    internal_ucm_suffix="$(sh /etc/cras/get_internal_ucm_suffix)"
+  fi
 fi
-# For boards that need a different device config, check which config
-# directory to use. Use that directory for both volume curves
-# and DSP config.
-if [ -f /etc/cras/get_device_config_dir ]; then
-  device_config_dir="$(sh /etc/cras/get_device_config_dir)"
+
+if [ -n "${device_config_dir}" ]; then
   DEVICE_CONFIG_DIR="--device_config_dir=${device_config_dir}"
   DSP_CONFIG="--dsp_config=${device_config_dir}/dsp.ini"
 fi
-if [ -f /etc/cras/get_internal_ucm_suffix ]; then
-  internal_ucm_suffix="$(sh /etc/cras/get_internal_ucm_suffix)"
+if [ -n "${internal_ucm_suffix}" ]; then
   INTERNAL_UCM_SUFFIX="--internal_ucm_suffix=${internal_ucm_suffix}"
 fi
+
 exec minijail0 -u cras -g cras -G -n -- /usr/bin/cras \
     ${DSP_CONFIG} ${DEVICE_CONFIG_DIR} ${DISABLE_PROFILE} \
     ${INTERNAL_UCM_SUFFIX}
