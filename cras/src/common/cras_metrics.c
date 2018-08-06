07f66e26c577c8afef14f9a058a88b4f0b616791
Hsin-Yu Chao <hychao@chromium.org>
2018-08-06 17:55:19 +0800

    CRAS: metrics - Use libmetrics instead of calling metrics_client
    This change simplifies the seccomp policy file, by reducing
    execve and a few other system calls.
    
    BUG=chromium:871668
    TEST=emerge adhd with chromeos-base/metrics side change,
    verify events and samples can be sent to UMA.
    CQ-DEPEND=CL:1163643,CL:1163644
    
    Change-Id: I28290de0b9f670cdc5ad739823490061c0545f9a
    Reviewed-on: https://chromium-review.googlesource.com/1163645
    Commit-Ready: Hsinyu Chao <hychao@chromium.org>
    Tested-by: Hsinyu Chao <hychao@chromium.org>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- cras/src/common/cras_metrics.c
+++ cras/src/common/cras_metrics.c
@@ -7,30 +7,35 @@
 #include <syslog.h>
 #include <unistd.h>
 
-#define METRICS_CLIENT "metrics_client"
+#ifdef HAVE_LIB_METRICS
+#include <metrics/c_metrics_library.h>
 
 void cras_metrics_log_event(const char *event)
 {
-	syslog(LOG_DEBUG, "Log event: %s", event);
-	if (!fork()) {
-		const char *argv[] = {METRICS_CLIENT, "-v", event, NULL} ;
-		execvp(argv[0], (char * const *)argv);
-		_exit(1);
-	}
+	CMetricsLibrary handle;
+
+	syslog(LOG_DEBUG, "UMA event: %s", event);
+	handle = CMetricsLibraryNew();
+	CMetricsLibrarySendCrosEventToUMA(handle, event);
+	CMetricsLibraryDelete(handle);
 }
 
 void cras_metrics_log_histogram(const char *name, int sample, int min,
 				int max, int nbuckets)
 {
-	if (!fork()) {
-		char tmp[4][16];
-		snprintf(tmp[0], 16, "%d", sample);
-		snprintf(tmp[1], 16, "%d", min);
-		snprintf(tmp[2], 16, "%d", max);
-		snprintf(tmp[3], 16, "%d", nbuckets);
-		const char *argv[] = {METRICS_CLIENT, name, tmp[0], tmp[1],
-				      tmp[2], tmp[3], NULL};
-		execvp(argv[0], (char * const *)argv);
-		_exit(1);
-	}
+	CMetricsLibrary handle;
+
+	syslog(LOG_DEBUG, "UMA name: %s", name);
+	handle = CMetricsLibraryNew();
+	CMetricsLibrarySendToUMA(handle, name, sample, min, max, nbuckets);
+	CMetricsLibraryDelete(handle);
 }
+#else
+void cras_metrics_log_event(const char *event)
+{
+}
+void cras_metrics_log_histogram(const char *name, int sample, int min,
+				int max, int nbuckets)
+{
+}
+#endif
