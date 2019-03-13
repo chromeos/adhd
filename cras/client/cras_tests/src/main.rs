8992b6a4afb3baafc94a46eece546ff781c6ad14
paulhsia <paulhsia@chromium.org>
2019-03-13 16:44:01 +0800

    CRAS: cras_tests: Use getopts crate for input args
    BUG=None
    TEST=Run commands
         $ cras_tests playback -f /path/to/playback/file
         $ cras_tests playback -h
         $ cras_tests help
    TEST=Run invalid command
    CQ-DEPEND=CL:1525643
    
    Change-Id: Ifd6f9f4e61d59c3599966b36b7bf55cc0560ab81
    Reviewed-on: https://chromium-review.googlesource.com/1521086
    Commit-Ready: Chih-Yang Hsia <paulhsia@chromium.org>
    Tested-by: Chih-Yang Hsia <paulhsia@chromium.org>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- cras/client/cras_tests/src/main.rs
+++ cras/client/cras_tests/src/main.rs
@@ -1,46 +1,104 @@
 // Copyright 2019 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-use std::env;
 use std::fs::File;
 use std::io::{Read, Write};
 use std::thread::spawn;
 type Result<T> = std::result::Result<T, Box<std::error::Error>>;
 
+use getopts::Options;
+
 use audio_streams::StreamSource;
 use libcras::CrasClient;
 
-const BUFFER_SIZE: usize = 256;
-const FRAME_RATE: usize = 44100;
-const NUM_CHANNELS: usize = 2;
+fn show_subcommand_usage(program_name: &str, subcommand: &str, opts: &Options) {
+    let brief = format!("Usage: {} {} [options]", program_name, subcommand);
+    print!("{}", opts.usage(&brief));
+}
+
+fn playback(args: &[String]) -> Result<()> {
+    let mut opts = Options::new();
+    opts.optopt("b", "buffer_size", "Buffer size in frames", "SIZE")
+        .optopt("c", "", "Number of channels", "NUM")
+        .optopt("f", "file", "Path to playback file", "FILE")
+        .optopt("r", "rate", "Audio frame rate (Hz)", "RATE")
+        .optflag("h", "help", "Print help message");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(e) => {
+            show_subcommand_usage(&args[0], &args[1], &opts);
+            return Err(Box::new(e));
+        }
+    };
+    if matches.opt_present("h") {
+        show_subcommand_usage(&args[0], &args[1], &opts);
+        return Ok(());
+    }
+    let file_name = match matches.opt_str("f") {
+        None => {
+            println!("Must input playback file name.");
+            show_subcommand_usage(&args[0], &args[1], &opts);
+            return Ok(());
+        }
+        Some(file_name) => file_name,
+    };
+    let buffer_size = matches.opt_get_default::<usize>("b", 256)?;
+    let num_channels = matches.opt_get_default::<usize>("c", 2)?;
+    let frame_rate = matches.opt_get_default::<usize>("r", 48000)?;
+    let mut cras_client = CrasClient::new()?;
+    let (_control, mut stream) =
+        cras_client.new_playback_stream(num_channels, frame_rate, buffer_size)?;
+
+    let mut file = File::open(&file_name).unwrap();
+    let thread = spawn(move || {
+        // Play samples from a file
+        let mut local_buffer = vec![0u8; buffer_size * num_channels * 2];
+        loop {
+            // Reads data to local buffer
+            let read_count = file.read(&mut local_buffer).unwrap();
+            if read_count == 0 {
+                break;
+            }
+            // Gets writable buffer from stream and
+            let mut buffer = stream.next_playback_buffer().unwrap();
+            // Writes data to stream buffer
+            let _write_frames = buffer.write(&local_buffer[..read_count]).unwrap();
+        }
+    });
+    thread.join().expect("Failed to join playback thread");
+    // Stream and client should gracefully be closed out of this scope
+
+    Ok(())
+}
+
+fn show_usage(program_name: &str) {
+    println!("Usage: {} [subcommand] <subcommand args>", program_name);
+    println!("\nSubcommands:\n");
+    println!("playback - Test playback function");
+    println!("\nhelp - Print help message");
+}
 
 fn main() -> Result<()> {
-    let args: Vec<String> = env::args().collect();
-    match args.len() {
-        2 => {
-            let mut cras_client = CrasClient::new()?;
-            let (_control, mut stream) =
-                cras_client.new_playback_stream(NUM_CHANNELS, FRAME_RATE, BUFFER_SIZE)?;
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("Must specify a subcommand.");
+        show_usage(&args[0]);
+        return Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "No subcommand",
+        )));
+    }
 
-            let thread = spawn(move || {
-                // Play 1000 * BUFFER_SIZE samples from a file
-                let mut file = File::open(&args[1]).unwrap();
-                let mut local_buffer = [0u8; BUFFER_SIZE * NUM_CHANNELS * 2];
-                for _i in 0..1000 {
-                    // Reads data to local buffer
-                    let _read_count = file.read(&mut local_buffer).unwrap();
-
-                    // Gets writable buffer from stream and
-                    let mut buffer = stream.next_playback_buffer().unwrap();
-                    // Writes data to stream buffer
-                    let _write_frames = buffer.write(&local_buffer).unwrap();
-                }
-            });
-            thread.join().expect("Failed to join playback thread");
-            // Stream and client should gracefully be closed out of this scope
-        }
-        _ => {
-            println!("cras_tests /path/to/playback_file.raw");
+    match args[1].as_ref() {
+        "playback" => playback(&args)?,
+        "help" => show_usage(&args[0]),
+        subcommand => {
+            println!("Subcommand \"{}\" does not exist.", subcommand);
+            show_usage(&args[0]);
+            return Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "Subcommand does not exist",
+            )));
         }
     };
     Ok(())
