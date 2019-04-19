chromeos/adhd

commit 2918356d57a068b6e0ddc9321f57e8d460589129

CRAS: stream_list - Check if device is pinned by a stream
When main thread wants to know if there's stream that pins
to certain device, we should not check that by iterating
all dev_streams because it's controlled by audio thread.

In future commit we'll restrict only audio thread can access
dev_stream. So fix the pin stream check by query stream_list
instead of iterating all dev_streams.

BUG=chromium:946356
TEST=unittest

Change-Id: Id2c7c751581603132c77815d904ceec98ab85cbd
Reviewed-on: https://chromium-review.googlesource.com/1433418
Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
Tested-by: Hsinyu Chao <hychao@chromium.org>
Reviewed-by: Hsinyu Chao <hychao@chromium.org>

cras/src/server/cras_iodev_list.c
@@ -425,7 +425,7 @@ static int close_dev_without_idle_check(struct cras_iodev *dev)
 {
 	if (!cras_iodev_is_open(dev))
 	       return -EINVAL;
-	if (cras_iodev_has_pinned_stream(dev))
+	if (stream_list_has_pinned_stream(stream_list, dev->info.idx))
 		syslog(LOG_ERR, "Closing device with pinned streams.");
 
 	remove_all_streams_from_dev(dev);
@@ -871,7 +871,8 @@ static int possibly_close_enabled_devs(enum CRAS_STREAM_DIRECTION dir)
 	/* No more default streams, close any device that doesn't have a stream
 	 * pinned to it. */
 	DL_FOREACH(enabled_devs[dir], edev) {
-		if (cras_iodev_has_pinned_stream(edev->dev))
+		if (stream_list_has_pinned_stream(stream_list,
+						  edev->dev->info.idx))
 			continue;
 		if (dir == CRAS_STREAM_INPUT) {
 			close_dev(edev->dev);
@@ -894,7 +895,7 @@ static void pinned_stream_removed(struct cras_rstream *rstream)
 	if (!dev)
 		return;
 	if (!cras_iodev_list_dev_is_enabled(dev) &&
-	    !cras_iodev_has_pinned_stream(dev))
+	    !stream_list_has_pinned_stream(stream_list, dev->info.idx))
 		close_pinned_device(dev);
 }
 
@@ -976,7 +977,7 @@ static int disable_device(struct enabled_dev *edev, bool force)
 			continue;
 		audio_thread_disconnect_stream(audio_thread, stream, dev);
 	}
-	if (cras_iodev_has_pinned_stream(dev))
+	if (stream_list_has_pinned_stream(stream_list, dev->info.idx))
 		return 0;
 	DL_FOREACH(device_enable_cbs, callback)
 		callback->disabled_cb(dev, callback->cb_data);
@@ -1007,7 +1008,7 @@ static int force_close_pinned_only_device(struct cras_iodev *dev)
 			continue;
 		audio_thread_disconnect_stream(audio_thread, rstream, dev);
 	}
-	if (cras_iodev_has_pinned_stream(dev))
+	if (stream_list_has_pinned_stream(stream_list, dev->info.idx))
 		return -EEXIST;
 	close_dev(dev);
 	dev->update_active_node(dev, dev->active_node->idx, 0);

cras/src/server/cras_iodev.c
@@ -1612,16 +1612,6 @@ int cras_iodev_set_mute(struct cras_iodev* iodev)
 	return 0;
 }
 
-int cras_iodev_has_pinned_stream(const struct cras_iodev *dev)
-{
-	const struct dev_stream *out;
-	DL_FOREACH(dev->streams, out) {
-		if (out->stream->is_pinned)
-			return 1;
-	}
-	return 0;
-}
-
 void cras_iodev_update_highest_hw_level(struct cras_iodev *iodev,
 		unsigned int hw_level)
 {

cras/src/server/cras_iodev.h
@@ -784,15 +784,6 @@ int cras_iodev_set_mute(struct cras_iodev* iodev);
 int cras_iodev_is_zero_volume(const struct cras_iodev *odev);
 
 /*
- * Checks if a iodev has pinned stream attached to it.
- * Args:
- *    dev[in] - The device.
- * Returns:
- *    1 if device has pinned stream. 0 otherwise.
- */
-int cras_iodev_has_pinned_stream(const struct cras_iodev *dev);
-
-/*
  * Updates the highest hardware level of the device.
  * Args:
  *    iodev - The device.

cras/src/server/stream_list.c
@@ -134,3 +134,15 @@ int stream_list_rm_all_client_streams(struct stream_list *list,
 
 }
 
+bool stream_list_has_pinned_stream(struct stream_list *list,
+				   unsigned int dev_idx)
+{
+	struct cras_rstream *rstream;
+	DL_FOREACH(list->streams, rstream) {
+		if (!rstream->is_pinned)
+			continue;
+		if (rstream->pinned_dev_idx == dev_idx)
+			return true;
+	}
+	return false;
+}

cras/src/server/stream_list.h
@@ -3,6 +3,8 @@
  * found in the LICENSE file.
  */
 
+#include <stdbool.h>
+
 #include "cras_types.h"
 #include "utlist.h"
 
@@ -35,3 +37,9 @@ int stream_list_rm(struct stream_list *list, cras_stream_id_t id);
 
 int stream_list_rm_all_client_streams(struct stream_list *list,
 				      struct cras_rclient *rclient);
+
+/*
+ * Checks if there is a stream pinned to the given device.
+ */
+bool stream_list_has_pinned_stream(struct stream_list *list,
+				   unsigned int dev_idx);

cras/src/tests/iodev_list_unittest.cc
@@ -84,7 +84,7 @@ static struct cras_iodev *audio_thread_dev_start_ramp_dev;
 static int audio_thread_dev_start_ramp_called;
 static enum CRAS_IODEV_RAMP_REQUEST audio_thread_dev_start_ramp_req ;
 static std::map<const struct cras_iodev*, enum CRAS_IODEV_STATE> cras_iodev_state_ret;
-static std::map<const struct cras_iodev*, int> cras_iodev_has_pinned_stream_ret;
+static std::map<int, bool> stream_list_has_pinned_stream_ret;
 static struct cras_rstream *audio_thread_disconnect_stream_stream;
 static int audio_thread_disconnect_stream_called;
 static int cras_iodev_is_zero_volume_ret;
@@ -113,7 +113,7 @@ class IoDevTestSuite : public testing::Test {
       audio_thread_disconnect_stream_called = 0;
       audio_thread_disconnect_stream_stream = NULL;
       audio_thread_is_dev_open_ret = 0;
-      cras_iodev_has_pinned_stream_ret.clear();
+      stream_list_has_pinned_stream_ret.clear();
 
       sample_rates_[0] = 44100;
       sample_rates_[1] = 48000;
@@ -1412,7 +1412,7 @@ TEST_F(IoDevTestSuite, AddRemovePinnedStream) {
 
   // Select d2, check pinned stream is not added to d2.
   update_active_node_called = 0;
-  cras_iodev_has_pinned_stream_ret[&d1_] = 1;
+  stream_list_has_pinned_stream_ret[d1_.info.idx] = 1;
   cras_iodev_list_select_node(CRAS_STREAM_OUTPUT,
       cras_make_node_id(d2_.info.idx, 0));
   EXPECT_EQ(1, audio_thread_add_stream_called);
@@ -1424,7 +1424,7 @@ TEST_F(IoDevTestSuite, AddRemovePinnedStream) {
 
   // Remove pinned stream from d1, check d1 is closed after stream removed.
   update_active_node_called = 0;
-  cras_iodev_has_pinned_stream_ret[&d1_] = 0;
+  stream_list_has_pinned_stream_ret[d1_.info.idx] = 0;
   EXPECT_EQ(0, stream_rm_cb(&rstream));
   EXPECT_EQ(1, cras_iodev_close_called);
   EXPECT_EQ(&d1_, cras_iodev_close_dev);
@@ -1472,7 +1472,7 @@ TEST_F(IoDevTestSuite, SuspendResumePinnedStream) {
   // Device state enters no_stream after stream is disconnected.
   d1_.state = CRAS_IODEV_STATE_NO_STREAM_RUN;
   // Device has no pinned stream now. But this pinned stream remains in stream_list.
-  cras_iodev_has_pinned_stream_ret[&d1_] = 0;
+  stream_list_has_pinned_stream_ret[d1_.info.idx] = 0;
 
   // Suspend
   observer_ops->suspend_changed(NULL, 1);
@@ -1808,9 +1808,10 @@ enum CRAS_IODEV_STATE cras_iodev_state(const struct cras_iodev *iodev)
 	return cras_iodev_state_ret[iodev];
 }
 
-int cras_iodev_has_pinned_stream(const struct cras_iodev *dev)
+bool stream_list_has_pinned_stream(struct stream_list *list,
+                                   unsigned int dev_idx)
 {
-  return cras_iodev_has_pinned_stream_ret[dev];
+  return stream_list_has_pinned_stream_ret[dev_idx];
 }
 
 struct stream_list *stream_list_create(stream_callback *add_cb,

commit 5fdefeb10dd1935e5bf79901e845c037e9df84a2

CRAS: iodev_list - Fix crash because of pinned stream on hotplug device
stream_list_has_pinned_stream() is used to check if there's a stream
pinned to specific device.
However in server side, if we force disable one device by detaching
all streams, the following stream_list_has_pinned_stream check becomes
redundant and incorrect because client hasn't actually delete this
stream yet. Remove that check in force_close_pinned_only_device to
prevent incorrect error. Also removed redundant check in
close_dev_without_idle_check to prevent incorrect error messages.

When there's a pinned stream on a hotplug device, and that device is
unplugged, the device will incorrectly not be closed and leak memory.
This is caused by the early return at stream_list_has_pinned_stream
in disable_device. To fix this we need to honor force flag and close
device.

Note that this was caused by
2918356d CRAS: stream_list - Check if device is pinned by a stream
which changes how we implement the pinned stream check.

BUG=chromium:953636
TEST=Enable assistant, hotplug USB headset and repeatedly
unplug then re-plug it. Verify CRAS doesn't crash.

Change-Id: Iad20150b0d2404c7b32d7c14f6acd32dd73a20fe
Reviewed-on: https://chromium-review.googlesource.com/1570975
Commit-Ready: Hsinyu Chao <hychao@chromium.org>
Tested-by: Hsinyu Chao <hychao@chromium.org>
Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>

cras/src/server/cras_iodev_list.c
@@ -392,8 +392,6 @@ static int close_dev_without_idle_check(struct cras_iodev *dev)
 {
 	if (!cras_iodev_is_open(dev))
 	       return -EINVAL;
-	if (stream_list_has_pinned_stream(stream_list, dev->info.idx))
-		syslog(LOG_ERR, "Closing device with pinned streams.");
 
 	remove_all_streams_from_dev(dev);
 	dev->idle_timeout.tv_sec = 0;
@@ -944,7 +942,11 @@ static int disable_device(struct enabled_dev *edev, bool force)
 			continue;
 		audio_thread_disconnect_stream(audio_thread, stream, dev);
 	}
-	if (stream_list_has_pinned_stream(stream_list, dev->info.idx))
+	/* If this is a force disable call, that guarantees pinned streams have
+	 * all been detached. Otherwise check with stream_list to see if
+	 * there's still a pinned stream using this device.
+	 */
+	if (!force && stream_list_has_pinned_stream(stream_list, dev->info.idx))
 		return 0;
 	DL_FOREACH(device_enable_cbs, callback)
 		callback->disabled_cb(dev, callback->cb_data);
@@ -965,7 +967,10 @@ static int force_close_pinned_only_device(struct cras_iodev *dev)
 {
 	struct cras_rstream *rstream;
 
-	/* Pull pinned streams off this device. */
+	/* Pull pinned streams off this device. Note that this is initiated
+	 * from server side, so the pin stream still exist in stream_list
+	 * pending client side to actually remove it.
+	 */
 	DL_FOREACH(stream_list_get(stream_list), rstream) {
 		if (rstream->direction != dev->direction)
 			continue;
@@ -975,8 +980,7 @@ static int force_close_pinned_only_device(struct cras_iodev *dev)
 			continue;
 		audio_thread_disconnect_stream(audio_thread, rstream, dev);
 	}
-	if (stream_list_has_pinned_stream(stream_list, dev->info.idx))
-		return -EEXIST;
+
 	close_dev(dev);
 	dev->update_active_node(dev, dev->active_node->idx, 0);
 	return 0;