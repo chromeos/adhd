c2c32a0994b8c68dd7795885ef2910ad7ccc477e
Hsin-Yu Chao <hychao@google.com>
2019-06-20 23:46:35 +0800

    CRAS: input_data - Use the correct offset to process APM
    Whenever we process input_data's float buffer, should use the
    unchanged stream offset.
    Fix the bug that we pass the audio area's offset to APM process
    function. This is incorrect in rare cases when the stream offset
    gets clipped by area's frame count.
    
    BUG=chromium:973745
    TEST=Run audio_CrasDevSwitchStress.input to verify input buffer
    level in CRAS doesn't go super high.
    
    Change-Id: I088a037a6496f1a7f69ade37195fb65c6286bb71
    Reviewed-on: https://chromium-review.googlesource.com/1670416
    Tested-by: Hsinyu Chao <hychao@chromium.org>
    Commit-Ready: Hsinyu Chao <hychao@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>

--- cras/src/server/input_data.c
+++ cras/src/server/input_data.c
@@ -89,6 +89,58 @@ void input_data_set_all_streams_read(struct input_data *data,
 	float_buffer_read(data->fbuffer, nframes);
 }
 
+/*
+ * The logic is not trivial to return the cras_audio_area and offset for
+ * a input stream to read. The buffer position and length of a bunch of
+ * input member variables are described below.
+ *
+ *                          hw_ptr                 appl_ptr
+ * a. buffer of input device: |------------------------|
+ * b. fbuffer of input data:         |<--------------->|
+ * c. stream offset of input data:         |<--------->|
+ *    stream offset of input data:                |<-->|
+ *    stream offset of input data:     |<------------->|
+ * d. audio area of input data:          |<----------->|
+ *
+ * One thing to keep in mind is, the offset could exceed the size of
+ * buffer to read. It's not intuitive though why the stream offset would
+ * exceed buffer size. Check this example:
+ *
+ * Idev gets input buffer 500 frames. One stream read 400, while the other
+ * stream read 100. We track stream offset [0, 300] after both stream
+ * consumes 100 frames. In the next wake up, audio thread asks idev to
+ * get 250 frames. Now the input data holds audio area containing 250 frames
+ * of audio as queried, while its float buffer contains 400 frames of audio
+ * deinterleaved from last wake up.
+ *
+ * Wake up at T0:
+ *                        hw_ptr                        appl_ptr
+ * Input audio area         |-------------------------------|
+ * deinterleave float       |-------------------------------|
+ * Stream 1 read                                     |------|
+ * Stream 2 read                    |-----------------------|
+ *
+ * Wake up at T1:
+                          hw_ptr                 appl_ptr
+ * Input audio area                     |------------|
+ * deinterleave float       |------------------------|
+ * Stream 1 offset                                   |
+ * Stream 2 offset                  |----------------|
+ *
+ * Case 1:
+ * A normal input stream, of read offset 0, about to read from device.
+ * We shall return the exact audio area from idev, and set read offset to 0.
+ *
+ * Case 2:
+ * A normal input stream, of read offset 300, about to read from device.
+ * We shall return the exact audio area from idev but clip read offset to 250.
+ *
+ * Case 3:
+ * An APM Stream of read offset 300, would like to read the deinterleaved
+ * float buffer. We shall let APM process the float buffer from offset 300.
+ * Don't bother clip read offset in this case, because fbuffer contains
+ * the deepest deinterleaved audio data ever read from idev.
+ */
 int input_data_get_for_stream(struct input_data *data,
 			      struct cras_rstream *stream,
 			      struct buffer_share *offsets,
@@ -97,29 +149,30 @@ int input_data_get_for_stream(struct input_data *data,
 {
 	int apm_processed;
 	struct cras_apm *apm;
-
-	/*
-	 * It is possible that area buffer frames is smaller than the
-	 * offset of stream. In this case, just reset the offset value
-	 * to area->frames to prevent caller using these information get
-	 * bad access to data.
-	 */
-	*area = data->area;
-	*offset = MIN(buffer_share_id_offset(offsets, stream->stream_id),
-		      data->area->frames);
+	int stream_offset = buffer_share_id_offset(offsets, stream->stream_id);
 
 	apm = cras_apm_list_get(stream->apm_list, data->dev_ptr);
-	if (apm == NULL)
-		return 0;
-
-	apm_processed = cras_apm_list_process(apm, data->fbuffer, *offset);
-	if (apm_processed < 0) {
-		cras_apm_list_remove(stream->apm_list, apm);
-		return 0;
+	if (apm == NULL) {
+		/*
+		 * Case 1 and 2 from above example.
+		 */
+		*area = data->area;
+		*offset = MIN(stream_offset, data->area->frames);
+	} else {
+		/*
+		 * Case 3 from above example.
+		 */
+		apm_processed = cras_apm_list_process(apm, data->fbuffer,
+						      stream_offset);
+		if (apm_processed < 0) {
+			cras_apm_list_remove(stream->apm_list, apm);
+			return 0;
+		}
+		buffer_share_offset_update(offsets, stream->stream_id,
+					   apm_processed);
+		*area = cras_apm_list_get_processed(apm);
+		*offset = 0;
 	}
-	buffer_share_offset_update(offsets, stream->stream_id, apm_processed);
-	*area = cras_apm_list_get_processed(apm);
-	*offset = 0;
 
 	return 0;
 }
