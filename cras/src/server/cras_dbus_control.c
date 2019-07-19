ca3c694994cc0118d1796443964b1c6c7283e76e
Hsin-Yu Chao <hychao@google.com>
2019-07-19 12:54:46 +0800

    CRAS: dbus_control - Add SetWbsEnabled method
    Exposing this command so that the WBS enabling flag can be set
    from crosh. This will allow user to test WBS feature without
    an test image.
    
    BUG=chromium:983898
    TEST=dbus-send --system --type=method_call --print-reply \
      --dest=org.chromium.cras /org/chromium/cras \
      org.chromium.cras.Control.SetWbsEnabled boolean:true
    
    Change-Id: I3109d727c86f1e85cc603b2cb4f47669660dfb1f
    Reviewed-on: https://chromium-review.googlesource.com/1710029
    Tested-by: Hsinyu Chao <hychao@chromium.org>
    Commit-Ready: Hsinyu Chao <hychao@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: En-Shuo Hsu <enshuo@google.com>

--- cras/src/server/cras_dbus_control.c
+++ cras/src/server/cras_dbus_control.c
@@ -113,6 +113,9 @@
 	"    <method name=\"IsAudioOutputActive\">\n"                           \
 	"      <arg name=\"active\" type=\"b\" direction=\"out\"/>\n"           \
 	"    </method>\n"                                                       \
+	"    <method name=\"SetWbsEnabled\">\n"                                 \
+	"      <arg name=\"enabled\" type=\"b\" direction=\"in\"/>\n"           \
+	"    </method>\n"                                                       \
 	"  </interface>\n"                                                      \
 	"  <interface name=\"" DBUS_INTERFACE_INTROSPECTABLE "\">\n"            \
 	"    <method name=\"Introspect\">\n"                                    \
@@ -761,6 +764,23 @@ static DBusHandlerResult handle_is_audio_active(DBusConnection *conn,
 	return DBUS_HANDLER_RESULT_HANDLED;
 }
 
+static DBusHandlerResult handle_set_wbs_enabled(DBusConnection *conn,
+						DBusMessage *message, void *arg)
+{
+	int rc;
+	dbus_bool_t enabled;
+
+	rc = get_single_arg(message, DBUS_TYPE_BOOLEAN, &enabled);
+	if (rc)
+		return rc;
+
+	cras_system_set_bt_wbs_enabled(enabled);
+
+	send_empty_reply(conn, message);
+
+	return DBUS_HANDLER_RESULT_HANDLED;
+}
+
 /* Handle incoming messages. */
 static DBusHandlerResult handle_control_message(DBusConnection *conn,
 						DBusMessage *message, void *arg)
@@ -877,6 +897,9 @@ static DBusHandlerResult handle_control_message(DBusConnection *conn,
 	} else if (dbus_message_is_method_call(message, CRAS_CONTROL_INTERFACE,
 					       "IsAudioOutputActive")) {
 		return handle_is_audio_active(conn, message, arg);
+	} else if (dbus_message_is_method_call(message, CRAS_CONTROL_INTERFACE,
+					       "SetWbsEnabled")) {
+		return handle_set_wbs_enabled(conn, message, arg);
 	}
 
 	return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
