chromeos/adhd

commit 57abaf66e2091ab8d643f02ea08f79c0fae5a28f

CRAS: bt_device - Fix the logic to start conn watch flow
The BT connect watch callback regularly checks if each of the
supported profiles are connected, and decide when to expose A2DP/HFP
node to audio settings.

Existing logic in CRAS restarts this connection watch flow if
bluetoothd tells CRAS the device capability changes. However often
the capability change is unrelated to A2DP or HFP so we can skip it.

BUG=chromium:968934
TEST=Use LG HBS700 to pair, and then disconnect it from BT settings UI.
Verify that audio settings doesn't show LG HBS700 as option.
Test fresh pair multiple times as there's a chance to reproduce it.

Change-Id: I6935ee4f758c6814ae983d9d0c786c8eaa71b35d
Reviewed-on: https://chromium-review.googlesource.com/1636844
Commit-Ready: Hsinyu Chao <hychao@chromium.org>
Tested-by: Hsinyu Chao <hychao@chromium.org>
Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
Reviewed-by: En-Shuo Hsu <enshuo@google.com>

cras/src/server/cras_bt_device.c
@@ -673,12 +673,50 @@ static void cras_bt_device_set_connected(struct cras_bt_device *device,
 	}
 }
 
+/*
+ * Check if the uuid is of a new audio profile that isn't listed
+ * as supported by device.
+ * Args:
+ *    device - The BT device holding supported profiles bitmap.
+ *    uuid - UUID string from the device properties notified by BlueZ.
+ * Returns:
+ *    True if uuid is a new audio profiles not already supported by device.
+ */
+static int update_supported_profiles(struct cras_bt_device *device,
+				     const char *uuid)
+{
+	static unsigned int audio_profiles =
+			CRAS_BT_DEVICE_PROFILE_A2DP_SINK |
+			CRAS_BT_DEVICE_PROFILE_HFP_HANDSFREE |
+			CRAS_BT_DEVICE_PROFILE_HSP_AUDIOGATEWAY;
+
+	enum cras_bt_device_profile profile =
+			cras_bt_device_profile_from_uuid(uuid);
+
+	if (profile == 0)
+		return 0;
+
+	/* Do nothing if this profile is not new. */
+	if (device->profiles & profile)
+		return 0;
+
+	/* Log this event as we might need to re-intialize the BT audio nodes
+	 * if new audio profile is reported for already connected device. */
+	if (device->connected && (profile & audio_profiles))
+		BTLOG(btlog, BT_NEW_AUDIO_PROFILE_AFTER_CONNECT,
+		      device->profiles, profile);
+	device->profiles |= profile;
+	cras_bt_device_log_profile(device, profile);
+
+	return (profile & audio_profiles);
+}
+
 void cras_bt_device_update_properties(struct cras_bt_device *device,
 				      DBusMessageIter *properties_array_iter,
 				      DBusMessageIter *invalidated_array_iter)
 {
 
-	int get_profile = 0;
+	int has_new_audio_profile = 0;
 
 	while (dbus_message_iter_get_arg_type(properties_array_iter) !=
 	       DBUS_TYPE_INVALID) {
@@ -743,17 +781,12 @@ void cras_bt_device_update_properties(struct cras_bt_device *device,
 			while (dbus_message_iter_get_arg_type(
 				       &uuid_array_iter) != DBUS_TYPE_INVALID) {
 				const char *uuid;
-				enum cras_bt_device_profile profile;
-
-				get_profile = 1;
 
 				dbus_message_iter_get_basic(&uuid_array_iter,
 							    &uuid);
-				profile = cras_bt_device_profile_from_uuid(
-					uuid);
 
-				device->profiles |= profile;
-				cras_bt_device_log_profile(device, profile);
+				has_new_audio_profile =
+					update_supported_profiles(device, uuid);
 
 				dbus_message_iter_next(&uuid_array_iter);
 			}
@@ -793,12 +826,12 @@ void cras_bt_device_update_properties(struct cras_bt_device *device,
 		dbus_message_iter_next(invalidated_array_iter);
 	}
 
-	/* If updated properties includes profile, and device is connected,
-	 * we need to start connection watcher. This is needed because on
-	 * some bluetooth device, supported profiles do not present when
-	 * device interface is added and they are updated later.
+	/* If updated properties includes new audio profile, and device is
+	 * connected, we need to start connection watcher. This is needed
+	 * because on some bluetooth device, supported profiles do not present
+	 * when device interface is added and they are updated later.
 	 */
-	if (get_profile && device->connected) {
+	if (has_new_audio_profile && device->connected) {
 		cras_bt_device_start_new_conn_watch_timer(device);
 	}
 }

cras/src/common/cras_types.h
@@ -248,6 +248,7 @@ enum CRAS_BT_LOG_EVENTS {
 	BT_HFP_SUPPORTED_FEATURES,
 	BT_HSP_NEW_CONNECTION,
 	BT_HSP_REQUEST_DISCONNECT,
+	BT_NEW_AUDIO_PROFILE_AFTER_CONNECT,
 	BT_RESET,
 	BT_SCO_CONNECT,
 	BT_TRANSPORT_ACQUIRE,

cras/src/tests/cras_test_client.c
@@ -791,6 +791,10 @@ static void show_btlog_tag(const struct cras_bt_event_log *log,
 	case BT_HSP_REQUEST_DISCONNECT:
 		printf("%-30s\n", "HSP_REQUEST_DISCONNECT");
 		break;
+	case BT_NEW_AUDIO_PROFILE_AFTER_CONNECT:
+		printf("%-30s old %u, new %u\n", "NEW_AUDIO_PROFILE_AFTER_CONNECT",
+			data1, data2);
+		break;
 	case BT_RESET:
 		printf("%-30s\n", "RESET");
 		break;

commit 22c057336f4c5e0a4a33761d806859e052479d7e

CRAS: bt_device - Fix bt device supports both audio and non-audio UUIDs
Fix a bug caused by commit 57abaf66e that if a BT device reports
both audio and non-audio UUIDs in one PropertiesChanged signal,
CRAS incorrectly treats it as not an audio device.

Here's the example dbus-monitor log:

 ... member=PropertiesChanged
   string "org.bluez.Device1"
   array [
      dict entry(
         string "UUIDs"
         variant             array [
               string "00001108-0000-1000-8000-00805f9b34fb"
               string "0000110b-0000-1000-8000-00805f9b34fb"
               string "0000110c-0000-1000-8000-00805f9b34fb"
               string "0000110e-0000-1000-8000-00805f9b34fb"
               string "0000111e-0000-1000-8000-00805f9b34fb"
               string "0000112e-0000-1000-8000-00805f9b34fb"
               string "00001200-0000-1000-8000-00805f9b34fb"
            ]
      )

BUG=b:138747027
TEST=Verify by AIRBAND bt headset.

Change-Id: I917c750acbca20adc7ca8bd866923f9bedb3fa61
Reviewed-on: https://chromium-review.googlesource.com/1741126
Tested-by: Hsinyu Chao <hychao@chromium.org>
Commit-Ready: Hsinyu Chao <hychao@chromium.org>
Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>

cras/src/server/cras_bt_device.c
@@ -762,9 +762,9 @@ void cras_bt_device_update_properties(struct cras_bt_device *device,
 				dbus_message_iter_get_basic(&uuid_array_iter,
 							    &uuid);
 
-				has_new_audio_profile =
-					cras_bt_device_add_supported_profiles(
-						device, uuid);
+				if (cras_bt_device_add_supported_profiles(
+						device, uuid))
+					has_new_audio_profile = 1;
 
 				dbus_message_iter_next(&uuid_array_iter);
 			}