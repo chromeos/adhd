179bd7aee5da3a993d76fae8621241610734afb7
En-Shuo Hsu <enshuo@chromium.org>
2019-08-29 08:58:38 +0800

    CRAS: Format fuzz and alsa_ucm
    Reformat codes with the .clang-format we just introduced.
    To format whole files provided in-place:
    clang-format -i -style=file [<file> ...]
    
    BUG=chromium:950858
    TEST=Add directory to check_format_error and run
    FEATURES="test" USE=asan emerge-${BOARD} -v media-sound/adhd
    
    Change-Id: Id3c0f7ee7a9e6cc808a6d7be0e281899b050f9c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/1773024
    Tested-by: En-Shuo Hsu <enshuo@google.com>
    Auto-Submit: En-Shuo Hsu <enshuo@google.com>
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Commit-Queue: En-Shuo Hsu <enshuo@google.com>

--- cras/src/fuzz/rclient_message.cc
+++ cras/src/fuzz/rclient_message.cc
@@ -17,29 +17,25 @@ extern "C" {
 #include "cras_system_state.h"
 }
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-  cras_rclient *client = cras_control_rclient_create(0, 0);
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  cras_rclient* client = cras_control_rclient_create(0, 0);
   cras_rclient_buffer_from_client(client, data, size, NULL, 0);
   cras_rclient_destroy(client);
 
   return 0;
 }
 
-extern "C" int LLVMFuzzerInitialize(int *argc, char ***argv) {
-  char *shm_name;
+extern "C" int LLVMFuzzerInitialize(int* argc, char*** argv) {
+  char* shm_name;
   if (asprintf(&shm_name, "/cras-%d", getpid()) < 0)
     exit(-ENOMEM);
-  struct cras_server_state *exp_state = (struct cras_server_state *)
-    calloc(1, sizeof(*exp_state));
+  struct cras_server_state* exp_state =
+      (struct cras_server_state*)calloc(1, sizeof(*exp_state));
   if (!exp_state)
     exit(-1);
   int rw_shm_fd = open("/dev/null", O_RDWR);
   int ro_shm_fd = open("/dev/null", O_RDONLY);
-  cras_system_state_init("/tmp",
-                         shm_name,
-                         rw_shm_fd,
-                         ro_shm_fd,
-                         exp_state,
+  cras_system_state_init("/tmp", shm_name, rw_shm_fd, ro_shm_fd, exp_state,
                          sizeof(*exp_state));
   free(shm_name);
 
