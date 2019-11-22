chromeos/adhd

commit 7139cca49b990e6e0b1d305d428b7b1acbf61a3e

CRAS: hfp_slc - Clean up HFP features bitmap
- Update the HF and AG feature bits definition to HFP 1.7.1
  The new codec negotiation feature will be used for WBS.
- Move HF/AG features definition to slc header, where the
  AT commands are implemented and actually using these feature bits.
- Let cras_hfp_ag pass in the supported features bitmap when creating
  an SLC handle. This helps to decouple SLC implementation and the
  logic deciding whether WBS is supported.

BUG=chromium:843048
TEST=Apply full patch set, execute command
cras_test_client --set_wbs_enabled 1 (or 0) to enable
and disable wideband mode while manual test Bose QC35 II

Change-Id: I825d82ea0966b0825b8fa5aa90d9ef301bad0277
Reviewed-on: https://chromium-review.googlesource.com/1632050
Commit-Ready: Hsinyu Chao <hychao@chromium.org>
Tested-by: Hsinyu Chao <hychao@chromium.org>
Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
Reviewed-by: En-Shuo Hsu <enshuo@google.com>

cras/src/server/cras_hfp_ag_profile.c
@@ -17,7 +17,6 @@
 #include "cras_hfp_info.h"
 #include "cras_hfp_iodev.h"
 #include "cras_hfp_alsa_iodev.h"
-#include "cras_hfp_slc.h"
 #include "cras_system_state.h"
 #include "cras_iodev_list.h"
 #include "utlist.h"
@@ -233,6 +232,7 @@ static int cras_hfp_ag_new_connection(DBusConnection *conn,
 				       int rfcomm_fd)
 {
 	struct audio_gateway *ag;
+	int ag_features;
 
 	BTLOG(btlog, BT_HFP_NEW_CONNECTION, 0, 0);
 
@@ -251,8 +251,19 @@ static int cras_hfp_ag_new_connection(DBusConnection *conn,
 	ag->device = device;
 	ag->conn = conn;
 	ag->profile = cras_bt_device_profile_from_uuid(profile->uuid);
+
+	/*
+	 * If the WBS enabled flag is set, add codec negotiation feature.
+	 * TODO(hychao): replace this check by query bluetooth stack whether
+	 * controller supports WBS feature.
+	 */
+	ag_features = profile->features;
+	if (cras_system_get_bt_wbs_enabled())
+		ag_features |= AG_CODEC_NEGOTIATION;
+
 	ag->slc_handle = hfp_slc_create(rfcomm_fd,
 					0,
+					ag_features,
 					device,
 					cras_hfp_ag_slc_initialized,
 					cras_hfp_ag_slc_disconnected);
@@ -283,7 +294,7 @@ static struct cras_bt_profile cras_hfp_ag_profile = {
 	.uuid = HFP_AG_UUID,
 	.version = HFP_VERSION_1_5,
 	.role = NULL,
-	.features = HFP_SUPPORTED_FEATURE & 0x1F,
+	.features = CRAS_AG_SUPPORTED_FEATURES & 0x1F,
 	.record = NULL,
 	.release = cras_hfp_ag_release,
 	.new_connection = cras_hfp_ag_new_connection,
@@ -320,7 +331,11 @@ static int cras_hsp_ag_new_connection(DBusConnection *conn,
 	ag->device = device;
 	ag->conn = conn;
 	ag->profile = cras_bt_device_profile_from_uuid(profile->uuid);
-	ag->slc_handle = hfp_slc_create(rfcomm_fd, 1, device, NULL,
+	ag->slc_handle = hfp_slc_create(rfcomm_fd,
+					1,
+					profile->features,
+					device,
+					NULL,
 					cras_hfp_ag_slc_disconnected);
 	DL_APPEND(connected_ags, ag);
 	cras_hfp_ag_slc_initialized(ag->slc_handle);

cras/src/server/cras_hfp_ag_profile.h
@@ -9,32 +9,10 @@
 #include <dbus/dbus.h>
 
 #include "cras_bt_device.h"
+#include "cras_hfp_slc.h"
 
-/* Hands-free Audio Gateway feature bits, listed in according
- * to their order in the bitmap defined in HFP spec.
- */
-/* Call waiting and 3-way calling */
-#define HFP_THREE_WAY_CALLING           0x0001
-/* EC and/or NR function */
-#define HFP_EC_ANDOR_NR                 0x0002
-/* Voice recognition activation */
-#define HFP_VOICE_RECOGNITION           0x0004
-/* Inband ringtone */
-#define HFP_INBAND_RINGTONE             0x0008
-/* Attach a number to voice tag */
-#define HFP_ATTACH_NUMBER_TO_VOICETAG   0x0010
-/* Ability to reject a call */
-#define HFP_REJECT_A_CALL               0x0020
-/* Enhanced call status */
-#define HFP_ENHANCED_CALL_STATUS        0x0040
-/* Enhanced call control */
-#define HFP_ENHANCED_CALL_CONTRO        0x0080
-/* Extended error result codes */
-#define HFP_EXTENDED_ERROR_RESULT_CODES 0x0100
-/* Codec negotiation */
-#define HFP_CODEC_NEGOTIATION           0x0200
-
-#define HFP_SUPPORTED_FEATURE           (HFP_ENHANCED_CALL_STATUS)
+/* The bitmap of HFP AG feature supported by CRAS */
+#define CRAS_AG_SUPPORTED_FEATURES           (AG_ENHANCED_CALL_STATUS)
 
 struct hfp_slc_handle;
 

cras/src/server/cras_hfp_slc.c
@@ -8,7 +8,6 @@
 
 #include "cras_bt_device.h"
 #include "cras_telephony.h"
-#include "cras_hfp_ag_profile.h"
 #include "cras_hfp_slc.h"
 #include "cras_system_state.h"
 
@@ -52,6 +51,7 @@
  *    service - Current service availability of AG stored in SLC.
  *    callheld - Current callheld status of AG stored in SLC.
  *    ind_event_report - Activate status of indicator events reporting.
+ *    ag_supported_features - Supported AG features bitmap.
  *    telephony - A reference of current telephony handle.
  *    device - The associated bt device.
  */
@@ -70,6 +70,7 @@ struct hfp_slc_handle {
 	int service;
 	int callheld;
 	int ind_event_report;
+	int ag_supported_features;
 	struct cras_bt_device *device;
 
 	struct cras_telephony_handle *telephony;
@@ -454,7 +455,7 @@ static int supported_features(struct hfp_slc_handle *handle, const char *cmd)
 	 * for now. Respond with +BRSF:<feature> to notify mandatory supported
 	 * features in AG(audio gateway).
 	 */
-	snprintf(response, 128, "+BRSF: %u", HFP_SUPPORTED_FEATURE);
+	snprintf(response, 128, "+BRSF: %u", handle->ag_supported_features);
 	err = hfp_send(handle, response);
 	if (err < 0)
 		return err;
@@ -615,6 +616,7 @@ static void slc_watch_callback(void *arg)
 
 struct hfp_slc_handle *hfp_slc_create(int fd,
 				      int is_hsp,
+				      int ag_supported_features,
 				      struct cras_bt_device *device,
 				      hfp_slc_init_cb init_cb,
 				      hfp_slc_disconnect_cb disconnect_cb)
@@ -627,6 +629,7 @@ struct hfp_slc_handle *hfp_slc_create(int fd,
 
 	handle->rfcomm_fd = fd;
 	handle->is_hsp = is_hsp;
+	handle->ag_supported_features = ag_supported_features;
 	handle->device = device;
 	handle->init_cb = init_cb;
 	handle->disconnect_cb = disconnect_cb;

cras/src/server/cras_hfp_slc.h
@@ -9,6 +9,41 @@
 struct hfp_slc_handle;
 struct cras_bt_device;
 
+/*
+ * Hands-free HFP and AG supported features bits definition.
+ * Per HFP 1.7.1 specification section 4.34.1, command
+ * AT+BRSF (Bluetooth Retrieve Supported Features)
+ */
+#define HF_EC_ANDOR_NR			0x0001
+#define HF_THREE_WAY_CALLING		0x0002
+#define HF_CLI_PRESENTATION_CAP		0x0004
+#define HF_VOICE_RECOGNITION		0x0008
+#define HF_REMOVE_VOLUME_CTONTROL	0x0010
+#define HF_ENHANCED_CALL_STATUS		0x0020
+#define HF_ENHANCED_CALL_CONTROL	0x0040
+#define HF_CODEC_NEGOTIATION		0x0080
+#define HF_HF_INDICATORS		0x0100
+#define HF_ESCO_S4_T2_SETTINGS		0x0200
+
+#define AG_THREE_WAY_CALLING		0x0001
+#define AG_EC_ANDOR_NR			0x0002
+#define AG_VOICE_RECOGNITION		0x0004
+#define AG_INBAND_RINGTONE		0x0008
+#define AG_ATTACH_NUMBER_TO_VOICETAG	0x0010
+#define AG_REJECT_A_CALL		0x0020
+#define AG_ENHANCED_CALL_STATUS		0x0040
+#define AG_ENHANCED_CALL_CONTROL	0x0080
+#define AG_EXTENDED_ERROR_RESULT_CODES	0x0100
+#define AG_CODEC_NEGOTIATION		0x0200
+#define AG_HF_INDICATORS		0x0400
+#define AG_ESCO_S4_T2_SETTINGS		0x0800
+
+/* Codec ids for codec negotiation, per HFP 1.7.1 spec appendix B. */
+#define HFP_CODEC_UNUSED		0
+#define HFP_CODEC_ID_CVSD		1
+#define HFP_CODEC_ID_MSBC		2
+#define HFP_MAX_CODECS			3
+
 /* Callback to call when service level connection initialized. */
 typedef int (*hfp_slc_init_cb)(struct hfp_slc_handle *handle);
 
@@ -20,6 +55,7 @@ typedef int (*hfp_slc_disconnect_cb)(struct hfp_slc_handle *handle);
  * Args:
  *    fd - the rfcomm fd used to initialize service level connection
  *    is_hsp - if the slc handle is created for headset profile
+ *    ag_supported_features - Supported AG features bitmap.
  *    device - The bt device associated with the created slc object
  *    init_cb - the callback function to be triggered when a service level
  *        connection is initialized.
@@ -27,6 +63,7 @@ typedef int (*hfp_slc_disconnect_cb)(struct hfp_slc_handle *handle);
  *        level connection is disconnected.
  */
 struct hfp_slc_handle *hfp_slc_create(int fd, int is_hsp,
+				      int ag_supported_features,
 				      struct cras_bt_device *device,
 				      hfp_slc_init_cb init_cb,
 				      hfp_slc_disconnect_cb disconnect_cb);
@@ -65,5 +102,8 @@ int hfp_event_set_service(struct hfp_slc_handle *handle, int value);
 
 /* Sets speaker gain value to headsfree device. */
 int hfp_event_speaker_gain(struct hfp_slc_handle *handle, int gain);
+
+/* Gets the selected codec for HFP, mSBC or CVSD. */
+int hfp_slc_get_selected_codec(struct hfp_slc_handle *handle);
 
 #endif /* CRAS_HFP_SLC_H_ */

cras/src/tests/hfp_ag_profile_unittest.cc
@@ -174,11 +174,12 @@ enum cras_bt_device_profile cras_bt_device_profile_from_uuid(const char *uuid) {
   return CRAS_BT_DEVICE_PROFILE_HSP_AUDIOGATEWAY;
 }
 
-struct hfp_slc_handle *hfp_slc_create(int fd,
-    int is_hsp,
+struct hfp_slc_handle *hfp_slc_create(
+    int fd, int is_hsp,
+    int ag_supported_features,
     struct cras_bt_device *device,
-    void *init_cb,
-    void *disconnect_cb) {
+    hfp_slc_init_cb init_cb,
+    hfp_slc_disconnect_cb disconnect_cb) {
   return NULL;
 }
 

cras/src/tests/hfp_slc_unittest.cc
@@ -42,7 +42,9 @@ namespace {
 TEST(HfpSlc, CreateSlcHandle) {
   ResetStubData();
 
-  handle = hfp_slc_create(0, 0, device, slc_initialized_cb,
+  handle = hfp_slc_create(0, 0,
+                          AG_ENHANCED_CALL_STATUS,
+                          device, slc_initialized_cb,
                           slc_disconnected_cb);
   ASSERT_EQ(1, cras_system_add_select_fd_called);
   ASSERT_EQ(handle, slc_cb_data);
@@ -58,7 +60,9 @@ TEST(HfpSlc, InitializeSlc) {
   ResetStubData();
 
   ASSERT_EQ(0, socketpair(AF_UNIX, SOCK_STREAM, 0, sock));
-  handle = hfp_slc_create(sock[0], 0, device, slc_initialized_cb,
+  handle = hfp_slc_create(sock[0], 0,
+                          AG_ENHANCED_CALL_STATUS,
+                          device, slc_initialized_cb,
                           slc_disconnected_cb);
 
   err = write(sock[1], "AT+CIND=?\r", 10);
@@ -113,7 +117,9 @@ TEST(HfpSlc, DisconnectSlc) {
   ResetStubData();
 
   ASSERT_EQ(0, socketpair(AF_UNIX, SOCK_STREAM, 0, sock));
-  handle = hfp_slc_create(sock[0], 0, device, slc_initialized_cb,
+  handle = hfp_slc_create(sock[0], 0,
+                          AG_ENHANCED_CALL_STATUS,
+                          device, slc_initialized_cb,
                           slc_disconnected_cb);
   /* Close socket right away to make read() get negative err code, and
    * fake the errno to ECONNRESET. */

commit 543307a13e46ef790a913685d5ee0e76e5139e04

CRAS: hfp_ag - Fix missing feature to +BSRF command
Commit 01f7483cf says that we should set the 5 bits of
profile feature for SDP record. This was broken by recent
commit 7139cca49.
Although we don't see any headset affected by this mistake,
we should fix it to align with HFP specification.

BUG=None
TEST=btmon to verify the response of +BSRF feature is not zero

Change-Id: Ic1c485fe8e6e9393c79a583eec3be2a3ea5ae78a
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/1910929
Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>
Tested-by: Hsinyu Chao <hychao@chromium.org>
Commit-Queue: Hsinyu Chao <hychao@chromium.org>

cras/src/server/cras_hfp_ag_profile.c
@@ -256,7 +256,7 @@ static int cras_hfp_ag_new_connection(DBusConnection *conn,
 	 * TODO(hychao): AND the two conditions to let bluetooth daemon
 	 * control whether to turn on WBS feature.
 	 */
-	ag_features = profile->features;
+	ag_features = CRAS_AG_SUPPORTED_FEATURES;
 	if (cras_system_get_bt_wbs_enabled() &&
 	    cras_bt_adapter_wbs_supported(adapter))
 		ag_features |= AG_CODEC_NEGOTIATION;
@@ -332,8 +332,9 @@ static int cras_hsp_ag_new_connection(DBusConnection *conn,
 	ag->device = device;
 	ag->conn = conn;
 	ag->profile = cras_bt_device_profile_from_uuid(profile->uuid);
-	ag->slc_handle = hfp_slc_create(rfcomm_fd, 1, profile->features, device,
-					NULL, cras_hfp_ag_slc_disconnected);
+	ag->slc_handle =
+		hfp_slc_create(rfcomm_fd, 1, CRAS_AG_SUPPORTED_FEATURES, device,
+			       NULL, cras_hfp_ag_slc_disconnected);
 	DL_APPEND(connected_ags, ag);
 	cras_hfp_ag_slc_initialized(ag->slc_handle);
 	return 0;