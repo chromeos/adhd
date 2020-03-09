18aa016d5bd0b51f29f818bbccb0a9dd648a1f1c
Fletcher Woodruff <fletcherw@chromium.org>
2020-03-09 11:46:31 -0600

    adhd: oss-fuzz: cleanup oss-fuzz build
    In preparation for bringing the upstream oss-fuzz build file in line
    with the version in the cras source tree:
    
    * Properly quote command arguments
    * Rename 'cras_hfp_slc_fuzzer' executable to just 'cras_hfp_slc' to
    match 'rclient_message'.
    * Start building with dbus (since cras_hfp requires it)
    * Disable webrtc in build
    * Add logic to zip corpus files for oss-fuzz
    
    BUG=b:151148778
    TEST=Build and run fuzzers according to cras/src/fuzz/README.md
    
    Change-Id: I6e842a9b16812407730e6b817a8a09130343e268
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/2095528
    Reviewed-by: Chih-Yang Hsia <paulhsia@chromium.org>
    Commit-Queue: Chih-Yang Hsia <paulhsia@chromium.org>
    Tested-by: Chih-Yang Hsia <paulhsia@chromium.org>

--- cras/src/fuzz/build.sh
+++ cras/src/fuzz/build.sh
@@ -4,18 +4,21 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
-# Builds fuzzers from within a container into /out/ directory.
-# Expects /src/adhd to contain an adhd checkout.
+# Builds fuzzers from within a container into ${OUT} directory.
+# Expects "${SRC}/adhd" to contain an adhd checkout.
 
-cd $SRC/adhd/cras
+cd "${SRC}/adhd/cras"
 ./git_prepare.sh
 
 FUZZER_LDFLAGS="${FUZZER_LDFLAGS} ${LIB_FUZZING_ENGINE}"
-./configure --disable-dbus --enable-fuzzer
+./configure --enable-fuzzer
 
-# Compile fuzzer
+# Compile fuzzers
 make -j$(nproc)
 
-# Copy fuzzer to /out/ directory
-cp $SRC/adhd/cras/src/cras_rclient_message_fuzzer $OUT/rclient_message
-cp $SRC/adhd/cras/src/cras_hfp_slc_fuzzer $OUT/cras_hfp_slc_fuzzer
+# Copy fuzzers and dependencies to "${OUT}" directory
+cp "${SRC}/adhd/cras/src/cras_rclient_message_fuzzer" "${OUT}/rclient_message"
+zip -j "${OUT}/rclient_message_corpus.zip" ./src/fuzz/corpus/*
+
+cp "${SRC}/adhd/cras/src/cras_hfp_slc_fuzzer" "${OUT}/cras_hfp_slc"
+cp "${SRC}/adhd/cras/src/fuzz/cras_hfp_slc.dict" "${OUT}/cras_hfp_slc.dict"
