chromeos/adhd

commit a563d702d439fdcef7fe8259a959604a73101d3a

CRAS: input_data - Skip SW capture gain only at AEC use case
For the case when external audio devices are selected as input
or output, which is not the typical AEC use case, CRAS shall
still take into account the SW capture gain.
This change aligns CRAS behavior to the case when APM runs inside
browser.

BUG=b:151794046
TEST=Manual test type-C headset.

Change-Id: I4784973c51c734d70821361d47c919d384010635
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/2108461
Reviewed-by: Chih-Yang Hsia <paulhsia@chromium.org>
Tested-by: Hsinyu Chao <hychao@chromium.org>
Commit-Queue: Hsinyu Chao <hychao@chromium.org>

cras/src/server/cras_apm_list.c
@@ -63,6 +63,9 @@ static const unsigned int MAX_INI_NAME_LEN = 63;
  *        stream.
  *    work_queue - A task queue instance created and destroyed by
  *        libwebrtc_apm.
+ *    use_tuned_settings - True if this APM uses settings tuned specifically
+ *        for this hardware in AEC use case. Otherwise it uses the generic
+ *        settings like run inside browser.
  */
 struct cras_apm {
 	webrtc_apm apm_ptr;
@@ -73,6 +76,7 @@ struct cras_apm {
 	struct cras_audio_format fmt;
 	struct cras_audio_area *area;
 	void *work_queue;
+	bool use_tuned_settings;
 	struct cras_apm *prev, *next;
 };
 
@@ -268,7 +272,6 @@ struct cras_apm *cras_apm_list_add_apm(struct cras_apm_list *list,
 				       bool is_aec_use_case)
 {
 	struct cras_apm *apm;
-	bool use_tuned_settings;
 
 	DL_FOREACH (list->apms, apm)
 		if (apm->dev_ptr == dev_ptr)
@@ -289,16 +292,16 @@ struct cras_apm *cras_apm_list_add_apm(struct cras_apm_list *list,
 
 	/* Use tuned settings only when the forward dev(capture) and reverse
 	 * dev(playback) both are in typical AEC use case. */
-	use_tuned_settings = is_aec_use_case;
+	apm->use_tuned_settings = is_aec_use_case;
 	if (rmodule->odev) {
-		use_tuned_settings &=
+		apm->use_tuned_settings &=
 			cras_iodev_is_aec_use_case(rmodule->odev->active_node);
 	}
 
 	/* Use the configs tuned specifically for internal device. Otherwise
 	 * just pass NULL so every other settings will be default. */
 	apm->apm_ptr =
-		use_tuned_settings ?
+		apm->use_tuned_settings ?
 			webrtc_apm_create(apm->fmt.num_channels,
 					  apm->fmt.frame_rate, aec_ini,
 					  apm_ini) :
@@ -686,6 +689,11 @@ void cras_apm_list_put_processed(struct cras_apm *apm, unsigned int frames)
 struct cras_audio_format *cras_apm_list_get_format(struct cras_apm *apm)
 {
 	return &apm->fmt;
+}
+
+bool cras_apm_list_get_use_tuned_settings(struct cras_apm *apm)
+{
+	return apm->use_tuned_settings;
 }
 
 void cras_apm_list_set_aec_dump(struct cras_apm_list *list, void *dev_ptr,

cras/src/server/cras_apm_list.h
@@ -143,6 +143,11 @@ void cras_apm_list_put_processed(struct cras_apm *apm, unsigned int frames);
  */
 struct cras_audio_format *cras_apm_list_get_format(struct cras_apm *apm);
 
+/*
+ * Gets if this apm instance is using tuned settings.
+ */
+bool cras_apm_list_get_use_tuned_settings(struct cras_apm *apm);
+
 /* Sets debug recording to start or stop.
  * Args:
  *    list - List contains the apm instance to start/stop debug recording.
@@ -226,6 +231,11 @@ static inline struct cras_audio_format *
 cras_apm_list_get_format(struct cras_apm *apm)
 {
 	return NULL;
+}
+
+static inline bool cras_apm_list_get_use_tuned_settings(struct cras_apm *apm)
+{
+	return 0;
 }
 
 static inline void cras_apm_list_set_aec_dump(struct cras_apm_list *list,

cras/src/server/dev_io.c
@@ -509,24 +509,12 @@ static int capture_to_streams(struct open_dev *adev)
 						  stream->stream,
 						  idev->buf_state, &area,
 						  &area_offset);
-			/*
-			 * The software gain scaler consist of two parts:
-			 * (1) The device gain scaler used when lack of hardware
-			 * gain control. Configured by the DefaultNodeGain label
-			 * in alsa UCM config.
-			 * (2) The gain scaler in cras_rstream set by app, for
-			 * example the AGC module in Chrome.
-			 *
-			 * APM has more advanced gain control mechanism, we shall
-			 * give APM total control of the captured samples without
-			 * additional gain scaler at all.
-			 */
+
 			software_gain_scaler =
-				stream->stream->apm_list ?
-					1.0f :
-					idev->software_gain_scaler *
-						cras_rstream_get_volume_scaler(
-							stream->stream);
+				input_data_get_software_gain_scaler(
+					idev->input_data,
+					idev->software_gain_scaler,
+					stream->stream);
 
 			this_read =
 				dev_stream_capture(stream, area, area_offset,

cras/src/server/input_data.c
@@ -191,3 +191,20 @@ int input_data_put_for_stream(struct input_data *data,
 
 	return 0;
 }
+
+float input_data_get_software_gain_scaler(struct input_data *data,
+					  float idev_sw_gain_scaler,
+					  struct cras_rstream *stream)
+{
+	struct cras_apm *apm;
+	/*
+	 * APM has more advanced gain control mechanism. If it is using tuned
+	 * settings, give APM total control of the captured samples without
+	 * additional gain scaler at all.
+	 */
+	apm = cras_apm_list_get_active_apm(stream, data->dev_ptr);
+	if (apm && cras_apm_list_get_use_tuned_settings(apm))
+		return 1.0f;
+
+	return idev_sw_gain_scaler * cras_rstream_get_volume_scaler(stream);
+}

cras/src/server/input_data.h
@@ -75,4 +75,21 @@ int input_data_put_for_stream(struct input_data *data,
 			      struct buffer_share *offsets,
 			      unsigned int frames);
 
+/*
+ * The software gain scaler of input path consist of two parts:
+ * (1) The device gain scaler used when lack of hardware gain control.
+ * Configured by the IntrinsicSensitivity label in alsa UCM config.
+ * (2) The gain scaler in cras_rstream set by app, for example the AGC
+ * module in Chrome.
+ * Args:
+ *    data - The input data that holds pointer to APM instance.
+ *    idev_sw_agin_scaler - The gain scaler configured on input iodev.
+ *    stream - To provide stream layer software gain.
+ * Returns:
+ *    1.0 if tuned APM in use, otherwise |iodev gain| * |cras_rstream gain|
+ */
+float input_data_get_software_gain_scaler(struct input_data *data,
+					  float idev_sw_gain_scaler,
+					  struct cras_rstream *stream);
+
 #endif /* INPUT_DATA_H_ */

cras/src/tests/dev_io_unittest.cc
@@ -27,6 +27,7 @@ struct audio_thread_event_log* atlog;
 #include "rstream_stub.h"
 
 static float dev_stream_capture_software_gain_scaler_val;
+static float input_data_get_software_gain_scaler_val;
 static unsigned int dev_stream_capture_avail_ret = 480;
 
 namespace {
@@ -74,18 +75,16 @@ TEST_F(DevIoSuite, CaptureGain) {
                                 CRAS_NODE_TYPE_MIC);
 
   dev->dev->state = CRAS_IODEV_STATE_NORMAL_RUN;
-  dev->dev->software_gain_scaler = 0.99f;
   iodev_stub_frames_queued(dev->dev.get(), 20, ts);
   DL_APPEND(dev_list, dev->odev.get());
   add_stream_to_dev(dev->dev, stream);
 
-  /* For stream that uses APM, always apply gain scaler 1.0f regardless of
-   * what node/stream gains are. */
-  stream->rstream->apm_list = reinterpret_cast<struct cras_apm_list*>(0xf0f);
+  /* The applied scaler gain should match what is reported by input_data. */
+  input_data_get_software_gain_scaler_val = 1.0f;
   dev_io_capture(&dev_list);
   EXPECT_EQ(1.0f, dev_stream_capture_software_gain_scaler_val);
 
-  stream->rstream->apm_list = 0x0;
+  input_data_get_software_gain_scaler_val = 0.99f;
   dev_io_capture(&dev_list);
   EXPECT_EQ(0.99f, dev_stream_capture_software_gain_scaler_val);
 }
@@ -202,6 +201,12 @@ int input_data_put_for_stream(struct input_data* data,
                               struct buffer_share* offsets,
                               unsigned int frames) {
   return 0;
+}
+
+float input_data_get_software_gain_scaler(struct input_data* data,
+                                          float idev_sw_gain_scaler,
+                                          struct cras_rstream* stream) {
+  return input_data_get_software_gain_scaler_val;
 }
 
 int cras_audio_thread_event_drop_samples() {

cras/src/tests/input_data_unittest.cc
@@ -14,12 +14,16 @@ extern "C" {
 
 namespace {
 
+#define FAKE_CRAS_APM_PTR reinterpret_cast<struct cras_apm*>(0x99)
+
 #ifdef HAVE_WEBRTC_APM
 static struct cras_audio_area apm_area;
 static unsigned int cras_apm_list_process_offset_val;
 static unsigned int cras_apm_list_process_called;
 static struct cras_apm* cras_apm_list_get_active_ret = NULL;
+static bool cras_apm_list_get_use_tuned_settings_val;
 #endif  // HAVE_WEBRTC_APM
+static float cras_rstream_get_volume_scaler_val;
 
 TEST(InputData, GetForInputStream) {
   void* dev_ptr = reinterpret_cast<void*>(0x123);
@@ -55,7 +59,7 @@ TEST(InputData, GetForInputStream) {
   EXPECT_EQ(600, offset);
 #ifdef HAVE_WEBRTC_APM
   EXPECT_EQ(0, cras_apm_list_process_called);
-  cras_apm_list_get_active_ret = reinterpret_cast<struct cras_apm*>(0x99);
+  cras_apm_list_get_active_ret = FAKE_CRAS_APM_PTR;
 #endif  // HAVE_WEBRTC_APM
 
   input_data_get_for_stream(data, &stream, offsets, &area, &offset);
@@ -73,6 +77,40 @@ TEST(InputData, GetForInputStream) {
 
   input_data_destroy(&data);
   buffer_share_destroy(offsets);
+}
+
+TEST(InputData, GetSWCaptureGain) {
+  void* dev_ptr = reinterpret_cast<void*>(0x123);
+  struct input_data* data = NULL;
+  struct cras_rstream stream;
+  float gain;
+
+  cras_rstream_get_volume_scaler_val = 0.8f;
+  stream.stream_id = 123;
+
+#ifdef HAVE_WEBRTC_APM
+  data = input_data_create(dev_ptr);
+
+  cras_apm_list_get_active_ret = FAKE_CRAS_APM_PTR;
+  cras_apm_list_get_use_tuned_settings_val = 1;
+  gain = input_data_get_software_gain_scaler(data, 0.7f, &stream);
+  EXPECT_FLOAT_EQ(1.0f, gain);
+
+  cras_apm_list_get_active_ret = NULL;
+  gain = input_data_get_software_gain_scaler(data, 0.7f, &stream);
+  EXPECT_FLOAT_EQ(0.56f, gain);
+
+  cras_apm_list_get_active_ret = FAKE_CRAS_APM_PTR;
+  cras_apm_list_get_use_tuned_settings_val = 0;
+  gain = input_data_get_software_gain_scaler(data, 0.6f, &stream);
+  EXPECT_FLOAT_EQ(0.48f, gain);
+  input_data_destroy(&data);
+#endif  // HAVE_WEBRTC_APM
+
+  data = input_data_create(dev_ptr);
+  gain = input_data_get_software_gain_scaler(data, 0.6f, &stream);
+  EXPECT_FLOAT_EQ(0.48f, gain);
+  input_data_destroy(&data);
 }
 
 extern "C" {
@@ -93,8 +131,14 @@ struct cras_audio_area* cras_apm_list_get_processed(struct cras_apm* apm) {
 }
 void cras_apm_list_remove_apm(struct cras_apm_list* list, void* dev_ptr) {}
 void cras_apm_list_put_processed(struct cras_apm* apm, unsigned int frames) {}
+bool cras_apm_list_get_use_tuned_settings(struct cras_apm* apm) {
+  return cras_apm_list_get_use_tuned_settings_val;
+}
 #endif  // HAVE_WEBRTC_APM
 
+float cras_rstream_get_volume_scaler(struct cras_rstream* rstream) {
+  return cras_rstream_get_volume_scaler_val;
+}
 }  // extern "C"
 }  // namespace
 

commit 930db7ee0f5cfa393e8d55bd0bc76562a78cd82d

CRAS: Fix undefined reference in {audio_thread,timing}_unittest
Build fails from
a563d702d439fdcef7fe8259a959604a73101d3a

BUG=None
TEST=Run commands under adhd/cras
     CC=clang CXX=clang++ ./configure
     make -j$(nproc) check

Change-Id: I990e0e9017412418222b17a1c5a1754980b0d8d1
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/2141574
Reviewed-by: Hsinyu Chao <hychao@chromium.org>
Commit-Queue: Chih-Yang Hsia <paulhsia@chromium.org>
Tested-by: Chih-Yang Hsia <paulhsia@chromium.org>

cras/src/tests/audio_thread_unittest.cc
@@ -1444,6 +1444,11 @@ int cras_audio_thread_event_drop_samples() {
   return 0;
 }
 
+float input_data_get_software_gain_scaler(struct input_data* data,
+                                          float idev_sw_gain_scaler,
+                                          struct cras_rstream* stream) {
+  return 1.0;
+}
 }  // extern "C"
 
 int main(int argc, char** argv) {

cras/src/tests/timing_unittest.cc
@@ -1163,6 +1163,12 @@ int input_data_put_for_stream(struct input_data* data,
   return 0;
 }
 
+float input_data_get_software_gain_scaler(struct input_data* data,
+                                          float idev_sw_gain_scaler,
+                                          struct cras_rstream* stream) {
+  return 1.0;
+}
+
 struct cras_audio_format* cras_rstream_post_processing_format(
     const struct cras_rstream* stream,
     void* dev_ptr) {