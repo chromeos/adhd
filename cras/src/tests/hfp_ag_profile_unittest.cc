chromeos/adhd

commit 52e2d8861ab53045f8d760de0ba33b0f7fc5369a

CRAS: hfp_ag - Allow re-register to HFP 1.7
Add NextHandsfreeProfile dbus message to allow UI to switch HFP
version registered with BlueZ.  This utility will make it easier
for Bluetooth HFP qualification process to HFP 1.7 which includes
wideband speech feature, as well as allowing end users to enable
WBS feature from UI.

BUG=b:149188379
TEST=dbus-send --system --type=method_call --print-reply \
      --dest=org.chromium.cras /org/chromium/cras \
      org.chromium.cras.Control.NextHandsfreeProfile boolean:true
to verify WBS is enabled.
dbus-send --system --type=method_call --print-reply \
      --dest=org.chromium.cras /org/chromium/cras \
      org.chromium.cras.Control.NextHandsfreeProfile boolean:false
to verify WBS is disabled.

Change-Id: I1b315a28ea36eaee1f4e9cee1c3dee1563edd502
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/2073978
Reviewed-by: En-Shuo Hsu <enshuo@chromium.org>
Tested-by: Hsinyu Chao <hychao@chromium.org>
Commit-Queue: Hsinyu Chao <hychao@chromium.org>

cras/src/server/cras_bt_profile.c
@@ -362,6 +362,46 @@ int cras_bt_register_profile(DBusConnection *conn,
 	return 0;
 }
 
+int cras_bt_unregister_profile(DBusConnection *conn,
+			       struct cras_bt_profile *profile)
+{
+	DBusMessage *method_call;
+	DBusMessageIter message_iter;
+	DBusError dbus_error;
+	DBusMessage *reply;
+
+	method_call = dbus_message_new_method_call(BLUEZ_SERVICE,
+						   PROFILE_MANAGER_OBJ_PATH,
+						   BLUEZ_PROFILE_MGMT_INTERFACE,
+						   "UnregisterProfile");
+
+	if (!method_call)
+		return -ENOMEM;
+	dbus_error_init(&dbus_error);
+	dbus_message_iter_init_append(method_call, &message_iter);
+	dbus_message_iter_append_basic(&message_iter, DBUS_TYPE_OBJECT_PATH,
+				       &profile->object_path);
+	reply = dbus_connection_send_with_reply_and_block(
+		conn, method_call, DBUS_TIMEOUT_USE_DEFAULT, &dbus_error);
+
+	if (!reply) {
+		dbus_error_free(&dbus_error);
+		dbus_message_unref(method_call);
+		return -EIO;
+	}
+
+	dbus_message_unref(method_call);
+
+	if (dbus_message_get_type(reply) == DBUS_MESSAGE_TYPE_ERROR) {
+		syslog(LOG_ERR, "Unregister profile returned error: %s",
+		       dbus_message_get_error_name(reply));
+		dbus_message_unref(reply);
+		return -EIO;
+	}
+	dbus_message_unref(reply);
+	return 0;
+}
+
 int cras_bt_register_profiles(DBusConnection *conn)
 {
 	struct cras_bt_profile *profile;
@@ -396,6 +436,19 @@ int cras_bt_add_profile(DBusConnection *conn, struct cras_bt_profile *profile)
 
 	DL_APPEND(profiles, profile);
 
+	return 0;
+}
+
+int cras_bt_rm_profile(DBusConnection *conn, struct cras_bt_profile *profile)
+{
+	DL_DELETE(profiles, profile);
+
+	if (!dbus_connection_unregister_object_path(conn,
+						    profile->object_path)) {
+		syslog(LOG_ERR, "Could not unregister BT profile %s",
+		       profile->object_path);
+		return -ENOMEM;
+	}
 	return 0;
 }
 

cras/src/server/cras_bt_profile.h
@@ -41,6 +41,13 @@ struct cras_bt_profile {
  */
 int cras_bt_add_profile(DBusConnection *conn, struct cras_bt_profile *profile);
 
+/* Removes |profile| from the list of profiles registered with bluez.
+ * Args:
+ *    conn - The dbus connection.
+ *    profile - Pointer to the profile structure to be removed.
+ */
+int cras_bt_rm_profile(DBusConnection *conn, struct cras_bt_profile *profile);
+
 /* Gets the profile by object path.
  * Args:
  *    path - The object path of the desired profile.
@@ -56,6 +63,22 @@ void cras_bt_profile_reset();
 
 /* Notifies all profiles when a device is disconnected. */
 void cras_bt_profile_on_device_disconnected(struct cras_bt_device *device);
+
+/* Registeres |profile| with bluez.
+ * Args:
+ *    conn - The dbus connection.
+ *    profile - Pointer to the profile structure to be registered.
+ */
+int cras_bt_register_profile(DBusConnection *conn,
+			     struct cras_bt_profile *profile);
+
+/* Unregisteres |profile| with bluez.
+ * Args:
+ *    conn - The dbus connection.
+ *    profile - Pointer to the profile structure to be unregistered.
+ */
+int cras_bt_unregister_profile(DBusConnection *conn,
+			       struct cras_bt_profile *profile);
 
 /* Registers all added profiles.
  * Args:

cras/src/server/cras_dbus_control.c
@@ -15,6 +15,7 @@
 #include "cras_dbus.h"
 #include "cras_dbus_control.h"
 #include "cras_dbus_util.h"
+#include "cras_hfp_ag_profile.h"
 #include "cras_iodev_list.h"
 #include "cras_observer.h"
 #include "cras_system_state.h"
@@ -90,6 +91,9 @@
 	"    </method>\n"                                                       \
 	"    <method name=\"RemoveActiveOutputNode\">\n"                        \
 	"      <arg name=\"node_id\" type=\"t\" direction=\"in\"/>\n"           \
+	"    </method>\n"                                                       \
+	"    <method name=\"NextHandsfreeProfile\">\n"                          \
+	"      <arg name=\"toggle\" type=\"b\" direction=\"in\"/>\n"            \
 	"    </method>\n"                                                       \
 	"    <method name=\"GetNumberOfActiveStreams\">\n"                      \
 	"      <arg name=\"num\" type=\"i\" direction=\"out\"/>\n"              \
@@ -720,6 +724,28 @@ handle_rm_active_node(DBusConnection *conn, DBusMessage *message, void *arg,
 	return DBUS_HANDLER_RESULT_HANDLED;
 }
 
+static DBusHandlerResult handle_next_handsfree_profile(DBusConnection *conn,
+						       DBusMessage *message,
+						       void *arg)
+{
+	int rc;
+	dbus_bool_t enabled;
+
+	rc = get_single_arg(message, DBUS_TYPE_BOOLEAN, &enabled);
+	if (rc)
+		return rc;
+
+	/* Change HFP version to register with BlueZ and the
+	 * wbs enabled flag for codec negotiation in SLC.
+	 */
+	cras_hfp_ag_profile_next_handsfree(conn, enabled);
+	cras_system_set_bt_wbs_enabled(enabled);
+
+	send_empty_reply(conn, message);
+
+	return DBUS_HANDLER_RESULT_HANDLED;
+}
+
 static DBusHandlerResult handle_get_num_active_streams(DBusConnection *conn,
 						       DBusMessage *message,
 						       void *arg)
@@ -1042,6 +1068,9 @@ static DBusHandlerResult handle_control_message(DBusConnection *conn,
 					       "RemoveActiveOutputNode")) {
 		return handle_rm_active_node(conn, message, arg,
 					     CRAS_STREAM_OUTPUT);
+	} else if (dbus_message_is_method_call(message, CRAS_CONTROL_INTERFACE,
+					       "NextHandsfreeProfile")) {
+		return handle_next_handsfree_profile(conn, message, arg);
 	} else if (dbus_message_is_method_call(message, CRAS_CONTROL_INTERFACE,
 					       "GetNumberOfActiveStreams")) {
 		return handle_get_num_active_streams(conn, message, arg);

cras/src/server/cras_hfp_ag_profile.c
@@ -25,6 +25,7 @@
 #define HFP_AG_PROFILE_NAME "Hands-Free Voice gateway"
 #define HFP_AG_PROFILE_PATH "/org/chromium/Cras/Bluetooth/HFPAG"
 #define HFP_VERSION_1_5 0x0105
+#define HFP_VERSION_1_7 0x0107
 #define HSP_AG_PROFILE_NAME "Headset Voice gateway"
 #define HSP_AG_PROFILE_PATH "/org/chromium/Cras/Bluetooth/HSPAG"
 #define HSP_VERSION_1_2 0x0102
@@ -76,6 +77,7 @@
 
 /* The "SupportedFeatures" attribute value of HFP AG service record in CRAS. */
 #define SDP_SUPPORTED_FEATURES 0
+#define SDP_SUPPORTED_FEATURES_1_7 FEATURES_AG_WIDE_BAND_SPEECH
 
 /* Object representing the audio gateway role for HFP/HSP.
  * Members:
@@ -311,6 +313,30 @@ static struct cras_bt_profile cras_hfp_ag_profile = {
 	.request_disconnection = cras_hfp_ag_request_disconnection,
 	.cancel = cras_hfp_ag_cancel
 };
+
+int cras_hfp_ag_profile_next_handsfree(DBusConnection *conn, bool enabled)
+{
+	unsigned int target_version = HFP_VERSION_1_5;
+	unsigned int target_feature = SDP_SUPPORTED_FEATURES;
+
+	if (enabled) {
+		target_version = HFP_VERSION_1_7;
+		target_feature = SDP_SUPPORTED_FEATURES_1_7;
+	}
+
+	if (cras_hfp_ag_profile.version == target_version)
+		return 0;
+
+	cras_bt_unregister_profile(conn, &cras_hfp_ag_profile);
+	cras_bt_rm_profile(conn, &cras_hfp_ag_profile);
+
+	cras_hfp_ag_profile.features = target_feature;
+	cras_hfp_ag_profile.version = target_version;
+
+	cras_bt_add_profile(conn, &cras_hfp_ag_profile);
+	cras_bt_register_profile(conn, &cras_hfp_ag_profile);
+	return 0;
+}
 
 int cras_hfp_ag_profile_create(DBusConnection *conn)
 {

cras/src/server/cras_hfp_ag_profile.h
@@ -7,6 +7,7 @@
 #define CRAS_HFP_AG_PROFILE_H_
 
 #include <dbus/dbus.h>
+#include <stdbool.h>
 
 #include "cras_bt_device.h"
 #include "cras_hfp_slc.h"
@@ -24,6 +25,12 @@
 #define FEATURES_AG_WIDE_BAND_SPEECH 0x0020
 
 struct hfp_slc_handle;
+
+/* Re-registers HFP to the next version. Currently on HFP 1.5 and next is 1.7.
+ * Args:
+ *    enabled - True to register HFP 1.7 otherwise to HFP 1.5
+ */
+int cras_hfp_ag_profile_next_handsfree(DBusConnection *conn, bool enabled);
 
 /* Adds a profile instance for HFP AG (Hands-Free Profile Audio Gateway). */
 int cras_hfp_ag_profile_create(DBusConnection *conn);

commit daadef625229f0be8fec7243d49f42aa9aca0d54

CRAS: Fix undefined reference in hfp_ag_profile_unittest
Build fails from commit:
52e2d8861ab53045f8d760de0ba33b0f7fc5369a

BUG=None
TEST=Run commands under adhd/cras
     CC=clang CXX=clang++ ./configure
     make -j$(nproc) check

Change-Id: I2e60be8bd05210bec197ab22741b8ea543037047
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/2141573
Reviewed-by: Hsinyu Chao <hychao@chromium.org>
Commit-Queue: Chih-Yang Hsia <paulhsia@chromium.org>
Tested-by: Chih-Yang Hsia <paulhsia@chromium.org>

cras/src/tests/hfp_ag_profile_unittest.cc
@@ -167,6 +167,25 @@ int cras_bt_add_profile(DBusConnection* conn, struct cras_bt_profile* profile) {
   return 0;
 }
 
+int cras_bt_rm_profile(DBusConnection* conn, struct cras_bt_profile* profile) {
+  internal_bt_profile = NULL;
+  return 0;
+}
+
+int cras_bt_register_profile(DBusConnection* conn,
+                             struct cras_bt_profile* profile) {
+  return 0;
+}
+
+int cras_bt_register_profiles(DBusConnection* conn) {
+  return 0;
+}
+
+int cras_bt_unregister_profile(DBusConnection* conn,
+                               struct cras_bt_profile* profile) {
+  return 0;
+}
+
 struct hfp_info* hfp_info_create() {
   return NULL;
 }