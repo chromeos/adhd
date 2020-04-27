420931b950146ab05a1a919a5e1e61c8a3cff795
paulhsia <paulhsia@chromium.org>
2020-04-28 03:24:42 +0800

    CRAS: Add cras_unified_rclient supports playback-capture
    Add a new client supports only playback and capture functions.
    
    BUG=b:155048379
    TEST=Apply patch test and test with cras_test_client
    
    Change-Id: If70f32ef6fcae5423dbb3d3d241e12f6c868395a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/2167816
    Reviewed-by: Dylan Reid <dgreid@chromium.org>
    Tested-by: Chih-Yang Hsia <paulhsia@chromium.org>
    Commit-Queue: Chih-Yang Hsia <paulhsia@chromium.org>

---
+++ cras/src/server/cras_unified_rclient.h
@@ -0,0 +1,21 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef CRAS_UNIFIED_RCLIENT_H_
+#define CRAS_UNIFIED_RCLIENT_H_
+
+struct cras_rclient;
+
+/* Creates a unified rclient structure. This client supports only playback
+ * and capture functions but not control features.
+ * Args:
+ *    fd - The file descriptor used for communication with the client.
+ *    id - Unique identifier for this client.
+ * Returns:
+ *    A pointer to the newly created rclient on success, NULL on failure.
+ */
+struct cras_rclient *cras_unified_rclient_create(int fd, size_t id);
+
+#endif /* CRAS_UNIFIED_RCLIENT_H_ */
