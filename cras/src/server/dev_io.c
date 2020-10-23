chromeos/adhd

commit bf22f07caef5f2836a8c74ab673f461162692726

CRAS: rstream - Log audio energy in output streams
Similar approach as media/base/audio_power_monitor.cc that
calculates energy using EWMA(exponentially-weighted moving
average) power from media/base/vector_math.cc

In this version, we downsample the audio frames by picking one
sample every |step| frames, and then calculate the EWMA power.

Note on choosing the sf value(smooth factor). We use the same
calculation as used in Chrome but down sampled to one sample
every 1ms. There's no performance impact observed in the cycles
count from 'perf stats'. However if changing to one sample per
0.5ms we can see cycles increase around 1%.

BUG=b:164443860
TEST=cras_test_client --dump_a | grep FETCH

Change-Id: Ifda7de0ef49257a610b1a3add29a3bd643032683
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/2461087
Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>
Tested-by: Hsinyu Chao <hychao@chromium.org>
Commit-Queue: Cheng-Yi Chiang <cychiang@chromium.org>

cras/src/server/dev_io.c
@@ -194,7 +194,8 @@ static int fetch_streams(struct open_dev *adev)
 		dev_stream_set_delay(dev_stream, delay);
 
 		ATLOG(atlog, AUDIO_THREAD_FETCH_STREAM, rstream->stream_id,
-		      cras_rstream_get_cb_threshold(rstream), delay);
+		      cras_rstream_get_cb_threshold(rstream),
+		      *((uint32_t *)&rstream->ewma.power));
 
 		rc = dev_stream_request_playback_samples(dev_stream, &now);
 		if (rc < 0) {

cras/src/tools/cras_test_client/cras_test_client.c
@@ -588,10 +588,16 @@ static void show_alog_tag(const struct audio_thread_event_log *log,
 		printf("%-30s id:%x shm_frames:%u cb_pending:%u\n",
 		       "WRITE_STREAMS_STREAM", data1, data2, data3);
 		break;
-	case AUDIO_THREAD_FETCH_STREAM:
-		printf("%-30s id:%x cbth:%u delay:%u\n",
-		       "WRITE_STREAMS_FETCH_STREAM", data1, data2, data3);
+	case AUDIO_THREAD_FETCH_STREAM: {
+		float f = *((float *)&data3);
+		/* Convert to dBFS and set to zero if it's insignificantly low.
+		 * Picking the the same threshold 1.0e-10f as in Chrome.
+		 */
+		f = (f < 1.0e-10f) ? 0.0f : 10.0f * log10f(f);
+		printf("%-30s id:%x cbth:%u power:%f dBFS\n",
+		       "WRITE_STREAMS_FETCH_STREAM", data1, data2, f);
 		break;
+	}
 	case AUDIO_THREAD_STREAM_ADDED:
 		printf("%-30s id:%x dev:%u\n", "STREAM_ADDED", data1, data2);
 		break;

cras/src/Makefile.am
@@ -155,6 +155,7 @@ cras_server_SOURCES = \
 	server/cras_volume_curve.c \
 	server/dev_io.c \
 	server/dev_stream.c \
+	server/ewma_power.c \
 	server/input_data.c \
 	server/linear_resampler.c \
 	server/polled_interval_checker.c \
@@ -437,6 +438,7 @@ TESTS = \
 	edid_utils_unittest \
 	empty_iodev_unittest \
 	expr_unittest \
+	ewma_power_unittest \
 	file_wait_unittest \
 	float_buffer_unittest \
 	fmt_conv_unittest \
@@ -879,6 +881,12 @@ buffer_share_unittest_SOURCES = tests/buffer_share_unittest.cc \
 buffer_share_unittest_CPPFLAGS = $(COMMON_CPPFLAGS) \
 	-I$(top_srcdir)/src/common -I$(top_srcdir)/src/server
 buffer_share_unittest_LDADD = -lgtest -liniparser -lpthread
+
+ewma_power_unittest_SOURCES = tests/ewma_power_unittest.cc \
+	server/ewma_power.c
+ewma_power_unittest_CPPFLAGS = $(COMMON_CPPFLAGS) \
+	-I$(top_srcdir)/src/common -I$(top_srcdir)/src/server
+ewma_power_unittest_LDADD = -lgtest
 
 iodev_list_unittest_SOURCES = tests/iodev_list_unittest.cc \
 	server/cras_iodev_list.c

cras/src/server/cras_rstream.c
@@ -292,6 +292,7 @@ int cras_rstream_create(struct cras_rstream_config *config,
 	stream->num_missed_cb = 0;
 	stream->is_pinned = (config->dev_idx != NO_DEVICE);
 	stream->pinned_dev_idx = config->dev_idx;
+	ewma_power_init(&stream->ewma, stream->format.frame_rate);
 
 	rc = setup_shm_area(stream, config);
 	if (rc < 0) {
@@ -472,9 +473,16 @@ void cras_rstream_update_input_write_pointer(struct cras_rstream *rstream)
 
 void cras_rstream_update_output_read_pointer(struct cras_rstream *rstream)
 {
+	size_t nfr = 0;
+	uint8_t *src;
 	unsigned int nwritten =
 		buffer_share_get_new_write_point(rstream->buf_state);
 
+	/* Retrieve the read pointer |src| start from which to calculate
+	 * the EWMA power. */
+	src = cras_shm_get_readable_frames(rstream->shm, 0, &nfr);
+	ewma_power_calculate(&rstream->ewma, (int16_t *)src,
+			     rstream->format.num_channels, nwritten);
 	cras_shm_buffer_read(rstream->shm, nwritten);
 }
 

cras/src/server/cras_rstream.h
@@ -14,6 +14,7 @@
 #include "cras_shm.h"
 #include "cras_types.h"
 #include "cras_rstream_config.h"
+#include "ewma_power.h"
 
 struct cras_connect_message;
 struct cras_rclient;
@@ -55,6 +56,7 @@ struct master_dev_info {
  *    first_missed_cb_ts - The time when the first missed callback happens.
  *    buf_state - State of the buffer from all devices for this stream.
  *    apm_list - List of audio processing module instances.
+ *    ewma - The ewma instance to calculate stream volume.
  *    num_attached_devs - Number of iodevs this stream has attached to.
  *    num_missed_cb - Number of callback schedules have been missed.
  *    queued_frames - Cached value of the number of queued frames in shm.
@@ -85,6 +87,7 @@ struct cras_rstream {
 	struct timespec first_missed_cb_ts;
 	struct buffer_share *buf_state;
 	struct cras_apm_list *apm_list;
+	struct ewma_power ewma;
 	int num_attached_devs;
 	int num_missed_cb;
 	int queued_frames;

cras/src/server/ewma_power.c
@@ -1,0 +1,46 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "ewma_power.h"
+#include "math.h"
+
+/* One sample per 1ms. */
+#define EWMA_SAMPLE_RATE 1000
+
+/* Smooth factor for EWMA, 1 - expf(-1.0/(rate * 0.01))
+ * where the 0.01 corresponds to 10ms interval that is chosen and
+ * being used in Chrome for a long time.
+ * Here the |rate| is set to the down sampled EWMA_SAMPLE_RATE and
+ * whenever it changes the calculated |smooth_factor| should be updated
+ * accordingly.
+ */
+const static float smooth_factor = 0.095;
+
+void ewma_power_init(struct ewma_power *ewma, unsigned int rate)
+{
+	ewma->power_set = 0;
+	ewma->step_fr = rate / EWMA_SAMPLE_RATE;
+}
+
+void ewma_power_calculate(struct ewma_power *ewma, const int16_t *buf,
+			  unsigned int channels, unsigned int size)
+{
+	int i, ch;
+	float power, f;
+	for (i = 0; i < size; i += ewma->step_fr * channels) {
+		power = 0.0f;
+		for (ch = 0; ch < channels; ch++) {
+			f = buf[i + ch] / 32768.0f;
+			power = f * f / channels;
+		}
+		if (!ewma->power_set) {
+			ewma->power = power;
+			ewma->power_set = 1;
+		} else {
+			ewma->power = smooth_factor * power +
+				      (1 - smooth_factor) * ewma->power;
+		}
+	}
+}

cras/src/server/ewma_power.h
@@ -1,0 +1,47 @@
+/* Copyright 2020 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef EWMA_POWER_H_
+#define EWMA_POWER_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/*
+ * The exponentially weighted moving average power module used to
+ * calculate the energe level in audio stream.
+ * Members:
+ *    power_set - Flag to note if the first power value has set.
+ *    power - The power value.
+ *    step_fr - How many frames to sample one for EWMA calculation.
+ */
+struct ewma_power {
+	bool power_set;
+	float power;
+	unsigned int step_fr;
+};
+
+/*
+ * Initializes the ewma_power object.
+ * Args:
+ *    ewma - The ewma_power object to initialize.
+ *    rate - The sample rate of the audio data that the ewma object
+ *        will calculate power from.
+ */
+void ewma_power_init(struct ewma_power *ewma, unsigned int rate);
+
+/*
+ * Feeds an audio buffer to ewma_power object to calculate the
+ * latest power value.
+ * Args:
+ *    ewma - The ewma_power object to calculate power.
+ *    buf - Pointer to the audio data.
+ *    channels - Number of channels of the audio data.
+ *    size - Length in frames of the audio data.
+ */
+void ewma_power_calculate(struct ewma_power *ewma, const int16_t *buf,
+			  unsigned int channels, unsigned int size);
+
+#endif /* EWMA_POWER_H_ */

cras/src/tests/ewma_power_unittest.cc
@@ -1,0 +1,74 @@
+// Copyright (c) 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "ewma_power.h"
+}
+
+namespace {
+
+TEST(EWMAPower, RelativePowerValue) {
+  struct ewma_power ewma;
+  int16_t buf[480];
+  float f;
+  int i;
+
+  for (i = 0; i < 480; i++)
+    buf[i] = 0x00fe;
+
+  ewma_power_init(&ewma, 48000);
+  EXPECT_EQ(48, ewma.step_fr);
+
+  ewma_power_calculate(&ewma, buf, 1, 480);
+  EXPECT_LT(0.0f, ewma.power);
+
+  // After 10ms of silence the power value decreases.
+  f = ewma.power;
+  for (i = 0; i < 480; i++)
+    buf[i] = 0x00;
+  ewma_power_calculate(&ewma, buf, 1, 480);
+  EXPECT_LT(ewma.power, f);
+
+  // After 300ms of silence the power value decreases to insignificant low.
+  for (i = 0; i < 30; i++)
+    ewma_power_calculate(&ewma, buf, 1, 480);
+  EXPECT_LT(ewma.power, 1.0e-10);
+}
+
+TEST(EWMAPower, PowerInStereoData) {
+  struct ewma_power ewma;
+  int16_t buf[960];
+  int i;
+  float f;
+
+  ewma_power_init(&ewma, 48000);
+
+  for (i = 0; i < 960; i += 2) {
+    buf[i] = 0x0;
+    buf[i + 1] = 0x00fe;
+  }
+  ewma_power_calculate(&ewma, buf, 2, 480);
+  EXPECT_LT(0.0f, ewma.power);
+
+  // After 10ms of silence the power value decreases.
+  f = ewma.power;
+  for (i = 0; i < 960; i++)
+    buf[i] = 0x0;
+  ewma_power_calculate(&ewma, buf, 2, 480);
+  EXPECT_LT(ewma.power, f);
+
+  // After 300ms of silence the power value decreases to insignificant low.
+  for (i = 0; i < 30; i++)
+    ewma_power_calculate(&ewma, buf, 2, 480);
+  EXPECT_LT(ewma.power, 1.0e-10);
+}
+
+}  // namespace
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}

commit c8455b6b66a50ea38f604dbd250ef96f145f12d7

CRAS: Fix strict aliasing violation
Casting a float pointer to a uint32_t pointer and dereferencing it is
undefined behavior, use memcpy and a temp instead. The compiler _should_
optimized the four byte memcpy to an assignment if possible.

Fixes: bf22f07ca ("CRAS: rstream - Log audio energy in output streams")
BUG: 171516669
Change-Id: I511cb089e617d21710d61555b469ce561797d44e
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/2493190
Reviewed-by: Hsinyu Chao <hychao@chromium.org>
Tested-by: Dylan Reid <dgreid@chromium.org>
Commit-Queue: Dylan Reid <dgreid@chromium.org>

cras/src/server/dev_io.c
@@ -193,9 +193,18 @@ static int fetch_streams(struct open_dev *adev)
 
 		dev_stream_set_delay(dev_stream, delay);
 
-		ATLOG(atlog, AUDIO_THREAD_FETCH_STREAM, rstream->stream_id,
-		      cras_rstream_get_cb_threshold(rstream),
-		      *((uint32_t *)&rstream->ewma.power));
+		// The log only accepts uint32 arguments, so the float power
+		// must be written as bits and assumed to have a float when
+		// parsing the log.
+		if (sizeof(uint32_t) == sizeof(float)) {
+			uint32_t pow_as_int;
+			memcpy(&pow_as_int, &rstream->ewma.power,
+			       sizeof(uint32_t));
+			ATLOG(atlog, AUDIO_THREAD_FETCH_STREAM,
+			      rstream->stream_id,
+			      cras_rstream_get_cb_threshold(rstream),
+			      pow_as_int);
+		}
 
 		rc = dev_stream_request_playback_samples(dev_stream, &now);
 		if (rc < 0) {

cras/src/tools/cras_test_client/cras_test_client.c
@@ -589,13 +589,18 @@ static void show_alog_tag(const struct audio_thread_event_log *log,
 		       "WRITE_STREAMS_STREAM", data1, data2, data3);
 		break;
 	case AUDIO_THREAD_FETCH_STREAM: {
-		float f = *((float *)&data3);
-		/* Convert to dBFS and set to zero if it's insignificantly low.
-		 * Picking the the same threshold 1.0e-10f as in Chrome.
-		 */
-		f = (f < 1.0e-10f) ? 0.0f : 10.0f * log10f(f);
-		printf("%-30s id:%x cbth:%u power:%f dBFS\n",
-		       "WRITE_STREAMS_FETCH_STREAM", data1, data2, f);
+		// Convert from the uint32_t log type back to float.
+		if (sizeof(uint32_t) == sizeof(float)) {
+			float f;
+			memcpy(&f, &data3, sizeof(float));
+			/* Convert to dBFS and set to zero if it's
+			 * insignificantly low.  Picking the the same threshold
+			 * 1.0e-10f as in Chrome.
+		         */
+			f = (f < 1.0e-10f) ? 0.0f : 10.0f * log10f(f);
+			printf("%-30s id:%x cbth:%u power:%f dBFS\n",
+			       "WRITE_STREAMS_FETCH_STREAM", data1, data2, f);
+		}
 		break;
 	}
 	case AUDIO_THREAD_STREAM_ADDED: