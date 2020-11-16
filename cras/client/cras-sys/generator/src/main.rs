70c7e1ddd140ec61f622104f86d253996dfc0d85
Fletcher Woodruff <fletcherw@chromium.org>
2020-11-16 11:31:06 -0700

    CRAS: regenerate rust bindings
    Fix the cras-sys binding generator by adding the new header file,
    packet_status_logger.h
    
    Update the header to include "#![allow(clippy::unreadable_literal)]" and
    "#![allow(clippy::cognitive_complexity)]" to silence some unhelpful
    clippy lints.
    
    Rerun the generator to update the generated bindings to reflect changes.
    
    BUG=None
    TEST=None
    
    Change-Id: Id06c125320530c5f355a5c0d488e178313f535af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/2541843
    Reviewed-by: Chih-Yang Hsia <paulhsia@chromium.org>
    Tested-by: Chih-Yang Hsia <paulhsia@chromium.org>
    Commit-Queue: Chih-Yang Hsia <paulhsia@chromium.org>

--- cras/client/cras-sys/generator/src/main.rs
+++ cras/client/cras-sys/generator/src/main.rs
@@ -36,6 +36,7 @@ fn copy_headers(src_dir: &Path, dst_dir: &Path) -> Result<(), String> {
         "cras_shm.h",
         "cras_types.h",
         "cras_util.h",
+        "packet_status_logger.h",
     ];
 
     for header in &header_files {
@@ -133,7 +134,11 @@ fn write_output(output_path: &Path, output: String) -> std::io::Result<()> {
  * cras_shm.h
  * cras_types.h
  * cras_util.h
+ * packet_status_logger.h
  */
+
+#![allow(clippy::unreadable_literal)]
+#![allow(clippy::cognitive_complexity)]
 ";
 
     let mut output_file = File::create(output_path)?;
