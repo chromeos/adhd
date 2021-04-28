08bbdb48beb102fb358c1ddf1b8947b235d9e560
paulhsia <paulhsia@chromium.org>
2021-04-28 22:56:51 +0800

    cras-sys: Use bindgen binary in build.rs
    Use bindgen binary in build.rs instead of the library to prevent
    re-building bindgen.
    
    Changes:
    - bindgen binary accept only one header, add cras_bindgen.h for
      `bindgen` command
    - Use Command and calls to bindgen directly
    
    BUG=b:186004820
    TEST=emerge-{BOARD} bindgen cras-sys libcras
    TEST=`cargo build` in chroot
    TEST=`vimdiff gen_from_old.rs gen_from_new.rs`
    
    Cq-Depend: chromium:2855152
    Change-Id: I2fa1514a0af2d64b6373d219570099787a93cb09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/2856170
    Tested-by: Chih-Yang Hsia <paulhsia@chromium.org>
    Reviewed-by: Allen Webb <allenwebb@google.com>
    Reviewed-by: Chih-Yang Hsia <paulhsia@chromium.org>
    Commit-Queue: Chih-Yang Hsia <paulhsia@chromium.org>

---
+++ cras/src/common/cras_bindgen.h
@@ -0,0 +1,14 @@
+/* Copyright 2021 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+/* headers for bindgen call from cras-sys. */
+#ifndef CRAS_BINDGEN_H_
+#define CRAS_BINDGEN_H_
+
+#include "cras_messages.h"
+#include "cras_types.h"
+#include "cras_audio_format.h"
+#include "cras_shm.h"
+
+#endif /* CRAS_BINDGEN_H_ */
