chromeos/adhd

commit 23e014e46d3b85160b45401a9ce5ad6566212de9

CRAS: bt_policy - Move schedule/cancel suspend to policy
Move the schedule and cancel suspend timer API from
cras_bt_device to the policy module. This commit doesn't
change any behavior. It allows the interface between
bt device and policy to be tested.

BUG=b:202505651
TEST=unittest

Change-Id: I8e6f0f595a5c5065386d79fb74e9b85a61e564f2
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/3213993
Tested-by: Hsinyu Chao <hychao@chromium.org>
Commit-Queue: Hsinyu Chao <hychao@chromium.org>
Reviewed-by: En-Shuo Hsu <enshuo@chromium.org>

cras/src/server/cras_bt_device.c
@@ -31,7 +31,6 @@
 #include "cras_hfp_slc.h"
 #include "cras_iodev.h"
 #include "cras_iodev_list.h"
-#include "cras_main_message.h"
 #include "cras_server_metrics.h"
 #include "cras_system_state.h"
 #include "cras_string.h"
@@ -64,20 +63,6 @@ static const unsigned int SCO_SUSPEND_DELAY_MS = 5000;
 
 static const unsigned int CRAS_SUPPORTED_PROFILES =
 	CRAS_BT_DEVICE_PROFILE_A2DP_SINK | CRAS_BT_DEVICE_PROFILE_HFP_HANDSFREE;
-
-enum BT_DEVICE_COMMAND {
-	BT_DEVICE_CANCEL_SUSPEND,
-	BT_DEVICE_SCHEDULE_SUSPEND,
-};
-
-struct bt_device_msg {
-	struct cras_main_message header;
-	enum BT_DEVICE_COMMAND cmd;
-	struct cras_bt_device *device;
-	struct cras_iodev *dev;
-	unsigned int arg1;
-	unsigned int arg2;
-};
 
 static struct cras_bt_device *devices;
 
@@ -234,8 +219,6 @@ static void cras_bt_device_destroy(struct cras_bt_device *device)
 
 	if (device->conn_watch_timer)
 		cras_tm_cancel_timer(tm, device->conn_watch_timer);
-	if (device->suspend_timer)
-		cras_tm_cancel_timer(tm, device->suspend_timer);
 	free(device->adapter_obj_path);
 	free(device->object_path);
 	free(device->address);
@@ -516,10 +499,6 @@ cras_bt_device_is_profile_connected(const struct cras_bt_device *device,
 	return !!(device->connected_profiles & profile);
 }
 
-static void
-bt_device_schedule_suspend(struct cras_bt_device *device, unsigned int msec,
-			   enum cras_bt_device_suspend_reason suspend_reason);
-
 /* Callback used to periodically check if supported profiles are connected. */
 static void bt_device_conn_watch_cb(struct cras_timer *timer, void *arg)
 {
@@ -586,8 +565,8 @@ static void bt_device_conn_watch_cb(struct cras_timer *timer, void *arg)
 		if (rc) {
 			syslog(LOG_ERR, "Start audio gateway failed, rc %d",
 			       rc);
-			bt_device_schedule_suspend(device, 0,
-						   HFP_AG_START_FAILURE);
+			cras_bt_policy_schedule_suspend(device, 0,
+							HFP_AG_START_FAILURE);
 		}
 	}
 	bt_device_set_nodes_plugged(device, 1);
@@ -604,7 +583,7 @@ arm_retry_timer:
 					     bt_device_conn_watch_cb, device);
 	} else {
 		syslog(LOG_ERR, "Connection watch timeout.");
-		bt_device_schedule_suspend(device, 0, CONN_WATCH_TIME_OUT);
+		cras_bt_policy_schedule_suspend(device, 0, CONN_WATCH_TIME_OUT);
 	}
 }
 
@@ -621,8 +600,6 @@ cras_bt_device_start_new_conn_watch_timer(struct cras_bt_device *device)
 		tm, CONN_WATCH_PERIOD_MS, bt_device_conn_watch_cb, device);
 }
 
-static void bt_device_cancel_suspend(struct cras_bt_device *device);
-
 void cras_bt_device_set_connected(struct cras_bt_device *device, int value)
 {
 	struct cras_tm *tm = cras_system_state_get_tm();
@@ -638,7 +615,7 @@ void cras_bt_device_set_connected(struct cras_bt_device *device, int value)
 		/* Device is disconnected, resets connected profiles and the
 		 * suspend timer which scheduled earlier. */
 		device->connected_profiles = 0;
-		bt_device_cancel_suspend(device);
+		cras_bt_policy_cancel_suspend(device);
 	}
 
 	device->connected = value;
@@ -663,8 +640,8 @@ void cras_bt_device_notify_profile_dropped(struct cras_bt_device *device,
 	 * given time so that user does not see a headset stay connected
 	 * but works with partial function.
 	 */
-	bt_device_schedule_suspend(device, PROFILE_DROP_SUSPEND_DELAY_MS,
-				   UNEXPECTED_PROFILE_DROP);
+	cras_bt_policy_schedule_suspend(device, PROFILE_DROP_SUSPEND_DELAY_MS,
+					UNEXPECTED_PROFILE_DROP);
 }
 
 /* Refresh the list of known supported profiles.
@@ -1003,8 +980,8 @@ int cras_bt_device_sco_connect(struct cras_bt_device *device, int codec)
 		       pollfd.revents, SCO_SUSPEND_DELAY_MS);
 		cras_server_metrics_hfp_sco_connection_error(
 			CRAS_METRICS_SCO_SKT_POLL_ERR_HUP);
-		bt_device_schedule_suspend(device, SCO_SUSPEND_DELAY_MS,
-					   HFP_SCO_SOCKET_ERROR);
+		cras_bt_policy_schedule_suspend(device, SCO_SUSPEND_DELAY_MS,
+						HFP_SCO_SOCKET_ERROR);
 		goto error;
 	}
 
@@ -1068,140 +1045,6 @@ void cras_bt_device_set_use_hardware_volume(struct cras_bt_device *device,
 int cras_bt_device_get_use_hardware_volume(struct cras_bt_device *device)
 {
 	return device->use_hardware_volume;
-}
-
-static void init_bt_device_msg(struct bt_device_msg *msg,
-			       enum BT_DEVICE_COMMAND cmd,
-			       struct cras_bt_device *device,
-			       struct cras_iodev *dev, unsigned int arg1,
-			       unsigned int arg2)
-{
-	memset(msg, 0, sizeof(*msg));
-	msg->header.type = CRAS_MAIN_BT;
-	msg->header.length = sizeof(*msg);
-	msg->cmd = cmd;
-	msg->device = device;
-	msg->dev = dev;
-	msg->arg1 = arg1;
-	msg->arg2 = arg2;
-}
-
-int cras_bt_device_cancel_suspend(struct cras_bt_device *device)
-{
-	struct bt_device_msg msg = CRAS_MAIN_MESSAGE_INIT;
-	int rc;
-
-	init_bt_device_msg(&msg, BT_DEVICE_CANCEL_SUSPEND, device, NULL, 0, 0);
-	rc = cras_main_message_send((struct cras_main_message *)&msg);
-	return rc;
-}
-
-int cras_bt_device_schedule_suspend(
-	struct cras_bt_device *device, unsigned int msec,
-	enum cras_bt_device_suspend_reason suspend_reason)
-{
-	struct bt_device_msg msg = CRAS_MAIN_MESSAGE_INIT;
-	int rc;
-
-	init_bt_device_msg(&msg, BT_DEVICE_SCHEDULE_SUSPEND, device, NULL, msec,
-			   suspend_reason);
-	rc = cras_main_message_send((struct cras_main_message *)&msg);
-	return rc;
-}
-
-static void bt_device_suspend_cb(struct cras_timer *timer, void *arg)
-{
-	struct cras_bt_device *device = (struct cras_bt_device *)arg;
-
-	BTLOG(btlog, BT_DEV_SUSPEND_CB, device->profiles,
-	      device->suspend_reason);
-	device->suspend_timer = NULL;
-
-	/* Error log the reason so we can track them in user reports. */
-	switch (device->suspend_reason) {
-	case A2DP_LONG_TX_FAILURE:
-		syslog(LOG_ERR, "Suspend dev: A2DP long Tx failure");
-		break;
-	case A2DP_TX_FATAL_ERROR:
-		syslog(LOG_ERR, "Suspend dev: A2DP Tx fatal error");
-		break;
-	case CONN_WATCH_TIME_OUT:
-		syslog(LOG_ERR, "Suspend dev: Conn watch times out");
-		break;
-	case HFP_SCO_SOCKET_ERROR:
-		syslog(LOG_ERR, "Suspend dev: SCO socket error");
-		break;
-	case HFP_AG_START_FAILURE:
-		syslog(LOG_ERR, "Suspend dev: HFP AG start failure");
-		break;
-	case UNEXPECTED_PROFILE_DROP:
-		syslog(LOG_ERR, "Suspend dev: Unexpected profile drop");
-		break;
-	}
-
-	cras_a2dp_suspend_connected_device(device);
-	cras_hfp_ag_suspend_connected_device(device);
-	cras_bt_device_disconnect(device->conn, device);
-}
-
-static void
-bt_device_schedule_suspend(struct cras_bt_device *device, unsigned int msec,
-			   enum cras_bt_device_suspend_reason suspend_reason)
-{
-	struct cras_tm *tm = cras_system_state_get_tm();
-
-	if (device->suspend_timer)
-		return;
-	device->suspend_reason = suspend_reason;
-	device->suspend_timer =
-		cras_tm_create_timer(tm, msec, bt_device_suspend_cb, device);
-}
-
-static void bt_device_cancel_suspend(struct cras_bt_device *device)
-{
-	struct cras_tm *tm = cras_system_state_get_tm();
-	if (device->suspend_timer == NULL)
-		return;
-	cras_tm_cancel_timer(tm, device->suspend_timer);
-	device->suspend_timer = NULL;
-}
-
-static void bt_device_process_msg(struct cras_main_message *msg, void *arg)
-{
-	struct bt_device_msg *bt_msg = (struct bt_device_msg *)msg;
-	struct cras_bt_device *device = NULL;
-
-	DL_FOREACH (devices, device) {
-		if (device == bt_msg->device)
-			break;
-	}
-
-	/* Do nothing if target device no longer exists. */
-	if (device == NULL)
-		return;
-
-	switch (bt_msg->cmd) {
-	case BT_DEVICE_SCHEDULE_SUSPEND:
-		bt_device_schedule_suspend(bt_msg->device, bt_msg->arg1,
-					   bt_msg->arg2);
-		break;
-	case BT_DEVICE_CANCEL_SUSPEND:
-		bt_device_cancel_suspend(bt_msg->device);
-		break;
-	default:
-		break;
-	}
-}
-
-void cras_bt_device_start_monitor()
-{
-	cras_main_message_add_handler(CRAS_MAIN_BT, bt_device_process_msg,
-				      NULL);
-}
-
-void cras_bt_device_stop_monitor()
-{
-	cras_main_message_rm_handler(CRAS_MAIN_BT);
 }
 
 void cras_bt_device_update_hardware_volume(struct cras_bt_device *device,

cras/src/server/cras_bt_device.h
@@ -15,16 +15,6 @@ struct cras_bt_device;
 struct cras_iodev;
 struct cras_timer;
 
-/* All the reasons for when CRAS schedule a suspend to BT device. */
-enum cras_bt_device_suspend_reason {
-	A2DP_LONG_TX_FAILURE,
-	A2DP_TX_FATAL_ERROR,
-	CONN_WATCH_TIME_OUT,
-	HFP_SCO_SOCKET_ERROR,
-	HFP_AG_START_FAILURE,
-	UNEXPECTED_PROFILE_DROP,
-};
-
 /* Object to represent a general bluetooth device, and used to
  * associate with some CRAS modules if it supports audio.
  * Members:
@@ -47,8 +37,6 @@ enum cras_bt_device_suspend_reason {
  *    conn_watch_retries - The retry count for conn_watch_timer.
  *    conn_watch_timer - The timer used to watch connected profiles and start
  *        BT audio input/ouput when all profiles are ready.
- *    suspend_timer - The timer used to suspend device.
- *    suspend_reason - The reason code for why suspend is scheduled.
  *    stable_id - The unique and persistent id of this bt_device.
  */
 struct cras_bt_device {
@@ -69,8 +57,6 @@ struct cras_bt_device {
 	int use_hardware_volume;
 	int conn_watch_retries;
 	struct cras_timer *conn_watch_timer;
-	struct cras_timer *suspend_timer;
-	enum cras_bt_device_suspend_reason suspend_reason;
 	unsigned int stable_id;
 
 	struct cras_bt_device *prev, *next;
@@ -195,11 +181,6 @@ cras_bt_device_get_active_profile(const struct cras_bt_device *device);
 void cras_bt_device_set_active_profile(struct cras_bt_device *device,
 				       unsigned int profile);
 
-void cras_bt_device_start_monitor();
-
-/* Stops monitoring messages sent for connected BT device. */
-void cras_bt_device_stop_monitor();
-
 /* Checks if the device has an iodev for A2DP. */
 int cras_bt_device_has_a2dp(struct cras_bt_device *device);
 
@@ -214,14 +195,6 @@ void cras_bt_device_update_hardware_volume(struct cras_bt_device *device,
 
 /* Notifies bt_device that a2dp connection is configured. */
 void cras_bt_device_a2dp_configured(struct cras_bt_device *device);
-
-/* Cancels any scheduled suspension of device. */
-int cras_bt_device_cancel_suspend(struct cras_bt_device *device);
-
-/* Schedules device to suspend after given delay. */
-int cras_bt_device_schedule_suspend(
-	struct cras_bt_device *device, unsigned int msec,
-	enum cras_bt_device_suspend_reason suspend_reason);
 
 /* Notifies bt device that audio gateway is initialized.
  * Args:

cras/src/server/cras_bt_policy.c
@@ -3,10 +3,17 @@
  * found in the LICENSE file.
  */
 
+#include <syslog.h>
+
+#include "cras_a2dp_endpoint.h"
 #include "cras_bt_device.h"
+#include "cras_bt_log.h"
+#include "cras_bt_policy.h"
+#include "cras_hfp_ag_profile.h"
 #include "cras_iodev.h"
 #include "cras_iodev_list.h"
 #include "cras_main_message.h"
+#include "cras_server_metrics.h"
 #include "cras_system_state.h"
 #include "cras_tm.h"
 #include "utlist.h"
@@ -15,6 +22,8 @@ static const unsigned int PROFILE_SWITCH_DELAY_MS = 500;
 
 enum BT_POLICY_COMMAND {
 	BT_POLICY_SWITCH_PROFILE,
+	BT_POLICY_SCHEDULE_SUSPEND,
+	BT_POLICY_CANCEL_SUSPEND,
 };
 
 struct bt_policy_msg {
@@ -33,6 +42,16 @@ struct profile_switch_policy {
 };
 
 struct profile_switch_policy *profile_switch_policies;
+
+/*    suspend_reason - The reason code for why suspend is scheduled. */
+struct suspend_policy {
+	struct cras_bt_device *device;
+	enum cras_bt_policy_suspend_reason suspend_reason;
+	struct cras_timer *timer;
+	struct suspend_policy *prev, *next;
+};
+
+struct suspend_policy *suspend_policies;
 
 static void profile_switch_delay_cb(struct cras_timer *timer, void *arg)
 {
@@ -133,6 +152,73 @@ static void init_bt_policy_msg(struct bt_policy_msg *msg,
 	msg->arg2 = arg2;
 }
 
+static void suspend_cb(struct cras_timer *timer, void *arg)
+{
+	struct suspend_policy *policy = (struct suspend_policy *)arg;
+
+	BTLOG(btlog, BT_DEV_SUSPEND_CB, policy->device->profiles,
+	      policy->suspend_reason);
+
+	/* Error log the reason so we can track them in user reports. */
+	switch (policy->suspend_reason) {
+	case A2DP_LONG_TX_FAILURE:
+		syslog(LOG_ERR, "Suspend dev: A2DP long Tx failure");
+		break;
+	case A2DP_TX_FATAL_ERROR:
+		syslog(LOG_ERR, "Suspend dev: A2DP Tx fatal error");
+		break;
+	case CONN_WATCH_TIME_OUT:
+		syslog(LOG_ERR, "Suspend dev: Conn watch times out");
+		break;
+	case HFP_SCO_SOCKET_ERROR:
+		syslog(LOG_ERR, "Suspend dev: SCO socket error");
+		break;
+	case HFP_AG_START_FAILURE:
+		syslog(LOG_ERR, "Suspend dev: HFP AG start failure");
+		break;
+	case UNEXPECTED_PROFILE_DROP:
+		syslog(LOG_ERR, "Suspend dev: Unexpected profile drop");
+		break;
+	}
+
+	cras_a2dp_suspend_connected_device(policy->device);
+	cras_hfp_ag_suspend_connected_device(policy->device);
+	cras_bt_device_disconnect(policy->device->conn, policy->device);
+
+	DL_DELETE(suspend_policies, policy);
+	free(policy);
+}
+
+static void schedule_suspend(struct cras_bt_device *device, unsigned int msec,
+			     enum cras_bt_policy_suspend_reason suspend_reason)
+{
+	struct cras_tm *tm = cras_system_state_get_tm();
+	struct suspend_policy *policy;
+
+	DL_SEARCH_SCALAR(suspend_policies, policy, device, device);
+	if (policy)
+		return;
+
+	policy = (struct suspend_policy *)calloc(1, sizeof(*policy));
+	policy->device = device;
+	policy->suspend_reason = suspend_reason;
+	policy->timer = cras_tm_create_timer(tm, msec, suspend_cb, policy);
+	DL_APPEND(suspend_policies, policy);
+}
+
+static void cancel_suspend(struct cras_bt_device *device)
+{
+	struct cras_tm *tm = cras_system_state_get_tm();
+	struct suspend_policy *policy;
+
+	DL_SEARCH_SCALAR(suspend_policies, policy, device, device);
+	if (policy) {
+		cras_tm_cancel_timer(tm, policy->timer);
+		DL_DELETE(suspend_policies, policy);
+		free(policy);
+	}
+}
+
 static void process_bt_policy_msg(struct cras_main_message *msg, void *arg)
 {
 	struct bt_policy_msg *policy_msg = (struct bt_policy_msg *)msg;
@@ -140,6 +226,14 @@ static void process_bt_policy_msg(struct cras_main_message *msg, void *arg)
 	switch (policy_msg->cmd) {
 	case BT_POLICY_SWITCH_PROFILE:
 		switch_profile(policy_msg->device, policy_msg->dev);
+		break;
+	case BT_POLICY_SCHEDULE_SUSPEND:
+		schedule_suspend(
+			policy_msg->device, policy_msg->arg1,
+			(enum cras_bt_policy_suspend_reason)policy_msg->arg2);
+		break;
+	case BT_POLICY_CANCEL_SUSPEND:
+		cancel_suspend(policy_msg->device);
 		break;
 	default:
 		break;
@@ -158,6 +252,29 @@ int cras_bt_policy_switch_profile(struct cras_bt_device *device,
 	return rc;
 }
 
+int cras_bt_policy_schedule_suspend(
+	struct cras_bt_device *device, unsigned int msec,
+	enum cras_bt_policy_suspend_reason suspend_reason)
+{
+	struct bt_policy_msg msg = CRAS_MAIN_MESSAGE_INIT;
+	int rc;
+
+	init_bt_policy_msg(&msg, BT_POLICY_SCHEDULE_SUSPEND, device, NULL, msec,
+			   suspend_reason);
+	rc = cras_main_message_send((struct cras_main_message *)&msg);
+	return rc;
+}
+
+int cras_bt_policy_cancel_suspend(struct cras_bt_device *device)
+{
+	struct bt_policy_msg msg = CRAS_MAIN_MESSAGE_INIT;
+	int rc;
+
+	init_bt_policy_msg(&msg, BT_POLICY_CANCEL_SUSPEND, device, NULL, 0, 0);
+	rc = cras_main_message_send((struct cras_main_message *)&msg);
+	return rc;
+}
+
 void cras_bt_policy_remove_device(struct cras_bt_device *device)
 {
 	struct profile_switch_policy *policy;
@@ -169,6 +286,7 @@ void cras_bt_policy_remove_device(struct cras_bt_device *device)
 		cras_tm_cancel_timer(tm, policy->timer);
 		free(policy);
 	}
+	cancel_suspend(device);
 }
 
 void cras_bt_policy_start()

commit ad386f572e6fa249ac7b95327ded5f109a921d61

CRAS: bt_device - Fix use after free crash
There are various reasons BT stack could inform CRAS to
remove certain BT devices, for example BT service crash.
That is a possible reason we see crash in handling BT
policy message handling in events from audio thread simply
because the message is handled later then when it has been
destroyed. This change fixes that by using a validity
check function.

Note that this is a regression caused by commit 23e014e
where we had the validity check in bt_device_suspend_cb
but lost that after refactor.

BUG=b:208939031
TEST=unittest

Change-Id: I5fd85f4319c92f481efc58d96dce2db970953b6e
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/3364838
Reviewed-by: Chih-Yang Hsia <paulhsia@chromium.org>
Commit-Queue: Hsinyu Chao <hychao@chromium.org>
Tested-by: Hsinyu Chao <hychao@chromium.org>

cras/src/server/cras_bt_device.c
@@ -255,6 +255,17 @@ struct cras_bt_device *cras_bt_device_get(const char *object_path)
 	return NULL;
 }
 
+bool cras_bt_device_valid(const struct cras_bt_device *target)
+{
+	struct cras_bt_device *device;
+
+	DL_FOREACH (devices, device) {
+		if (device == target)
+			return true;
+	}
+	return false;
+}
+
 const char *cras_bt_device_object_path(const struct cras_bt_device *device)
 {
 	return device->object_path;

cras/src/server/cras_bt_device.h
@@ -83,6 +83,11 @@ void cras_bt_device_reset();
 
 struct cras_bt_device *cras_bt_device_get(const char *object_path);
 
+/* Checks if the target bt device is still valid. Used in async events
+ * from audio thread to main thread where bt device could have already
+ * been destroyed. */
+bool cras_bt_device_valid(const struct cras_bt_device *target);
+
 const char *cras_bt_device_object_path(const struct cras_bt_device *device);
 
 /* Gets the stable id of given cras_bt_device. */

cras/src/server/cras_bt_policy.c
@@ -239,6 +239,13 @@ static void process_bt_policy_msg(struct cras_main_message *msg, void *arg)
 {
 	struct bt_policy_msg *policy_msg = (struct bt_policy_msg *)msg;
 
+	/* Before we handle the policy message, check if the memory pointing
+	 * to the bt device is still valid. It could have already been destroyed
+	 * in main thread for other reasons caused by BT stack. If that's the
+	 * case then just skip this message. */
+	if (!cras_bt_device_valid(policy_msg->device))
+		return;
+
 	switch (policy_msg->cmd) {
 	case BT_POLICY_SWITCH_PROFILE:
 		switch_profile(policy_msg->device, policy_msg->dev);

cras/src/tests/bt_device_unittest.cc
@@ -143,17 +143,33 @@ class BtDeviceTestSuite : public testing::Test {
 };
 
 TEST(BtDeviceSuite, CreateBtDevice) {
-  struct cras_bt_device* device;
+  struct cras_bt_device *device, *device2;
+  struct cras_bt_device* inval_dev;
 
   device = cras_bt_device_create(NULL, FAKE_OBJ_PATH);
   EXPECT_NE((void*)NULL, device);
 
   device = cras_bt_device_get(FAKE_OBJ_PATH);
   EXPECT_NE((void*)NULL, device);
+  EXPECT_EQ(1, cras_bt_device_valid(device));
+
+  /* Pick an address that is not a valid device for sure. */
+  inval_dev = reinterpret_cast<struct cras_bt_device*>(device + 0x1);
+  EXPECT_EQ(0, cras_bt_device_valid(inval_dev));
+
+  device2 = cras_bt_device_create(NULL, "/another/obj");
+  EXPECT_NE((void*)NULL, device2);
+  EXPECT_EQ(1, cras_bt_device_valid(device2));
+  EXPECT_EQ(1, cras_bt_device_valid(device));
 
   cras_bt_device_remove(device);
   device = cras_bt_device_get(FAKE_OBJ_PATH);
   EXPECT_EQ((void*)NULL, device);
+  EXPECT_EQ(0, cras_bt_device_valid(device));
+  EXPECT_EQ(1, cras_bt_device_valid(device2));
+
+  cras_bt_device_remove(device2);
+  EXPECT_EQ(0, cras_bt_device_valid(device2));
 }
 
 TEST_F(BtDeviceTestSuite, AppendRmIodev) {

cras/src/tests/bt_policy_unittest.cc
@@ -26,6 +26,7 @@ static int cras_bt_device_disconnect_called;
 static int cras_bt_device_connect_profile_called;
 static int cras_bt_device_remove_conflict_called;
 static int cras_bt_device_set_nodes_plugged_called;
+static bool cras_bt_device_valid_ret;
 
 void ResetStubData() {
   cras_tm_create_timer_called = 0;
@@ -41,6 +42,7 @@ void ResetStubData() {
   cras_bt_device_remove_conflict_called = 0;
   cras_bt_device_set_nodes_plugged_called = 0;
   cras_tm_create_timer_ret = reinterpret_cast<struct cras_timer*>(0x123);
+  cras_bt_device_valid_ret = 1;
 }
 
 // Iodev callback
@@ -70,19 +72,44 @@ class BtPolicyTestSuite : public testing::Test {
     device.bt_iodevs[CRAS_STREAM_INPUT] = &idev;
 
     btlog = cras_bt_event_log_init();
+
+    msg = CRAS_MAIN_MESSAGE_INIT;
   }
   virtual void TearDown() { cras_bt_event_log_deinit(btlog); }
 
+  struct bt_policy_msg msg;
   struct cras_bt_device device;
   struct cras_iodev idev;
   struct cras_iodev odev;
 };
 
+TEST_F(BtPolicyTestSuite, HandleMessageWithInvalidDev) {
+  /* Pretend the BT device is no longer valid. The message handler will
+   * just skip it without any issue. */
+  cras_bt_device_valid_ret = 0;
+  init_bt_policy_msg(&msg, BT_POLICY_SWITCH_PROFILE, &device, &idev, 0, 0);
+  process_bt_policy_msg(&msg.header, NULL);
+
+  EXPECT_EQ(0, cras_iodev_list_suspend_dev_called);
+  EXPECT_EQ(0, cras_iodev_list_resume_dev_called);
+  EXPECT_EQ(0, cras_tm_create_timer_called);
+
+  init_bt_policy_msg(&msg, BT_POLICY_SCHEDULE_SUSPEND, &device, NULL, 200,
+                     UNEXPECTED_PROFILE_DROP);
+  process_bt_policy_msg(&msg.header, NULL);
+  EXPECT_EQ(0, cras_tm_create_timer_called);
+  EXPECT_EQ(0, cras_hfp_ag_suspend_connected_device_called);
+  EXPECT_EQ(0, cras_a2dp_suspend_connected_device_called);
+  EXPECT_EQ(0, cras_bt_device_disconnect_called);
+}
+
 TEST_F(BtPolicyTestSuite, SwitchProfile) {
   /* In the typical switch profile case, the associated input and
    * output iodev are suspended and resumed later. */
   EXPECT_EQ(0, cras_iodev_list_suspend_dev_called);
-  switch_profile(&device, &idev);
+  init_bt_policy_msg(&msg, BT_POLICY_SWITCH_PROFILE, &device, &idev, 0, 0);
+  process_bt_policy_msg(&msg.header, NULL);
+
   EXPECT_EQ(2, cras_iodev_list_suspend_dev_called);
   EXPECT_EQ(1, cras_iodev_list_resume_dev_called);
   EXPECT_EQ(idev.info.idx, cras_iodev_list_resume_dev_idx);
@@ -94,7 +121,8 @@ TEST_F(BtPolicyTestSuite, SwitchProfile) {
 }
 
 TEST_F(BtPolicyTestSuite, SwitchProfileRepeatedly) {
-  switch_profile(&device, &idev);
+  init_bt_policy_msg(&msg, BT_POLICY_SWITCH_PROFILE, &device, &idev, 0, 0);
+  process_bt_policy_msg(&msg.header, NULL);
   EXPECT_EQ(2, cras_iodev_list_suspend_dev_called);
   EXPECT_EQ(1, cras_iodev_list_resume_dev_called);
   EXPECT_EQ(idev.info.idx, cras_iodev_list_resume_dev_idx);
@@ -104,7 +132,7 @@ TEST_F(BtPolicyTestSuite, SwitchProfileRepeatedly) {
    * is executed will cause the timer being cancelled and redo
    * all the suspend/resume and timer creation.
    */
-  switch_profile(&device, &idev);
+  process_bt_policy_msg(&msg.header, NULL);
   EXPECT_EQ(1, cras_tm_cancel_timer_called);
   EXPECT_EQ(4, cras_iodev_list_suspend_dev_called);
   EXPECT_EQ(2, cras_iodev_list_resume_dev_called);
@@ -119,7 +147,8 @@ TEST_F(BtPolicyTestSuite, DropHfpBeforeSwitchProfile) {
    * expected to still be suspended and resumed.
    */
   device.bt_iodevs[CRAS_STREAM_INPUT] = NULL;
-  switch_profile(&device, &idev);
+  init_bt_policy_msg(&msg, BT_POLICY_SWITCH_PROFILE, &device, &idev, 0, 0);
+  process_bt_policy_msg(&msg.header, NULL);
   EXPECT_EQ(1, cras_iodev_list_suspend_dev_called);
   EXPECT_EQ(0, cras_iodev_list_resume_dev_called);
   EXPECT_EQ(1, cras_tm_create_timer_called);
@@ -129,7 +158,8 @@ TEST_F(BtPolicyTestSuite, DropHfpBeforeSwitchProfile) {
 }
 
 TEST_F(BtPolicyTestSuite, DropA2dpWhileSwitchProfile) {
-  switch_profile(&device, &idev);
+  init_bt_policy_msg(&msg, BT_POLICY_SWITCH_PROFILE, &device, &idev, 0, 0);
+  process_bt_policy_msg(&msg.header, NULL);
   EXPECT_EQ(2, cras_iodev_list_suspend_dev_called);
   EXPECT_EQ(1, cras_iodev_list_resume_dev_called);
   EXPECT_EQ(idev.info.idx, cras_iodev_list_resume_dev_idx);
@@ -146,7 +176,8 @@ TEST_F(BtPolicyTestSuite, DropA2dpWhileSwitchProfile) {
 }
 
 TEST_F(BtPolicyTestSuite, RemoveDevWhileSwitchProfile) {
-  switch_profile(&device, &idev);
+  init_bt_policy_msg(&msg, BT_POLICY_SWITCH_PROFILE, &device, &idev, 0, 0);
+  process_bt_policy_msg(&msg.header, NULL);
   EXPECT_EQ(2, cras_iodev_list_suspend_dev_called);
   EXPECT_EQ(1, cras_iodev_list_resume_dev_called);
   EXPECT_EQ(idev.info.idx, cras_iodev_list_resume_dev_idx);
@@ -163,11 +194,15 @@ TEST_F(BtPolicyTestSuite, RemoveDevWhileSwitchProfile) {
 }
 
 TEST_F(BtPolicyTestSuite, ScheduleCancelSuspend) {
-  schedule_suspend(&device, 200, UNEXPECTED_PROFILE_DROP);
+  init_bt_policy_msg(&msg, BT_POLICY_SCHEDULE_SUSPEND, &device, NULL, 200,
+                     UNEXPECTED_PROFILE_DROP);
+  process_bt_policy_msg(&msg.header, NULL);
   EXPECT_EQ(1, cras_tm_create_timer_called);
 
   /* Schedule suspend does nothing if there's a ongoing one. */
-  schedule_suspend(&device, 100, HFP_AG_START_FAILURE);
+  init_bt_policy_msg(&msg, BT_POLICY_SCHEDULE_SUSPEND, &device, NULL, 100,
+                     HFP_AG_START_FAILURE);
+  process_bt_policy_msg(&msg.header, NULL);
   EXPECT_EQ(1, cras_tm_create_timer_called);
 
   cras_tm_create_timer_cb(NULL, cras_tm_create_timer_cb_data);
@@ -175,10 +210,13 @@ TEST_F(BtPolicyTestSuite, ScheduleCancelSuspend) {
   EXPECT_EQ(1, cras_a2dp_suspend_connected_device_called);
   EXPECT_EQ(1, cras_bt_device_disconnect_called);
 
-  schedule_suspend(&device, 200, HFP_AG_START_FAILURE);
+  init_bt_policy_msg(&msg, BT_POLICY_SCHEDULE_SUSPEND, &device, NULL, 200,
+                     HFP_AG_START_FAILURE);
+  process_bt_policy_msg(&msg.header, NULL);
   EXPECT_EQ(2, cras_tm_create_timer_called);
 
-  cancel_suspend(&device);
+  init_bt_policy_msg(&msg, BT_POLICY_CANCEL_SUSPEND, &device, NULL, 0, 0);
+  process_bt_policy_msg(&msg.header, NULL);
   EXPECT_EQ(1, cras_tm_cancel_timer_called);
 }
 
@@ -186,7 +224,9 @@ TEST_F(BtPolicyTestSuite, DevRemoveWithScheduleSuspend) {
   cras_bt_policy_remove_device(&device);
   EXPECT_EQ(0, cras_tm_cancel_timer_called);
 
-  schedule_suspend(&device, 200, UNEXPECTED_PROFILE_DROP);
+  init_bt_policy_msg(&msg, BT_POLICY_SCHEDULE_SUSPEND, &device, NULL, 200,
+                     UNEXPECTED_PROFILE_DROP);
+  process_bt_policy_msg(&msg.header, NULL);
   EXPECT_EQ(1, cras_tm_create_timer_called);
 
   cras_bt_policy_remove_device(&device);
@@ -402,6 +442,9 @@ int cras_bt_device_connect_profile(DBusConnection* conn,
                                    const char* uuid) {
   cras_bt_device_connect_profile_called++;
   return 0;
+}
+bool cras_bt_device_valid(const struct cras_bt_device* target) {
+  return cras_bt_device_valid_ret;
 }
 
 }  // extern "C"