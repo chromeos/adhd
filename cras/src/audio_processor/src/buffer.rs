bb6a0cfdb935072d4e226f555b410a08ec93f827
Li-Yu Yu <aaronyu@google.com>
2022-06-16 13:46:14 +0800

    audio_processor: Add MultiSlice to reference samples
    BUG=b:232742804
    TEST=cargo build && cargo test
    
    Change-Id: Ic791eb63f1420b09b450602e79179e8ca7ace72d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/3708981
    Tested-by: Li-Yu Yu <aaronyu@google.com>
    Reviewed-by: Chih-Yang Hsia <paulhsia@chromium.org>
    Commit-Queue: Li-Yu Yu <aaronyu@google.com>

--- cras/src/audio_processor/src/buffer.rs
+++ cras/src/audio_processor/src/buffer.rs
@@ -27,6 +27,11 @@ impl<T: Clone> MultiBuffer<T> {
 }
 
 impl<'a, S: Sample> MultiBuffer<S> {
+    /// Get a [`MultiSlice`] referencing the `MultiBuffer`
+    pub fn as_multi_slice(&'a mut self) -> MultiSlice<'a, S> {
+        MultiSlice::from_vecs(&mut self.data)
+    }
+
     /// Create `n` buffers with each with the specified `length`.
     /// Contents are initialized to `S::default()`.
     pub fn new(length: usize, n: usize) -> Self {
@@ -41,7 +46,7 @@ impl<'a, S: Sample> MultiBuffer<S> {
 }
 
 #[cfg(test)]
-mod tests {
+mod buffer_tests {
     use crate::MultiBuffer;
 
     #[test]
@@ -66,5 +71,64 @@ mod tests {
     fn with_capacity() {
         let buf = MultiBuffer::<f32>::with_capacity(4, 2);
         assert_eq!(buf.data, [[], []]);
+    }
+
+    #[test]
+    fn as_multi_slice() {
+        let mut buf = MultiBuffer::from(vec![vec![1, 2, 3, 4], vec![5, 6, 7, 8]]);
+        let mut slices = buf.as_multi_slice();
+        slices.data[0][1] = 0;
+        slices.data[1][2] = 0;
+        assert_eq!(buf.data, [[1, 0, 3, 4], [5, 6, 0, 8]]);
+    }
+}
+
+/// A `MultiSlice` holds multiple references to buffers of audio samples or bytes.
+/// Each slice typically references a channel of audio data.
+pub struct MultiSlice<'a, T> {
+    data: Vec<&'a mut [T]>,
+}
+
+impl<'a, T> MultiSlice<'a, T> {
+    /// Create a `MultiSlice` referencing data owned by `vecs`.
+    pub fn from_vecs(vecs: &'a mut [Vec<T>]) -> Self {
+        Self {
+            data: vecs.iter_mut().map(|ch| &mut ch[..]).collect(),
+        }
+    }
+
+    /// Create a `MultiSlice` referencing data referenced by `slices`.
+    pub fn from_raw(slices: Vec<&'a mut [T]>) -> Self {
+        Self { data: slices }
+    }
+
+    /// Consume `self` and return the holded slices.
+    pub fn into_raw(self) -> Vec<&'a mut [T]> {
+        self.data
+    }
+}
+
+#[cfg(test)]
+mod slice_tests {
+    use crate::MultiSlice;
+
+    #[test]
+    fn from_vecs() {
+        let mut vecs = vec![vec![1, 2, 3, 4], vec![5, 6, 7, 8]];
+        let mut slices = MultiSlice::from_vecs(&mut vecs);
+        slices.data[0][2] = 0;
+        slices.data[1][3] = 0;
+        assert_eq!(vecs, [[1, 2, 0, 4], [5, 6, 7, 0]]);
+    }
+
+    #[test]
+    fn slices_convertion() {
+        let mut a0 = [1, 2, 3, 4];
+        let mut a1 = [5, 6, 7, 8];
+        let raw = vec![&mut a0[..], &mut a1[..]];
+        let mut slices = MultiSlice::from_raw(raw);
+        slices.data[0][1] = 0;
+        slices.data[1][2] = 0;
+        assert_eq!(slices.into_raw(), [[1, 0, 3, 4], [5, 6, 0, 8]]);
     }
 }
