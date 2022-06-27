02de7d9f2a1309f8eea6c45498c039d3f24693ab
cranelw <cranelw@chromium.org>
2022-06-27 06:48:44 +0000

    CRAS: adds sample_buffer
    sample_buffer wraps byte_buffer and works at sample granularity,
    with size of each sample provided by the users.
    
    Usage:
    1. sample_buffer_init and sample_buffer_cleanup.
      sample_buffer will create and own the underlying byte_buffer.
    2. sample_buf_weak_ref
      sample_buffer uses an existing byte_buffer and doesn't own it.
    
    BUG=b:216075565
    TEST=sample_buffer_unittest.cc
    
    Change-Id: I55540a41e6cc2a97014fd28d633160f803a53320
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/3715172
    Commit-Queue: Cranel W <cranelw@chromium.org>
    Tested-by: Cranel W <cranelw@chromium.org>
    Reviewed-by: Chih-Yang Hsia <paulhsia@chromium.org>

---
+++ cras/src/common/sample_buffer.h
@@ -0,0 +1,273 @@
+/* Copyright 2022 The ChromiumOS Authors.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/*
+ * Sample buffer helps manipulate buffer in the granularity of samples.
+ *
+ * Usage:
+ *   1. sample_buffer_init and sample_buffer_cleanup:
+ *     In this way, sample_buffer will own the underlying byte_buffer and is
+ *     responsible for freeing it. We'd better not touching the underlying
+ *     buffer directly. Instead, we should only use the provided functions to
+ *     interact with the sample_buffer.
+ *   2. sample_buffer_weak_ref:
+ *     In this way, sample_buffer creates a weak reference of a byte_buffer,
+ *     and won't free it.
+ */
+#ifndef SAMPLE_BUFFER_H_
+#define SAMPLE_BUFFER_H_
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/param.h>
+
+#include "byte_buffer.h"
+
+/* A sample buffer.
+ *  sample_size - the number of bytes of each sample in the buffer.
+ *  buf - the byte_buffer that stores the data.
+ */
+struct sample_buffer {
+	const size_t sample_size;
+	struct byte_buffer *buf;
+};
+
+/* Inits a sample_buffer.
+ *
+ * Example usage:
+ *    struct sample_buffer buf = {};
+ *    int rc = sample_buffer_init(&buf, 100, 4);
+ *    if (rc != 0)
+ *      <error handling>;
+ *    <interaction with sample_buf>
+ *    sample_buffer_cleanup(&buf);
+ *
+ * Args:
+ *    num_samples - number of samples in the sample_buffer.
+ *    sample_size - size of each sample in the sample_buffer.
+ *    buf - the sample_buffer to be initialized.
+ * Returns:
+ *    0 on success, otherwise a negative error code.
+ */
+int sample_buffer_init(const size_t num_samples, const size_t sample_size,
+		       struct sample_buffer *buf)
+{
+	struct byte_buffer *internal_buf =
+		byte_buffer_create(num_samples * sample_size);
+	if (!internal_buf)
+		return -1;
+
+	struct sample_buffer temp_buf = { .sample_size = sample_size,
+					  .buf = internal_buf };
+	memcpy(buf, &temp_buf, sizeof(struct sample_buffer));
+
+	return 0;
+}
+
+/* Cleans up a sample_buffer.
+ *
+ * Args:
+ *    buf - the sample_buffer to be cleaned up.
+ */
+void sample_buffer_cleanup(struct sample_buffer *buf)
+{
+	if (buf && buf->buf)
+		byte_buffer_destroy(&buf->buf);
+}
+
+/* Checks if the sample_buf works well with the given byte_buffer.
+ *
+ * Args:
+ *    buf - the byte_buffer that is going to be validated.
+ *    sample_size - size of each sample that used to validate the byte_buffer.
+ * Returns:
+ *    1 on success, otherwise 0.
+ */
+static int sample_buffer_validate_byte_buffer(struct byte_buffer *buf,
+					      const size_t sample_size)
+{
+	return buf && (sample_size != 0) &&
+	       (buf->used_size % sample_size == 0) &&
+	       (buf->read_idx % sample_size == 0) &&
+	       (buf->write_idx % sample_size == 0) &&
+	       (buf->level % sample_size == 0);
+}
+
+/* Creates a sample_buffer that uses the given buf as its internal buffer.
+ *
+ * Do not call sample_buffer_cleanup for buffers created by this function.
+ *
+ * Args:
+ *    ref_buf - the byte_buffer to be referenced weakly.
+ *    sample_size - size of each sample.
+ * Returns:
+ *    A sample_buffer that uses the given ref_buf as its internal buffer.
+ */
+struct sample_buffer sample_buffer_weak_ref(struct byte_buffer *ref_buf,
+					    const size_t sample_size)
+{
+	assert(sample_buffer_validate_byte_buffer(ref_buf, sample_size) &&
+	       "sample_buffer_validate_byte_buffer failed.");
+	struct sample_buffer buf = { .sample_size = sample_size,
+				     .buf = ref_buf };
+	return buf;
+}
+
+/* Gets the number of readable samples.
+ *
+ * Args:
+ *    buf - the sample_buffer.
+ * Returns:
+ *    A unsigned integer indicating the number of readable samples.
+ */
+static inline unsigned int sample_buf_readable(const struct sample_buffer *buf)
+{
+	return buf_readable(buf->buf) / buf->sample_size;
+}
+
+/* Gets the number of queued samples.
+ *
+ * Args:
+ *    buf - the sample_buffer.
+ * Returns:
+ *    A unsigned integer indicating the number of queued samples.
+ */
+static inline unsigned int sample_buf_queued(const struct sample_buffer *buf)
+{
+	return buf_queued(buf->buf) / buf->sample_size;
+}
+
+/* Gets the pointer for reading samples.
+ *
+ * Args:
+ *    buf - the sample_buffer.
+ * Returns:
+ *    A pointer pointing to the head sample to read.
+ */
+static inline uint8_t *sample_buf_read_pointer(struct sample_buffer *buf)
+{
+	return buf_read_pointer(buf->buf);
+}
+
+/* Gets the pointer for reading samples and the number of readable samples.
+ *
+ * Args:
+ *    buf - the sample_buffer.
+ *    num_readable_samples - the number of readable samples.
+ * Returns:
+ *    A pointer pointing to the head sample to read.
+ */
+static inline uint8_t *
+sample_buf_read_pointer_size(struct sample_buffer *buf,
+			     unsigned int *num_readable_samples)
+{
+	*num_readable_samples = sample_buf_readable(buf);
+	return sample_buf_read_pointer(buf);
+}
+
+/* Increments the internal read pointer by num_inc_samples.
+ *
+ * Args:
+ *    buf - the sample_buffer.
+ *    num_inc_samples - the number of samples for the internal read pointer to
+ *      increment.
+ */
+static inline void sample_buf_increment_read(struct sample_buffer *buf,
+					     size_t num_inc_samples)
+{
+	buf_increment_read(buf->buf, num_inc_samples * buf->sample_size);
+}
+
+/* Gets the number of writable space in sample.
+ *
+ * Args:
+ *    buf - the sample_buffer.
+ * Returns:
+ *    A unsigned integer indicating the number of writable space in sample.
+ */
+static inline unsigned int sample_buf_writable(const struct sample_buffer *buf)
+{
+	return buf_writable(buf->buf) / buf->sample_size;
+}
+
+/* Gets the number of available space in sample.
+ *
+ * Args:
+ *    buf - the sample_buffer.
+ * Returns:
+ *    A unsigned integer indicating the number of available space in sample.
+ */
+static inline unsigned int sample_buf_available(const struct sample_buffer *buf)
+{
+	return buf_available(buf->buf) / buf->sample_size;
+}
+
+/* Gets the pointer for writing samples.
+ *
+ * Args:
+ *    buf - the sample_buffer.
+ * Returns:
+ *    A pointer pointing to the head position to write.
+ */
+static inline uint8_t *sample_buf_write_pointer(struct sample_buffer *buf)
+{
+	return buf_write_pointer(buf->buf);
+}
+
+/* Gets the pointer for writing samples and the number of writable space in
+ * sample.
+ *
+ * Args:
+ *    buf - the sample_buffer.
+ *    num_writable_samples - the number of writable space in sample.
+ * Returns:
+ *    A pointer pointing to the head position to write.
+ */
+static inline uint8_t *
+sample_buf_write_pointer_size(struct sample_buffer *buf,
+			      unsigned int *num_writable_samples)
+{
+	*num_writable_samples = sample_buf_writable(buf);
+	return sample_buf_write_pointer(buf);
+}
+
+/* Increments the internal write pointer by num_inc_samples.
+ *
+ * Args:
+ *    buf - the sample_buffer.
+ *    num_inc_samples - the number of samples for the internal write pointer
+ *      to increment.
+ */
+static inline void sample_buf_increment_write(struct sample_buffer *buf,
+					      size_t num_inc_samples)
+{
+	buf_increment_write(buf->buf, num_inc_samples * buf->sample_size);
+}
+
+/* Resets the sample_buffer.
+ *
+ * Args:
+ *    buf - the sample_buffer.
+ */
+static inline void sample_buf_reset(struct sample_buffer *buf)
+{
+	buf_reset(buf->buf);
+}
+
+/* Indicates if the buffer is full and the internal read index is zero.
+ *
+ * Args:
+ *    buf - the sample_buffer.
+ * Returns:
+ *    1 if the buffer is full and the internal read index is zero, otherwise 0.
+ */
+static inline int
+sample_buf_full_with_zero_read_index(struct sample_buffer *buf)
+{
+	return sample_buf_readable(buf) ==
+	       buf->buf->used_size / buf->sample_size;
+}
+
+#endif /* SAMPLE_BUFFER_H_ */
---
+++ cras/src/tests/sample_buffer_unittest.cc
@@ -0,0 +1,158 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gtest/gtest.h>
+
+#include "byte_buffer.h"
+#include "sample_buffer.h"
+
+namespace {
+
+struct SampleBufferTestParam {
+  unsigned int sample_size;
+  unsigned int num_bytes;
+};
+
+class SampleBufferTestSuite
+    : public testing::TestWithParam<SampleBufferTestParam> {};
+
+TEST_P(SampleBufferTestSuite, TestInitCleanUpBuffer) {
+  const auto& param = SampleBufferTestSuite::GetParam();
+  const size_t num_samples = 3;
+  struct sample_buffer buf = {};
+
+  EXPECT_EQ(sample_buffer_init(num_samples, param.sample_size, &buf), 0);
+  EXPECT_EQ(sample_buf_available(&buf), num_samples);
+
+  sample_buffer_cleanup(&buf);
+}
+
+TEST_P(SampleBufferTestSuite, TestWeakRef) {
+  const auto& param = SampleBufferTestSuite::GetParam();
+  const size_t num_samples = 3;
+  struct byte_buffer* byte_buf =
+      byte_buffer_create(param.sample_size * num_samples);
+
+  sample_buffer_weak_ref(byte_buf, param.sample_size);
+
+  // After the sample buf disappeared, byte_buf is still available.
+  buf_write_pointer(byte_buf)[0] = 0;
+  byte_buffer_destroy(&byte_buf);
+}
+
+TEST_P(SampleBufferTestSuite, TestWeakRefExternalBuffer) {
+  const auto& param = SampleBufferTestSuite::GetParam();
+  struct byte_buffer* buf = byte_buffer_create(param.num_bytes);
+  struct sample_buffer sample_buf =
+      sample_buffer_weak_ref(buf, param.sample_size);
+
+  EXPECT_EQ(sample_buf_readable(&sample_buf), 0);
+  EXPECT_EQ(sample_buf_queued(&sample_buf), 0);
+  EXPECT_EQ(sample_buf_writable(&sample_buf),
+            param.num_bytes / param.sample_size);
+  EXPECT_EQ(sample_buf_available(&sample_buf),
+            param.num_bytes / param.sample_size);
+  EXPECT_EQ(sample_buf_write_pointer(&sample_buf), buf_write_pointer(buf));
+  EXPECT_EQ(sample_buf_read_pointer(&sample_buf), buf_read_pointer(buf));
+
+  unsigned int num_writable_sample = 0;
+  EXPECT_EQ(sample_buf_write_pointer_size(&sample_buf, &num_writable_sample),
+            buf_write_pointer(buf));
+  EXPECT_EQ(num_writable_sample, param.num_bytes / param.sample_size);
+
+  unsigned int num_readable_sample = 0;
+  EXPECT_EQ(sample_buf_read_pointer_size(&sample_buf, &num_readable_sample),
+            buf_read_pointer(buf));
+  EXPECT_EQ(num_readable_sample, 0);
+
+  byte_buffer_destroy(&buf);
+}
+
+TEST_P(SampleBufferTestSuite, TestWriteReadExternalBuffer) {
+  const auto& param = SampleBufferTestSuite::GetParam();
+  struct byte_buffer* buf = byte_buffer_create(param.num_bytes);
+  struct sample_buffer sample_buf =
+      sample_buffer_weak_ref(buf, param.sample_size);
+
+  {  // writes sample(s)
+    sample_buf_increment_write(&sample_buf, 1);
+
+    EXPECT_EQ(sample_buf_queued(&sample_buf), 1);
+    EXPECT_EQ(buf_queued(buf), 1 * param.sample_size);
+    EXPECT_EQ(sample_buf_readable(&sample_buf), 1);
+    EXPECT_EQ(buf_readable(buf), 1 * param.sample_size);
+
+    EXPECT_EQ(sample_buf_available(&sample_buf),
+              buf_available(buf) / param.sample_size);
+    EXPECT_EQ(sample_buf_writable(&sample_buf),
+              buf_writable(buf) / param.sample_size);
+  }
+
+  {  // reads sample(s)
+    sample_buf_increment_read(&sample_buf, 1);
+
+    EXPECT_EQ(sample_buf_queued(&sample_buf), 0);
+    EXPECT_EQ(buf_queued(buf), 0);
+    EXPECT_EQ(sample_buf_readable(&sample_buf), 0);
+    EXPECT_EQ(buf_readable(buf), 0);
+
+    EXPECT_EQ(sample_buf_available(&sample_buf),
+              buf_available(buf) / param.sample_size);
+    EXPECT_EQ(sample_buf_writable(&sample_buf),
+              buf_writable(buf) / param.sample_size);
+  }
+
+  byte_buffer_destroy(&buf);
+}
+
+TEST_P(SampleBufferTestSuite, TestSampleBufFullWithZeroReadIndex) {
+  const auto& param = SampleBufferTestSuite::GetParam();
+  struct sample_buffer buf = {};
+  const size_t num_samples = 3;
+  EXPECT_EQ(sample_buffer_init(num_samples, param.sample_size, &buf), 0);
+
+  EXPECT_EQ(sample_buf_full_with_zero_read_index(&buf), 0);
+  sample_buf_increment_write(&buf, num_samples);
+  EXPECT_EQ(sample_buf_full_with_zero_read_index(&buf), 1);
+  sample_buf_increment_read(&buf, num_samples);
+  EXPECT_EQ(sample_buf_full_with_zero_read_index(&buf), 0);
+
+  sample_buffer_cleanup(&buf);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    TestWithParams,
+    SampleBufferTestSuite,
+    testing::Values(SampleBufferTestParam{.sample_size = 1, .num_bytes = 2},
+                    SampleBufferTestParam{.sample_size = 2, .num_bytes = 2},
+                    SampleBufferTestParam{.sample_size = 2, .num_bytes = 4},
+                    SampleBufferTestParam{.sample_size = 3, .num_bytes = 3},
+                    SampleBufferTestParam{.sample_size = 4, .num_bytes = 4}));
+
+class SampleBufferTestCheckSuite
+    : public testing::TestWithParam<SampleBufferTestParam> {};
+
+TEST_P(SampleBufferTestCheckSuite, TestCheckFalse) {
+  const auto& param = SampleBufferTestSuite::GetParam();
+  struct byte_buffer* buf = byte_buffer_create(param.num_bytes);
+
+  ASSERT_DEATH(sample_buffer_weak_ref(buf, param.sample_size),
+               "sample_buffer_validate_byte_buffer failed.");
+
+  byte_buffer_destroy(&buf);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    TestWithParams,
+    SampleBufferTestCheckSuite,
+    testing::Values(SampleBufferTestParam{.sample_size = 2, .num_bytes = 3},
+                    SampleBufferTestParam{.sample_size = 3, .num_bytes = 4},
+                    SampleBufferTestParam{.sample_size = 4, .num_bytes = 5}));
+
+}  // namespace
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
