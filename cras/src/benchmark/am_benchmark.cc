80ffb725ddb4c836908833f4aabd98c1c9a8e4b9
Chih-Yang Hsia <paulhsia@chromium.org>
2022-11-30 17:11:24 +0000

    cras_bench: Add BM_AM/SR benchmark
    - Add benchmark for testing sr model directly.
    - Support adding sleep between each inference with
      sleep_duration = 5, 10, 20, 40ms
      delay to simulate CRAS's use case.
    - Add --//:ml and ml_build
    
    BUG=b:260857329
    TEST=`cras_bench --benchmark_filter=BM_AM`
    TEST=bazel build //...
    
    Change-Id: I9ac385c480d0f9fd3a98f566696619535eda9f2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/4067560
    Tested-by: Chih-Yang Hsia <paulhsia@chromium.org>
    Reviewed-by: Li-Yu Yu <aaronyu@google.com>
    Commit-Queue: Chih-Yang Hsia <paulhsia@chromium.org>

---
+++ cras/src/benchmark/am_benchmark.cc
@@ -0,0 +1,83 @@
+// Copyright 2022 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <chrono>
+#include <cstdint>
+#include <iostream>
+#include <random>
+#include <thread>
+#include <vector>
+
+#include <benchmark/benchmark.h>
+
+#include "am.h"
+#include "benchmark_util.h"
+
+namespace {
+
+class BM_AM : public benchmark::Fixture {
+ public:
+  void SetUp(const ::benchmark::State& state) {
+    frames = 480;
+    std::random_device rnd_device;
+    std::mt19937 engine{rnd_device()};
+    samples = gen_float_samples(frames, engine);
+    output_buf = std::vector<float>(frames, 0);
+
+    int microseconds = state.range(0);
+    sleep_duration = std::chrono::duration<double, std::micro>{
+        static_cast<double>(microseconds)};
+  }
+
+  void TearDown(const ::benchmark::State& state) {}
+
+  // Number of |frames|.
+  size_t frames;
+  // Sleep time in microseconds.
+  std::chrono::duration<double, std::micro> sleep_duration;
+  // |frames| of samples.
+  std::vector<float> samples;
+  // |frames| of samples.
+  std::vector<float> output_buf;
+};
+
+BENCHMARK_DEFINE_F(BM_AM, SR)(benchmark::State& state) {
+  const char model_path[] =
+      "/run/imageloader/sr-bt-dlc/package/root/btnb.tflite";
+  struct am_context* ctx = am_new(model_path);
+  if (ctx == nullptr) {
+    state.SkipWithError("Model does not exist!");
+    return;
+  }
+
+  for (auto _ : state) {
+    // Use manual timer to skip sleep period
+    auto start = std::chrono::high_resolution_clock::now();
+    int rc = 0;
+    benchmark::DoNotOptimize(rc = am_process(ctx, samples.data(), frames,
+                                             output_buf.data(), frames));
+    if (rc != 0) {
+      state.SkipWithError("am_process error.");
+      break;
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed_seconds =
+        std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+    state.SetIterationTime(elapsed_seconds.count());
+    // Sleep here to simulate audio thread behavior
+    std::this_thread::sleep_for(sleep_duration);
+  }
+  state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(frames) *
+                          /*bytes per sample=*/2);
+  am_free(ctx);
+}
+
+BENCHMARK_REGISTER_F(BM_AM, SR)
+    ->Arg(5000)
+    ->Arg(10000)
+    ->Arg(20000)
+    ->Arg(40000)
+    ->UseManualTime();
+
+}  // namespace
