bdf419a8ba76f073f96633be4a6738be2fb10f0a
Li-Yu Yu <aaronyu@google.com>
2023-01-06 15:02:48 +0800

    Fix undefined behavior in IoDevTestSuite::RequestFloop
    1.  struct cras_floop_pair cfps[NUM_FLOOP_PAIRS_MAX];
        Uninitialized memory
    2.  cras_floop_pair_create_return = &cfps[i];
    3.  cras_iodev_list_request_floop()
    4.  mocked cras_floop_pair_create()
        returns cras_floop_pair_create_return,
        which contains uninitialized fields.
    5.  IoDevTestSuite::TearDown()
    6.  cras_iodev_list_reset();
    7.  cras_iodev_list_disable_floop_pair();
    8.  remove_all_streams_from_dev();
    9.  audio_thread_rm_open_dev(audio_thread, dev->direction, dev->info.idx);
        the dev->direction is uninitialized.
        audio_thread_rm_open_dev's 2nd argument is a enum.
        Assigning an invalid value to a enum is undefined behavior.
    
    BUG=b:251701189
    TEST=bazel test //... --config=ubsan  # with CL:4127648
    
    Change-Id: Id37c4c31508fd52e293581c5e3ed4a44088f8f64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/4142140
    Tested-by: Li-Yu Yu <aaronyu@google.com>
    Commit-Queue: Li-Yu Yu <aaronyu@google.com>
    Auto-Submit: Li-Yu Yu <aaronyu@google.com>
    Reviewed-by: Chih-Yang Hsia <paulhsia@chromium.org>
    Reviewed-by: Baili Deng <bailideng@google.com>

--- cras/src/tests/iodev_list_unittest.cc
+++ cras/src/tests/iodev_list_unittest.cc
@@ -3219,7 +3219,7 @@ INSTANTIATE_TEST_SUITE_P(
                            -5}));
 
 TEST_F(IoDevTestSuite, RequestFloop) {
-  struct cras_floop_pair cfps[NUM_FLOOP_PAIRS_MAX];
+  struct cras_floop_pair cfps[NUM_FLOOP_PAIRS_MAX] = {};
   // cras_floop_pair_create fails and returns NULL
   EXPECT_EQ(-ENOMEM, cras_iodev_list_request_floop(nullptr));
 
