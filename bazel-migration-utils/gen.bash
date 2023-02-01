e4b2c91e13a343f0074eca0e922d488b3213cd7c
Li-Yu Yu <aaronyu@google.com>
2023-02-01 09:48:46 +0800

    git rm -r bazel-migration-utils
    The job is done.
    
    BUG=b:251701189
    TEST=None
    
    Change-Id: Ib4a6785a1c8b47b051f13339a561d87ddf718a02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/4211476
    Tested-by: Li-Yu Yu <aaronyu@google.com>
    Commit-Queue: Li-Yu Yu <aaronyu@google.com>
    Reviewed-by: Chih-Yang Hsia <paulhsia@chromium.org>
    Auto-Submit: Li-Yu Yu <aaronyu@google.com>

--- bazel-migration-utils/gen.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Copyright 2022 The ChromiumOS Authors
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-set -o pipefail
-cd "$(dirname -- "${BASH_SOURCE[0]}")"
-go run .| buildifier -type build > ../cras/src/tests/BUILD.bazel
