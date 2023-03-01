fdceeadce06c20f5e9a22513e5a8dd35ddce1170
whalechang <whalechang@google.com>
2023-03-01 22:18:52 +0800

    Migration C/C++ extention to Native Debug
    Remove C/C++ extention debugger config.
    This script will only open gdbserver now.
    Start CRAS without minijail before gdbserver open.
    
    BUG=b:269219693
    TEST=cras_remote_gdb_server.py --remote dut_ip --port 5678
    
    Change-Id: I9b4d60bd78eba49ee0e7de6d26fe3fee39ee629d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/4297930
    Reviewed-by: Li-Yu Yu <aaronyu@google.com>
    Commit-Queue: Ching Yun Chang <whalechang@google.com>
    Tested-by: ChromeOS Audio Quick Verifier <audio-qv@chromeos-audio-qv.iam.gserviceaccount.com>

---
+++ devtools/cras_remote_gdb_server.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import json
+import os
+from pathlib import Path
+import sys
+import time
+
+from chromite.lib import remote_access
+
+
+class GDBServer:
+    def __init__(self, ssh_ip, ssh_port, gdb_port):
+        self.ssh_ip = ssh_ip
+        self.ssh_port = ssh_port
+        self.gdb_port = gdb_port
+        self.ssh_settings = remote_access.CompileSSHConnectSettings(
+            **self.GetExtraSshConfig(),
+        )
+
+    def GetExtraSshConfig(self):
+        return {
+            "CheckHostIP": "no",
+            "BatchMode": "yes",
+            "LogLevel": "QUIET",
+            f"LocalForward {str(self.gdb_port)}": f"localhost:{str(self.gdb_port)}",
+        }
+
+    def RunRemoteGDBServer(self):
+        """Handle remote gdb server."""
+        with remote_access.ChromiumOSDeviceHandler(
+            self.ssh_ip,
+            port=self.ssh_port,
+            connect_settings=self.ssh_settings,
+        ) as device:
+            gdbserver_cmds = [
+                "gdbserver",
+                "--attach",
+                f":{self.gdb_port}",
+                "$(ps -C cras -o pid=)",
+            ]
+            # stop cras with minijail
+            stop_cras = ["stop", "cras"]
+            # stop cras-dev
+            stop_cras_dev = ["stop", "cras-dev"]
+            # start cras without minijail
+            start_cras_dev = ["start", "cras-dev"]
+            # gdbserver clean up command
+            CleanupCmd = ["kill", "$(ps -C gdbserver -o pid=)"]
+
+            device.RegisterCleanupCmd(CleanupCmd)
+            print("clean up gdbserver")
+            device.run(CleanupCmd, check=False)
+            print("stop cras")
+            device.run(stop_cras, check=False)
+            print("stop cras-dev")
+            device.run(stop_cras_dev, check=False)
+            print("start cras-dev")
+            device.run(start_cras_dev)
+            print("start runing gdbserver on dut cmd: ", gdbserver_cmds)
+            device.run(gdbserver_cmds)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='remote GDB server',
+        description='This tool will automatically open remote gdb for dut and attach CRAS\n',
+    )
+    parser.add_argument("--remote", default=None, required=True, help="ip of target machine")
+    parser.add_argument("--port", default=1234, required=False, help="GDB server port")
+    options = parser.parse_args()
+    ssh_ip, _, ssh_port = options.remote.partition(":")
+    if not ssh_port:
+        ssh_port = 22
+    gdb_port = int(options.port)
+    ssh_port = int(ssh_port)
+    server = GDBServer(ssh_ip, ssh_port, gdb_port)
+    server.RunRemoteGDBServer()
+
+
+if __name__ == "__main__":
+    main()
