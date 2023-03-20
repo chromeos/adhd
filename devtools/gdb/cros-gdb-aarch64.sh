f9aa5cb57c3b9449f86df32a9602d8e95cef7ef6
whalechang <whalechang@google.com>
2023-03-20 11:55:16 +0800

    Add arm gdb in chroot
    BUG=b:269219693
    TEST=cherry corsola work
    
    Change-Id: I596128854b67f9660c996a2e4877faae452c5595
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/4349256
    Reviewed-by: Li-Yu Yu <aaronyu@google.com>
    Tested-by: Ching Yun Chang <whalechang@google.com>
    Tested-by: ChromeOS Audio Quick Verifier <audio-qv@chromeos-audio-qv.iam.gserviceaccount.com>
    Commit-Queue: Ching Yun Chang <whalechang@google.com>

---
+++ devtools/gdb/cros-gdb-aarch64.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Copyright 2023 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+# execute gdb within chroot
+cros_sdk /usr/bin/aarch64-cros-linux-gnu-gdb "$@"
