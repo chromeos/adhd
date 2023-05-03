f4e77cd4b93f457b599a57045b254d9f94258af2
Curtis Malainey <cujomalainey@chromium.org>
2023-05-03 11:28:42 -0700

    cras: ini: refactor ini code
    The ini parser code has suffered from a LOT of copy paste. Refactor by
    moving all integer fields into a lookup table so new fields are single
    line changes going forward.
    
    BUG=None
    TEST=check struct after parsing
    
    Change-Id: Ideba9b8e56dc34fc26a9c9e058382d8c4b2c1c3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/4500315
    Tested-by: ChromeOS Audio Quick Verifier <audio-qv@chromeos-audio-qv.iam.gserviceaccount.com>
    Auto-Submit: Curtis Malainey <cujomalainey@chromium.org>
    Reviewed-by: Li-Yu Yu <aaronyu@google.com>
    Commit-Queue: Li-Yu Yu <aaronyu@google.com>

--- cras/src/server/config/cras_board_config.c
+++ cras/src/server/config/cras_board_config.c
@@ -6,57 +6,54 @@
 #include "cras/src/server/config/cras_board_config.h"
 
 #include <errno.h>
+#include <stddef.h>
 #include <syslog.h>
 
 #include "cras/src/server/iniparser_wrapper.h"
+#include "cras_util.h"
 
-static const int32_t DEFAULT_OUTPUT_BUFFER_SIZE = 512;
-static const int32_t AEC_SUPPORTED_DEFAULT = 0;
-static const int32_t AEC_GROUP_ID_DEFAULT = -1;
-static const int32_t NS_SUPPORTED_DEFAULT = 0;
-static const int32_t AGC_SUPPORTED_DEFAULT = 0;
-static const int32_t NC_SUPPORTED_DEFAULT = 0;
-static const int32_t HW_ECHO_REF_DISABLED_DEFAULT = 1;
-static const int32_t AEC_ON_DSP_SUPPORTED_DEFAULT = 0;
-static const int32_t NS_ON_DSP_SUPPORTED_DEFAULT = 0;
-static const int32_t AGC_ON_DSP_SUPPORTED_DEFAULT = 0;
-static const int32_t BLUETOOTH_WBS_ENABLED_INI_DEFAULT = 1;
-static const int32_t BLUETOOTH_HFP_OFFLOAD_FINCH_APPLIED_INI_DEFAULT = 1;
-static const int32_t BLUETOOTH_DEPRIORITIZE_WBS_MIC_INI_DEFAULT = 0;
-static const int32_t HOTWORD_PAUSE_AT_SUSPEND_DEFAULT = 0;
-static const int32_t MAX_INTERNAL_MIC_GAIN_DEFAULT = 2000;
-static const int32_t MAX_INTERNAL_SPK_CHANNELS_DEFAULT = 2;
-// MAX_HEADPHONE_CHANNELS_DEFAULT applied to both headphone and lineout.
-static const int32_t MAX_HEADPHONE_CHANNELS_DEFAULT = 2;
-static const int32_t NC_STANDALONE_MODE_DEFAULT = 0;
+#define board_offset(x) (offsetof(struct cras_board_config, x))
 
 #define CONFIG_NAME "board.ini"
-#define DEFAULT_OUTPUT_BUF_SIZE_INI_KEY "output:default_output_buffer_size"
-#define AEC_SUPPORTED_INI_KEY "processing:aec_supported"
-#define AEC_GROUP_ID_INI_KEY "processing:group_id"
-#define NS_SUPPORTED_INI_KEY "processing:ns_supported"
-#define AGC_SUPPORTED_INI_KEY "processing:agc_supported"
-#define NC_SUPPORTED_INI_KEY "processing:nc_supported"
-#define HW_ECHO_REF_DISABLED_KEY "processing:hw_echo_ref_disabled"
-#define AEC_ON_DSP_SUPPORTED_INI_KEY "processing:aec_on_dsp_supported"
-#define NS_ON_DSP_SUPPORTED_INI_KEY "processing:ns_on_dsp_supported"
-#define AGC_ON_DSP_SUPPORTED_INI_KEY "processing:agc_on_dsp_supported"
-#define BLUETOOTH_WBS_ENABLED_INI_KEY "bluetooth:wbs_enabled"
-#define BLUETOOTH_HFP_OFFLOAD_FINCH_APPLIED_INI_KEY \
-  "bluetooth:hfp_offload_finch_applied"
-#define BLUETOOTH_DEPRIORITIZE_WBS_MIC_INI_KEY "bluetooth:deprioritize_wbs_mic"
+
 #define UCM_IGNORE_SUFFIX_KEY "ucm:ignore_suffix"
-#define HOTWORD_PAUSE_AT_SUSPEND "hotword:pause_at_suspend"
-#define MAX_INTERNAL_MIC_GAIN "input:max_internal_mic_gain"
-#define MAX_INTERNAL_SPK_CHANNELS_INI_KEY "output:max_internal_speaker_channels"
-#define MAX_HEADPHONE_CHANNELS_INI_KEY "output:max_headphone_channels"
-#define NC_STANDALONE_MODE_INI_KEY "processing:nc_standalone_mode"
+
+struct ini_int_field {
+  int32_t default_value;
+  int32_t offset;
+  const char* key;
+};
+
+// clang-format off
+static const struct ini_int_field INI_INT_KEYS[] = {
+    {512,  board_offset(default_output_buffer_size),   "output:default_output_buffer_size"},
+    {0,    board_offset(aec_supported),                "processing:aec_supported"},
+    {-1,   board_offset(aec_group_id),                 "processing:group_id"},
+    {0,    board_offset(ns_supported),                 "processing:ns_supported"},
+    {0,    board_offset(agc_supported),                "processing:agc_supported"},
+    {0,    board_offset(nc_supported),                 "processing:nc_supported"},
+    {1,    board_offset(hw_echo_ref_disabled),         "processing:hw_echo_ref_disabled"},
+    {0,    board_offset(aec_on_dsp_supported),         "processing:aec_on_dsp_supported"},
+    {0,    board_offset(ns_on_dsp_supported),          "processing:ns_on_dsp_supported"},
+    {0,    board_offset(agc_on_dsp_supported),         "processing:agc_on_dsp_supported"},
+    {1,    board_offset(bt_wbs_enabled),               "bluetooth:wbs_enabled"},
+    {1,    board_offset(bt_hfp_offload_finch_applied), "bluetooth:hfp_offload_finch_applied"},
+    {0,    board_offset(deprioritize_bt_wbs_mic),       "bluetooth:deprioritize_wbs_mic"},
+    {0,    board_offset(hotword_pause_at_suspend),      "hotword:pause_at_suspend"},
+    {2000, board_offset(max_internal_mic_gain),         "input:max_internal_mic_gain"},
+    {2,    board_offset(max_internal_speaker_channels), "output:max_internal_speaker_channels"},
+    // max_headphone_channels applied to both headphone and lineout.
+    {2,    board_offset(max_headphone_channels),        "output:max_headphone_channels"},
+    {0,    board_offset(nc_standalone_mode),            "processing:nc_standalone_mode"},
+};
+// clang-format on
 
 void cras_board_config_get(const char* config_path,
                            struct cras_board_config* board_config) {
   char ini_name[MAX_INI_NAME_LENGTH + 1];
   const char* ptr;
   dictionary* ini;
+  uint8_t* cfg_ptr = ((uint8_t*)board_config);
 
   if (config_path == NULL) {
     goto set_default;
@@ -70,46 +67,10 @@ void cras_board_config_get(const char* config_path,
     goto set_default;
   }
 
-  board_config->default_output_buffer_size = iniparser_getint(
-      ini, DEFAULT_OUTPUT_BUF_SIZE_INI_KEY, DEFAULT_OUTPUT_BUFFER_SIZE);
-
-  board_config->aec_supported =
-      iniparser_getint(ini, AEC_SUPPORTED_INI_KEY, AEC_SUPPORTED_DEFAULT);
-
-  board_config->aec_group_id =
-      iniparser_getint(ini, AEC_GROUP_ID_INI_KEY, AEC_GROUP_ID_DEFAULT);
-
-  board_config->ns_supported =
-      iniparser_getint(ini, NS_SUPPORTED_INI_KEY, NS_SUPPORTED_DEFAULT);
-
-  board_config->agc_supported =
-      iniparser_getint(ini, AGC_SUPPORTED_INI_KEY, AGC_SUPPORTED_DEFAULT);
-
-  board_config->nc_supported =
-      iniparser_getint(ini, NC_SUPPORTED_INI_KEY, NC_SUPPORTED_DEFAULT);
-
-  board_config->hw_echo_ref_disabled = iniparser_getint(
-      ini, HW_ECHO_REF_DISABLED_KEY, HW_ECHO_REF_DISABLED_DEFAULT);
-
-  board_config->aec_on_dsp_supported = iniparser_getint(
-      ini, AEC_ON_DSP_SUPPORTED_INI_KEY, AEC_ON_DSP_SUPPORTED_DEFAULT);
-
-  board_config->ns_on_dsp_supported = iniparser_getint(
-      ini, NS_ON_DSP_SUPPORTED_INI_KEY, NS_ON_DSP_SUPPORTED_DEFAULT);
-
-  board_config->agc_on_dsp_supported = iniparser_getint(
-      ini, AGC_ON_DSP_SUPPORTED_INI_KEY, AGC_ON_DSP_SUPPORTED_DEFAULT);
-
-  board_config->bt_wbs_enabled = iniparser_getint(
-      ini, BLUETOOTH_WBS_ENABLED_INI_KEY, BLUETOOTH_WBS_ENABLED_INI_DEFAULT);
-
-  board_config->bt_hfp_offload_finch_applied =
-      iniparser_getint(ini, BLUETOOTH_HFP_OFFLOAD_FINCH_APPLIED_INI_KEY,
-                       BLUETOOTH_HFP_OFFLOAD_FINCH_APPLIED_INI_DEFAULT);
-
-  board_config->deprioritize_bt_wbs_mic =
-      iniparser_getint(ini, BLUETOOTH_DEPRIORITIZE_WBS_MIC_INI_KEY,
-                       BLUETOOTH_DEPRIORITIZE_WBS_MIC_INI_DEFAULT);
+  for (int i = 0; i < ARRAY_SIZE(INI_INT_KEYS); i++) {
+    *((int32_t*)(cfg_ptr + INI_INT_KEYS[i].offset)) = iniparser_getint(
+        ini, INI_INT_KEYS[i].key, INI_INT_KEYS[i].default_value);
+  }
 
   ptr = iniparser_getstring(ini, UCM_IGNORE_SUFFIX_KEY, "");
   if (ptr) {
@@ -118,47 +79,14 @@ void cras_board_config_get(const char* config_path,
       syslog(LOG_ERR, "Failed to call strdup: %d", errno);
     }
   }
-
-  board_config->hotword_pause_at_suspend = iniparser_getint(
-      ini, HOTWORD_PAUSE_AT_SUSPEND, HOTWORD_PAUSE_AT_SUSPEND_DEFAULT);
-
-  board_config->max_internal_mic_gain = iniparser_getint(
-      ini, MAX_INTERNAL_MIC_GAIN, MAX_INTERNAL_MIC_GAIN_DEFAULT);
-
-  board_config->max_internal_speaker_channels =
-      iniparser_getint(ini, MAX_INTERNAL_SPK_CHANNELS_INI_KEY,
-                       MAX_INTERNAL_SPK_CHANNELS_DEFAULT);
-
-  board_config->max_headphone_channels = iniparser_getint(
-      ini, MAX_HEADPHONE_CHANNELS_INI_KEY, MAX_HEADPHONE_CHANNELS_DEFAULT);
-
-  board_config->nc_standalone_mode = iniparser_getint(
-      ini, NC_STANDALONE_MODE_INI_KEY, NC_STANDALONE_MODE_DEFAULT);
-
   iniparser_freedict(ini);
   syslog(LOG_DEBUG, "Loaded ini file %s", ini_name);
   return;
 
 set_default:
-  board_config->default_output_buffer_size = DEFAULT_OUTPUT_BUFFER_SIZE;
-  board_config->aec_supported = AEC_SUPPORTED_DEFAULT;
-  board_config->aec_group_id = AEC_GROUP_ID_DEFAULT;
-  board_config->ns_supported = NS_SUPPORTED_DEFAULT;
-  board_config->agc_supported = AGC_SUPPORTED_DEFAULT;
-  board_config->nc_supported = NC_SUPPORTED_DEFAULT;
-  board_config->hw_echo_ref_disabled = HW_ECHO_REF_DISABLED_DEFAULT;
-  board_config->aec_on_dsp_supported = AEC_ON_DSP_SUPPORTED_DEFAULT;
-  board_config->ns_on_dsp_supported = NS_ON_DSP_SUPPORTED_DEFAULT;
-  board_config->agc_on_dsp_supported = AGC_ON_DSP_SUPPORTED_DEFAULT;
   board_config->ucm_ignore_suffix = NULL;
-  board_config->bt_wbs_enabled = BLUETOOTH_WBS_ENABLED_INI_DEFAULT;
-  board_config->bt_hfp_offload_finch_applied =
-      BLUETOOTH_HFP_OFFLOAD_FINCH_APPLIED_INI_DEFAULT;
-  board_config->deprioritize_bt_wbs_mic =
-      BLUETOOTH_DEPRIORITIZE_WBS_MIC_INI_DEFAULT;
-  board_config->max_internal_mic_gain = MAX_INTERNAL_MIC_GAIN_DEFAULT;
-  board_config->max_internal_speaker_channels =
-      MAX_INTERNAL_SPK_CHANNELS_DEFAULT;
-  board_config->max_headphone_channels = MAX_HEADPHONE_CHANNELS_DEFAULT;
-  board_config->nc_standalone_mode = NC_STANDALONE_MODE_DEFAULT;
+  for (int i = 0; i < ARRAY_SIZE(INI_INT_KEYS); i++) {
+    *((int32_t*)(cfg_ptr + INI_INT_KEYS[i].offset)) =
+        INI_INT_KEYS[i].default_value;
+  }
 }
