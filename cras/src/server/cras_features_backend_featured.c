307d6485c60e21718c09f359e966792e5e9fd306
Li-Yu Yu <aaronyu@google.com>
2023-05-16 22:08:20 +0800

    cras_features: Cache CFeatureLibraryIsEnabledBlockingWithTimeout calls
    c_feature_library does not recommend caching the results.
    However we can only use the blocking version and we have way too many
    D-Bus calls that we might block the main thread and CRAS clients,
    so we accept the tradeoffs that a stale cache may introduce.
    
    BUG=b:281964203
    TEST=CQ
    
    Change-Id: I8f4087b40d52f3790701ceaed6eec749f7717108
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/4538882
    Reviewed-by: Chih-Yang Hsia <paulhsia@chromium.org>
    Tested-by: ChromeOS Audio Quick Verifier <audio-qv@chromeos-audio-qv.iam.gserviceaccount.com>
    Commit-Queue: Li-Yu Yu <aaronyu@google.com>

--- cras/src/server/cras_features_backend_featured.c
+++ cras/src/server/cras_features_backend_featured.c
@@ -4,12 +4,36 @@
  */
 #include <featured/c_feature_library.h>
 #include <stdbool.h>
+#include <time.h>
 
 #include "cras/src/server/cras_features_impl.h"
 
 #define FEATURE_LIBRARY_TIMEOUT_MS 500
+#define FEATURE_LIBRARY_CACHE_TTL_SEC 5
+
+// c_feature_library does not recommend caching the results.
+// However we can only use the blocking version and we have way too many
+// D-Bus calls that we might block the main thread and CRAS clients,
+// so we accept the tradeoffs that a stale cache may introduce.
+// TODO(b/277860318): Properly ListenForRefetchNeeded().
+static struct cached_feature {
+  bool cache_value;
+  time_t expires_sec;
+} cached_features[NUM_FEATURES];
 
 bool cras_features_backend_get_enabled(const struct cras_feature* feature) {
+  const enum cras_feature_id id = cras_feature_get_id(feature);
+
+  // Get current time.
+  struct timespec ts;
+  clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+
+  // If cache is live, return it.
+  if (ts.tv_sec < cached_features[id].expires_sec) {
+    return cached_features[id].cache_value;
+  }
+
+  // Query the feature status.
   const struct VariationsFeature featured_feature = {
       .name = feature->name,
       .default_state = feature->default_enabled ? FEATURE_ENABLED_BY_DEFAULT
@@ -20,5 +44,8 @@ bool cras_features_backend_get_enabled(const struct cras_feature* feature) {
       lib, &featured_feature, FEATURE_LIBRARY_TIMEOUT_MS);
   CFeatureLibraryDelete(lib);
 
+  // Set the cache value.
+  cached_features[id].cache_value = enabled;
+  cached_features[id].expires_sec = ts.tv_sec + FEATURE_LIBRARY_CACHE_TTL_SEC;
   return enabled;
 }
---
+++ cras/src/server/cras_features_backend_featured_test.cc
@@ -0,0 +1,47 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <featured/c_feature_library.h>
+
+#include "cras/src/server/cras_features.h"
+#include "gtest/gtest.h"
+
+// Mocks.
+extern "C" {
+
+int CFeatureLibraryIsEnabledBlockingWithTimeout_result;
+int CFeatureLibraryIsEnabledBlockingWithTimeout(
+    CFeatureLibrary handle,
+    const struct VariationsFeature* const feature,
+    int timeout_ms) {
+  return CFeatureLibraryIsEnabledBlockingWithTimeout_result;
+}
+
+struct timespec clock_gettime_result;
+int clock_gettime(clockid_t clockid, struct timespec* tp) {
+  *tp = clock_gettime_result;
+  return 0;
+}
+}
+
+TEST(FeaturesBackendFeatured, Caching) {
+  clock_gettime_result = {0, 0};
+  CFeatureLibraryIsEnabledBlockingWithTimeout_result = true;
+  EXPECT_EQ(cras_feature_enabled(CrosLateBootAudioTestFeatureFlag), true);
+
+  // Library result changed to false.
+  CFeatureLibraryIsEnabledBlockingWithTimeout_result = false;
+
+  for (int i = 0; i < 5; i++) {
+    clock_gettime_result = {i, 0};
+    // cras_feature_enabled should continue to return true, due to caching
+    EXPECT_EQ(cras_feature_enabled(CrosLateBootAudioTestFeatureFlag), true);
+  }
+
+  // Cache expired after 5 seconds, should return false
+  clock_gettime_result = {5, 0};
+  EXPECT_EQ(cras_feature_enabled(CrosLateBootAudioTestFeatureFlag), false);
+  clock_gettime_result = {6, 0};
+  EXPECT_EQ(cras_feature_enabled(CrosLateBootAudioTestFeatureFlag), false);
+}
