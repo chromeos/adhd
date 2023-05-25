1e37b0184b43813513ad03731c0eb347e7eacb1e
Li-Yu Yu <aaronyu@google.com>
2023-05-26 01:07:14 +0800

    Use sudo instead of runuser
    runuser is removed CL:4546917.
    
    BUG=b:284308852
    TEST=cras-dev.sh still works
    
    Change-Id: I5e3efd841d1d7296986896e164cdb6a7400465a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/4568463
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: ChromeOS Audio Quick Verifier <audio-qv@chromeos-audio-qv.iam.gserviceaccount.com>
    Commit-Queue: Li-Yu Yu <aaronyu@google.com>

--- init/cras-dev.sh
+++ init/cras-dev.sh
@@ -5,7 +5,7 @@
 
 source /usr/share/cros/init/cras-env.sh || exit 1
 
-exec runuser -u cras \
+exec sudo -u cras \
         -- \
         /usr/bin/cras \
         ${DSP_CONFIG} ${DEVICE_CONFIG_DIR} \
