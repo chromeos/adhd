99317a4a4718f1213eaf335b1e6d3b35a608ba3a
Chih-Yang Hsia <paulhsia@chromium.org>
2023-05-31 02:20:37 +0800

    sof_sys: Regenerate bindings with correct packed address lookup
    bindgen is using addr_of! instead of "reference first and convert
    to pointer later" since
    https://github.com/rust-lang/rust-bindgen/pull/2203.
    Regenerate bindings.rs with bindgen 0.63.0.
    
    BUG=b:239850356
    TEST=cargo clippy
    
    Change-Id: Id4f6f4c0ccbf222de27cd35d42dbdda7958dac2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/4570362
    Commit-Queue: Chih-Yang Hsia <paulhsia@chromium.org>
    Tested-by: chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com <chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com>
    Tested-by: ChromeOS Audio Quick Verifier <audio-qv@chromeos-audio-qv.iam.gserviceaccount.com>
    Tested-by: Chih-Yang Hsia <paulhsia@chromium.org>
    Reviewed-by: Li-Yu Yu <aaronyu@google.com>

--- sof_sys/src/lib.rs
+++ sof_sys/src/lib.rs
@@ -6,9 +6,6 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
-// FIXME: See b/239850356.
-// This should be removed when that bug is fixed.
-#![warn(unaligned_references)]
 
 pub mod bindings;
 #[allow(unused_imports)]
