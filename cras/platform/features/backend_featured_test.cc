4e687dfce0923cbc023f45630e735ba289f2dc90
Li-Yu Yu <aaronyu@google.com>
2023-06-07 17:24:30 +0800

    Reland "Fetch feature flags in a dedicated worker thread"
    This is a reland of commit bbcf36ac32aac23dc91a5f1ab99eaf770e50f9c2
    
    Original change's description:
    > Fetch feature flags in a dedicated worker thread
    >
    > BUG=b:283153010
    > TEST=tast run dut2 audio.CrasFeatures
    >
    > Change-Id: Ib5f00f9c501c81e8bed19754f13e52f8bf8810b9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/4600635
    > Reviewed-by: Chih-Yang Hsia <paulhsia@chromium.org>
    > Tested-by: chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com <chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com>
    > Commit-Queue: Li-Yu Yu <aaronyu@google.com>
    > Tested-by: Li-Yu Yu <aaronyu@google.com>
    > Tested-by: ChromeOS Audio Quick Verifier <audio-qv@chromeos-audio-qv.iam.gserviceaccount.com>
    
    BUG=b:283153010
    TEST=staging tryjob http://go/bbid/8778401212078841761 passed
    
    Cq-Depend: chromium:4609929
    Change-Id: Ie61c221a7b28ce2e07d1a5342ce2afb9b46bd2f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/4609931
    Tested-by: chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com <chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com>
    Reviewed-by: Chih-Yang Hsia <paulhsia@chromium.org>
    Tested-by: ChromeOS Audio Quick Verifier <audio-qv@chromeos-audio-qv.iam.gserviceaccount.com>
    Tested-by: Li-Yu Yu <aaronyu@google.com>
    Commit-Queue: Li-Yu Yu <aaronyu@google.com>

--- cras/platform/features/backend_featured_test.cc
+++ cras/platform/features/backend_featured_test.cc
@@ -2,49 +2,87 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <featured/c_feature_library.h>
+#include <featured/fake_platform_features.h>
+#include <unistd.h>
 
+#include "cras/platform/features/backend_featured.h"
 #include "cras/platform/features/features.h"
 #include "gtest/gtest.h"
 
-// Mocks.
-extern "C" {
-
-int CFeatureLibraryIsEnabledBlockingWithTimeout_result;
-int CFeatureLibraryIsEnabledBlockingWithTimeout(
-    CFeatureLibrary handle,
-    const struct VariationsFeature* const feature,
-    int timeout_ms) {
-  return CFeatureLibraryIsEnabledBlockingWithTimeout_result;
-}
-
-struct timespec clock_gettime_result;
-int clock_gettime(clockid_t clockid, struct timespec* tp) {
-  *tp = clock_gettime_result;
-  return 0;
-}
-}
-
-TEST(FeaturesBackendFeatured, Caching) {
-  // Necessary to test cras_feature_enabled properly.
-  EXPECT_EQ(cras_features_init(), 0);
-
-  clock_gettime_result = {0, 0};
-  CFeatureLibraryIsEnabledBlockingWithTimeout_result = true;
-  EXPECT_EQ(cras_feature_enabled(CrOSLateBootDisabledByDefault), true);
-
-  // Library result changed to false.
-  CFeatureLibraryIsEnabledBlockingWithTimeout_result = false;
-
-  for (int i = 0; i < 5; i++) {
-    clock_gettime_result = {i, 0};
-    // cras_feature_enabled should continue to return true, due to caching
-    EXPECT_EQ(cras_feature_enabled(CrOSLateBootDisabledByDefault), true);
+// Library adapter of the fake feature_library.
+class FakeFeatureLibraryAdapterImpl : public FeatureLibraryAdapter {
+ public:
+  bool Initialize(scoped_refptr<dbus::Bus> bus) override {
+    fake_lib_ = std::make_unique<feature::FakePlatformFeatures>(bus);
+    // Also set lib_ so FeatureLibraryAdapter::Get() can acquire it.
+    lib_ = fake_lib_.get();
+    return true;
   }
 
-  // Cache expired after 5 seconds, should return false
-  clock_gettime_result = {5, 0};
-  EXPECT_EQ(cras_feature_enabled(CrOSLateBootDisabledByDefault), false);
-  clock_gettime_result = {6, 0};
-  EXPECT_EQ(cras_feature_enabled(CrOSLateBootDisabledByDefault), false);
+  void Shutdown() override {
+    fake_lib_.reset(nullptr);
+  }
+
+  std::unique_ptr<feature::FakePlatformFeatures> fake_lib_;
+};
+
+TEST(FeaturesBackendFeatured, InitShutdown) {
+  // The real instance can be initialized once.
+  // At least check that it does not leak memory.
+  ASSERT_EQ(cras_features_init(), -ECONNRESET);
+  // The second init should fail.
+  ASSERT_EQ(cras_features_init(), -EEXIST);
+  cras_features_deinit();
+
+  // Try to construct and destruct the fake version multiple times.
+  ASSERT_EQ(
+      backend_featured_init(std::make_unique<FakeFeatureLibraryAdapterImpl>()),
+      0);
+  cras_features_deinit();
+  ASSERT_EQ(
+      backend_featured_init(std::make_unique<FakeFeatureLibraryAdapterImpl>()),
+      0);
+  cras_features_deinit();
+  ASSERT_EQ(
+      backend_featured_init(std::make_unique<FakeFeatureLibraryAdapterImpl>()),
+      0);
+  cras_features_deinit();
+  // Should be safe to double destruct.
+  cras_features_deinit();
+}
+
+TEST(FeaturesBackendFeatured, IsEnabled) {
+  auto adapter = std::make_unique<FakeFeatureLibraryAdapterImpl>();
+  FakeFeatureLibraryAdapterImpl* adapter_unowned = adapter.get();
+  ASSERT_EQ(backend_featured_init(std::move(adapter)), 0);
+  // Once backend_featured_init returns fake_lib_ should be initialized.
+  auto fake_lib = adapter_unowned->fake_lib_.get();
+
+  // Initial state.
+  EXPECT_TRUE(cras_feature_enabled(CrOSLateBootEnabledByDefault));
+  EXPECT_FALSE(cras_feature_enabled(CrOSLateBootDisabledByDefault));
+
+  // Invert enabled status.
+  fake_lib->SetEnabled("CrOSLateBootEnabledByDefault", false);
+  fake_lib->SetEnabled("CrOSLateBootDisabledByDefault", true);
+  fake_lib->TriggerRefetchSignal();
+  while (cras_feature_enabled(CrOSLateBootEnabledByDefault) ||
+         !cras_feature_enabled(CrOSLateBootDisabledByDefault)) {
+    usleep(1000);
+  }
+  EXPECT_FALSE(cras_feature_enabled(CrOSLateBootEnabledByDefault));
+  EXPECT_TRUE(cras_feature_enabled(CrOSLateBootDisabledByDefault));
+
+  // Clear enabled status.
+  fake_lib->ClearEnabled("CrOSLateBootEnabledByDefault");
+  fake_lib->ClearEnabled("CrOSLateBootDisabledByDefault");
+  fake_lib->TriggerRefetchSignal();
+  while (!cras_feature_enabled(CrOSLateBootEnabledByDefault) ||
+         cras_feature_enabled(CrOSLateBootDisabledByDefault)) {
+    usleep(1000);
+  }
+  EXPECT_TRUE(cras_feature_enabled(CrOSLateBootEnabledByDefault));
+  EXPECT_FALSE(cras_feature_enabled(CrOSLateBootDisabledByDefault));
+
+  cras_features_deinit();
 }
