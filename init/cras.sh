cc7bdf8518a605314823e0a783fc3992b90bad64
Gwendal Grignou <gwendal@chromium.org>
2023-08-15 10:18:35 -0700

    init: cras: In minijail, remove [dest] bindmount when same as [src]
    From minijail man page:
    """
    -b <src>[,[dest][,<writeable>]] ...
    Bind-mount src into the chroot directory at dest, optionally writeable.
    The src path must be an absolute path.
    
    If dest is not specified, it will default to src.
    ....
    """
    
    BUG=none
    TEST=check cras service starts.
    
    Change-Id: I784c1bba54d00ff9b39057fb9bbc8f6f1766ef62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/4780479
    Auto-Submit: Gwendal Grignou <gwendal@chromium.org>
    Commit-Queue: Gwendal Grignou <gwendal@chromium.org>
    Commit-Queue: Li-Yu Yu <aaronyu@google.com>
    Tested-by: chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com <chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com>
    Reviewed-by: Li-Yu Yu <aaronyu@google.com>
    Tested-by: ChromeOS Audio Quick Verifier <audio-qv@chromeos-audio-qv.iam.gserviceaccount.com>

--- init/cras.sh
+++ init/cras.sh
@@ -11,17 +11,17 @@ exec minijail0 -u cras -g cras -G --uts -v -l \
         -b '/usr/share/alsa/ucm/Loopback/HiFi.conf,/usr/share/alsa/ucm/Loopback/HiFi.conf' \
         -f /sys/fs/cgroup/cpuset/user_space/media/tasks \
         -k 'tmpfs,/run,tmpfs,MS_NODEV|MS_NOEXEC|MS_NOSUID,mode=755,size=10M' \
-        -b /run/bluetooth/audio,/run/bluetooth/audio \
-        -b /run/cras,/run/cras,1 \
-        -b /run/dbus,/run/dbus,1 \
+        -b /run/bluetooth/audio \
+        -b /run/cras,,1 \
+        -b /run/dbus,,1 \
         -k '/run/imageloader,/run/imageloader,none,MS_BIND|MS_REC' \
-        -b /run/udev,/run/udev \
-        -b /dev,/dev \
-        -b /dev/shm,/dev/shm,1 \
+        -b /run/udev \
+        -b /dev \
+        -b /dev/shm,,1 \
         -k proc,/proc,proc \
-        -b /sys,/sys \
+        -b /sys \
         -k 'tmpfs,/var,tmpfs,MS_NODEV|MS_NOEXEC|MS_NOSUID,mode=755,size=10M' \
-        -b /var/lib/metrics/,/var/lib/metrics/,1 \
+        -b /var/lib/metrics,,1 \
         -b /var/lib/metrics/structured,,1 \
         -b /var/lib/metrics/structured/events,,1 \
         -- \
