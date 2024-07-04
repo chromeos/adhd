53cc8d1db14e08d15debe3b85c5eba07e606f26f
Nathan Lee <nathanmyl@google.com>
2024-07-04 04:27:01 +0000

    cras: dsp: Add testtool for comparing behaviors of C code and rust code
    Use python to write a script which complie the dsp tests with libraries
    in C and rust respectively, and run the two binaries with same input to
    compare their behaavior. To run the test tool, an additional 'adhd'
    directory is needed and would be passed to the tool as an argument.
    
    BUG=None
    TEST=None
    
    Change-Id: Iabb49d86469bc0fff06cf4dc08b1fe1ea756fa08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/5677488
    Commit-Queue: Nathan Lee <nathanmyl@google.com>
    Tested-by: chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com <chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com>
    Reviewed-by: Li-Yu Yu <aaronyu@google.com>

---
+++ devtools/test_dsp_rust/test_dsp_rust.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env vpython3
+# Copyright 2024 The ChromiumOS Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import glob
+import os
+import shutil
+import subprocess
+import sys
+
+
+parser = argparse.ArgumentParser(
+    description='Build the same target with C and Rust from two adhd directory.'
+)
+parser.add_argument(
+    'test_target', metavar='"test_target.c"', type=str, help='Build target to be test'
+)
+parser.add_argument(
+    'adhd_dir',
+    metavar='"comparing adhd directory"',
+    type=str,
+    help='Origin adhd directory with C libraries',
+)
+parser.add_argument(
+    'testfiles_dir', metavar='"testfiles directory"', type=str, help='Directory with .wav testfiles'
+)
+args = parser.parse_args()
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+CUR_ADHD = os.path.join(SCRIPT_DIR, "../..")
+
+if (not os.path.isfile(os.path.join(CUR_ADHD, "cras/src/dsp/tests/", args.test_target))) or (
+    not os.path.isfile(os.path.join(args.adhd_dir, "cras/src/dsp/tests/", args.test_target))
+):
+    raise SystemExit("Test targets not found")
+
+if not os.path.isfile(os.path.join(SCRIPT_DIR, "cmpraw")):
+    subprocess.run(["bazel", "build", "//cras/src/dsp/tests:cmpraw"], cwd=CUR_ADHD, check=True)
+    shutil.copy("bazel-bin/cras/src/dsp/tests/cmpraw", SCRIPT_DIR)
+BUILD_TARGET = args.test_target.removesuffix(".c")
+try:
+    os.mkdir(os.path.join(SCRIPT_DIR, "sandbox"))
+except FileExistsError:
+    pass
+shutil.copy(
+    os.path.join("bazel-bin/cras/src/dsp/tests/", BUILD_TARGET),
+    os.path.join(SCRIPT_DIR, "sandbox/rust"),
+)
+subprocess.run(["bazel", "build", "//cras/src/dsp/tests:" + BUILD_TARGET], cwd=args.adhd_dir)
+shutil.copy(
+    os.path.join("bazel-bin/cras/src/dsp/tests/", BUILD_TARGET),
+    os.path.join(SCRIPT_DIR, "sandbox/c"),
+)
+os.chdir(SCRIPT_DIR)
+for file in os.listdir(args.testfiles_dir):
+    f = os.path.join(args.testfiles_dir, file)
+    subprocess.run(
+        [
+            "sox",
+            f,
+            "--bits",
+            "16",
+            "--encoding",
+            "signed-integer",
+            "--endian",
+            "little",
+            "./sandbox/test_file.raw",
+        ],
+        cwd=SCRIPT_DIR,
+        check=True,
+    )
+    subprocess.run(
+        ["./sandbox/rust", "sandbox/test_file.raw", "sandbox/rust_output.raw"],
+        cwd=SCRIPT_DIR,
+        check=True,
+    )
+    subprocess.run(
+        ["./sandbox/c", "sandbox/test_file.raw", "sandbox/c_output.raw"], cwd=SCRIPT_DIR, check=True
+    )
+    print(file + " result:", end='', flush=True)
+    subprocess.run(
+        ["./cmpraw", "sandbox/c_output.raw", "sandbox/rust_output.raw"], cwd=SCRIPT_DIR, check=True
+    )
+shutil.rmtree("sandbox")
+print("Remove generated binary with the command if there is no further testing:")
+print("\trm -f devtools/test_dsp_rust/cmpraw")
