dfcd1472a1b47bfa533d8aa413c8f269df33386e
Li-Yu Yu <aaronyu@google.com>
2024-10-12 20:27:24 +0800

    Remove catch-all //cras/src/server/rust
    Build targets should depend on the cras_rust_library or the
    cc_library binding, instead of the catch-all rust_library.
    
    cras_rust_library will provide the rust_library or redirect to
    @system_cras_rust depending on the build mode.
    
    BUG=b:372902037
    TEST=devtools/rust_generate.py
    TEST=cargo build && SYSTEM_CRAS_RUST_LIB=$PWD/target/debug/libcras_rust.a bazel test --//:system_cras_rust //...
    TEST=bazel test //...
    TEST=FEATURES='test -usersandbox' emerge-amd64-generic cras_rust adhd
    
    Change-Id: I29c27172fdd86af4ea2917459fd5e82a8e0a7b7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/5920306
    Tested-by: chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com <chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com>
    Commit-Queue: Li-Yu Yu <aaronyu@google.com>
    Reviewed-by: Hung-Hsien Chen <hunghsienchen@chromium.org>

--- cras/src/server/rust/staticlib/src/lib.rs
+++ cras/src/server/rust/staticlib/src/lib.rs
@@ -2,5 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// The only purpose of this package is to export the cras crate as a staticlib.
-pub use cras;
+// The only purpose of this crate is to export the cras crates as a staticlib.
+pub use cras_common;
+pub use cras_dlc;
+pub use cras_feature_tier;
+pub use cras_features_backend;
+pub use cras_processor;
+pub use cras_rate_estimator;
+pub use cras_s2;
+pub use dsp_rust;
