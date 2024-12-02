48431246977f301c776e24a3f2cd208cc6bcc658
Baili Deng <bailideng@google.com>
2024-12-02 18:08:08 +0800

    sound_card_init_yaml_to_xml.py: Adjust generated directories and license
    Adjust the generated file directories so that copying over config files
    is easier.
    
    Adjust the generated AOSP license format so that it passes the preupload
    hooks.
    
    BUG=b:377624799
    TEST=`python3 sound_card_init_yaml_to_xml.py \
          ${SRC}/overlays/overlay-brya/chromeos-base/chromeos-bsp-brya/files \
          ../sound_card_init`
    
    Change-Id: I18f73d842e941b1430a31c4d2fbc16c95ca35cd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/6062459
    Tested-by: chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com <chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com>
    Commit-Queue: Baili Deng <bailideng@google.com>
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>

--- devtools/sound_card_init_yaml_to_xml.py
+++ devtools/sound_card_init_yaml_to_xml.py
@@ -10,6 +10,20 @@ import xml.etree.ElementTree as ET
 import yaml
 
 
+def get_card_name(ucm_file):
+    with open(ucm_file, "r") as f:
+        ucm_string = f.read()
+
+    current_device = None
+    lines = ucm_string.strip().split("\n")
+
+    # Extract SoundCardName from the first occurrence of cdev
+    for line in lines:
+        line = line.strip()
+        if line.startswith("cdev"):
+            return line.split(" ")[1].strip('"').split('hw:')[1]
+
+
 def list_to_xml_element(data, root, key):
     """Converts a array to XML elements."""
     for item in data:
@@ -40,7 +54,7 @@ def dict_to_xml(data, output_file):
     dict_to_xml_element(data, root)
     # add the licence comment in the xml
     comment = ET.Comment(
-        "Copyright (C) 2024 The Android Open Source Project\n\n"
+        "\nCopyright (C) 2024 The Android Open Source Project\n\n"
         "Licensed under the Apache License, Version 2.0 (the \"License\");\n"
         "you may not use this file except in compliance with the License.\n"
         "You may obtain a copy of the License at\n\n"
@@ -49,7 +63,7 @@ def dict_to_xml(data, output_file):
         "distributed under the License is distributed on an \"AS IS\" BASIS,\n"
         "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"
         "See the License for the specific language governing permissions and\n"
-        "limitations under the License."
+        "limitations under the License.\n"
     )
     root.insert(0, comment)  # insert before the root's first child
     tree = ET.ElementTree(root)
@@ -75,6 +89,7 @@ def convert_sound_card_init_configs(base_dir, output_dir):
     """Converts sound-card-init-config yaml files to AMP.xml."""
 
     audio_sound_card_init_path = os.path.join("audio", "sound-card-init-config")
+    audio_ucm_path = os.path.join("audio", "ucm-config")
 
     for model_dir in os.listdir(base_dir):
         model_path = os.path.join(base_dir, model_dir)
@@ -87,9 +102,20 @@ def convert_sound_card_init_configs(base_dir, output_dir):
 
                 if os.path.isfile(config):
                     amp_name = get_amp_name(yaml_file_name)
-                    output_file = os.path.join(output_dir, model_dir, f"{amp_name}.xml")
-                    config_data = parse_sound_card_init_yaml(config)
-                    dict_to_xml(config_data, output_file)
+                    for ucm_suffix in os.listdir(os.path.join(model_path, audio_ucm_path)):
+                        ucm_dir = os.path.join(model_path, audio_ucm_path, ucm_suffix)
+                        ucm_file = os.path.join(ucm_dir, "HiFi.conf")
+                        if os.path.isfile(ucm_file):
+                            card_name = get_card_name(ucm_file)
+                            model_output_dir = os.path.join(
+                                output_dir, model_dir, f"{card_name}.{model_dir}"
+                            )
+
+                            output_file = os.path.join(model_output_dir, f"{amp_name}.xml")
+                            # Create the directory for the output file if it doesn't exist
+                            os.makedirs(os.path.dirname(output_file), exist_ok=True)
+                            config_data = parse_sound_card_init_yaml(config)
+                            dict_to_xml(config_data, output_file)
 
 
 if __name__ == "__main__":
