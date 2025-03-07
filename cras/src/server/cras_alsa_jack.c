4df35ad3c51d3cb77242283622634b544cac2697
Baili Deng <bailideng@google.com>
2025-03-07 19:41:07 +0800

    cras: Add "DP" string check to check for HDMI/DP Jack
    Due to naming conventions, Navi has a HDMI/DP Jack that is not correctly
    detected.
    
    Update the jack name check to include checking for "DP" string in jack.
    Add additional name checking instead of using info in PCM to reduce
    drastic changes introduce in CRAS.
    
    BUG=b:401415704
    TEST=Check Navi "sof-mt8196_tas2563_5682 DP Jack" is detected as HDMI
    
    Change-Id: Ic74737f75ebacf2ed71dd0305f1100a1af47d8d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/6332423
    Tested-by: Baili Deng <bailideng@google.com>
    Auto-Submit: Baili Deng <bailideng@google.com>
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Tested-by: chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com <chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com>
    Reviewed-by: Terry Cheong <htcheong@chromium.org>
    Commit-Queue: Terry Cheong <htcheong@chromium.org>

--- cras/src/server/cras_alsa_jack.c
+++ cras/src/server/cras_alsa_jack.c
@@ -704,8 +704,9 @@ static snd_hctl_elem_t* find_eld_control_by_dev_index(snd_hctl_t* hctl,
 static int is_jack_hdmi_dp(const char* jack_name) {
   // TODO(hychao): Use the information provided in UCM instead of
   // name matching.
-  static const char* hdmi_dp = "HDMI";
-  return !!strstr(jack_name, hdmi_dp);
+  static const char* hdmi = "HDMI";
+  static const char* dp = "DP";
+  return !!strstr(jack_name, hdmi) || !!strstr(jack_name, dp);
 }
 
 /* Find GPIO jacks for this jack_list.
