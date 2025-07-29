206b8171948eaa3412eeef4c58ac025e3a7bc3e6
Li-Yu Yu <aaronyu@google.com>
2025-07-29 05:16:48 +0000

    Make cras.sh comment friendly
    This should be a no-op change.
    
    BUG=b:434815967
    TEST=CQ
    
    Change-Id: I0a1c1cfa808b217bc53bfed12c6dfc28912e246e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/6796990
    Commit-Queue: Li-Yu Yu <aaronyu@google.com>
    Tested-by: chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com <chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com>
    Reviewed-by: Hung-Hsien Chen <hunghsienchen@chromium.org>

--- init/cras.sh
+++ init/cras.sh
@@ -3,34 +3,45 @@
 # found in the LICENSE file.
 
 source /usr/share/cros/init/cras-env.sh || exit 1
-# Leave cras in the init pid namespace as it uses its PID as an IPC identifier.
-exec minijail0 -u cras -g cras -G --uts -v -l \
-        -T static \
-        -P /mnt/empty \
-        -b /,/ \
-        -b '/usr/share/alsa/ucm/Loopback/HiFi.conf,/usr/share/alsa/ucm/Loopback/HiFi.conf' \
-        -f /sys/fs/cgroup/cpuset/user_space/media/tasks \
-        -k 'tmpfs,/run,tmpfs,MS_NODEV|MS_NOEXEC|MS_NOSUID,mode=755,size=10M' \
-        -b /run/bluetooth/audio \
-        -b /run/perfetto,/run/perfetto,1 \
-        -b /run/cras,,1 \
-        -b /run/dbus,,1 \
-        -k '/run/imageloader,/run/imageloader,none,MS_BIND|MS_REC' \
-        -b /run/libsegmentation \
-        -b /run/udev \
-        -b /run/chromeos-config/v1 \
-        -b /dev \
-        -b /dev/shm,,1 \
-        -k proc,/proc,proc \
-        -b /sys \
-        -k 'tmpfs,/var,tmpfs,MS_NODEV|MS_NOEXEC|MS_NOSUID,mode=755,size=10M' \
-        -b /var/lib/metrics,,1 \
-        -b /var/lib/metrics/structured,,1 \
-        -b /var/lib/metrics/structured/events,,1 \
+
+FIRST_MINIJAIL0_ARGS=(
+        # No -p: leave cras in the init pid namespace as it uses its PID as an IPC identifier.
+        -u cras -g cras -G --uts -v -l
+        -T static
+        -P /mnt/empty
+        -b /,/
+        -b '/usr/share/alsa/ucm/Loopback/HiFi.conf,/usr/share/alsa/ucm/Loopback/HiFi.conf'
+        -f /sys/fs/cgroup/cpuset/user_space/media/tasks
+        -k 'tmpfs,/run,tmpfs,MS_NODEV|MS_NOEXEC|MS_NOSUID,mode=755,size=10M'
+        -b /run/bluetooth/audio
+        -b /run/perfetto,/run/perfetto,1
+        -b /run/cras,,1
+        -b /run/dbus,,1
+        -k '/run/imageloader,/run/imageloader,none,MS_BIND|MS_REC'
+        -b /run/libsegmentation
+        -b /run/udev
+        -b /run/chromeos-config/v1
+        -b /dev
+        -b /dev/shm,,1
+        -k proc,/proc,proc
+        -b /sys
+        -k 'tmpfs,/var,tmpfs,MS_NODEV|MS_NOEXEC|MS_NOSUID,mode=755,size=10M'
+        -b /var/lib/metrics,,1
+        -b /var/lib/metrics/structured,,1
+        -b /var/lib/metrics/structured/events,,1
+)
+
+SECOND_MINIJAIL0_ARGS=(
+        -n
+        -Y
+        -S /usr/share/policy/cras-seccomp.policy
+)
+
+exec minijail0 \
+        "${FIRST_MINIJAIL0_ARGS[@]}" \
         -- \
-        /sbin/minijail0 -n \
-        -Y \
-        -S /usr/share/policy/cras-seccomp.policy \
+        /sbin/minijail0 \
+        "${SECOND_MINIJAIL0_ARGS[@]}" \
         -- \
         /usr/bin/cras \
         ${DSP_CONFIG} ${DEVICE_CONFIG_DIR} \
