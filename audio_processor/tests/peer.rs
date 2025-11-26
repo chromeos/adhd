35f470b7ee3563938d99ed180f8c063b5a47f4fd
Li-Yu Yu <aaronyu@google.com>
2025-11-26 08:14:56 +0000

    audio_processor: Extend peer test to cover FFI
    BUG=b:446725446
    TEST=bazel test //audio_processor:integration_tests_tests/peer
    
    Change-Id: I2910d277253cb186b5b0f6c1a20431b043a08f27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/7207127
    Tested-by: chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com <chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com>
    Reviewed-by: Hung-Hsien Chen <hunghsienchen@chromium.org>
    Commit-Queue: Li-Yu Yu <aaronyu@google.com>
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>

--- audio_processor/tests/peer.rs
+++ audio_processor/tests/peer.rs
@@ -44,3 +44,39 @@ fn test_peer() {
     let output = processor.process(input.as_multi_slice()).unwrap();
     assert_eq!(output.into_raw(), [[-11., -12., -13.], [-15., -16., -17.]]);
 }
+
+#[test]
+#[cfg(feature = "bazel")]
+fn test_peer_ffi_negate() {
+    let audio_worker_executable = env!("CARGO_BIN_EXE_audio-worker");
+    let factory =
+        AudioWorkerSubprocessFactory::default().with_executable_path(audio_worker_executable);
+    let test_plugin_path = std::env::var("LIBTEST_PLUGINS_SO").expect("LIBTEST_PLUGINS_SO not set");
+
+    let mut processor = ManagedBlockingSeqPacketProcessor::new(
+        &factory,
+        Format {
+            channels: 1,
+            block_size: 4,
+            frame_rate: 48000,
+        },
+        config::Processor::Plugin {
+            path: test_plugin_path.into(),
+            constructor: "negate_processor_create".to_string(),
+        },
+    )
+    .unwrap();
+
+    assert_eq!(
+        processor.get_output_format(),
+        Format {
+            channels: 1,
+            block_size: 4,
+            frame_rate: 48000,
+        }
+    );
+
+    let mut input = MultiBuffer::from(vec![vec![1., 2., 3., 4.]]);
+    let output = processor.process(input.as_multi_slice()).unwrap();
+    assert_eq!(output.into_raw(), [[-1., -2., -3., -4.]]);
+}
